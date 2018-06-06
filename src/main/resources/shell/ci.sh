4844a55d12553bf6476e1d41d9fe1ec4d27d5191
10980 <zhiyang.zhu@hand-china.com>
2018-06-06 11:00:55 +0800

    [ADD]ci change

---
+++ src/main/resources/shell/ci.sh
@@ -0,0 +1,45 @@
+# 获取的项目名称
+export GROUP_NAME={{ GROUP_NAME }}
+# 获取的应用名称
+export PROJECT_NAME={{ PROJECT_NAME }}
+# 获取当前提交版本号，具体规则请查阅：https://github.com/choerodon/cibase
+export CI_COMMIT_TAG=$(GetVersion)
+# 更新maven项目本版本号
+function update_pom_version(){
+    mvn versions:set -DnewVersion=${CI_COMMIT_TAG} || \
+    find . -name pom.xml | xargs xml ed -L \
+        -N x=http://maven.apache.org/POM/4.0.0 \
+        -u '/x:project/x:version' -v "${CI_COMMIT_TAG}"
+    mvn versions:commit
+}
+# 测试分支合并到指定分支，比如合并到master：git_merge master，默认合并到develop
+function git_merge(){
+    git config user.name ${GITLAB_USER_NAME}
+    git config user.email ${GITLAB_USER_EMAIL}
+    git checkout origin/${1:-"develop"}
+    git merge ${CI_COMMIT_SHA} --no-commit --no-ff
+}
+# 此项为上传构建并上传chart包到Choerodon中，只有通过此函数Choerodon才会有相应版本记录。
+function chart_build(){
+    # 查找Chart.yaml文件
+    CHART_PATH=`find . -maxdepth 3 -name Chart.yaml`
+    # 重置values.yaml文件中image.repository属性
+    yq w -i ${CHART_PATH%/*}/values.yaml image.repository ${DOCKER_REGISTRY}/${GROUP_NAME}/${PROJECT_NAME}
+    # 构建chart包，重写version与app-version为当前版本
+    helm package ${CHART_PATH%/*} --version ${CI_COMMIT_TAG} --app-version ${CI_COMMIT_TAG}
+    TEMP=${CHART_PATH%/*}
+    FILE_NAME=${TEMP##*/}
+    # 通过Choerodon API上传chart包
+    curl -X POST \
+        -F "token=${Token}" \
+        -F "version=${CI_COMMIT_TAG}" \
+        -F "file=@${FILE_NAME}-${CI_COMMIT_TAG}.tgz" \
+        -F "commit=${CI_COMMIT_SHA}" \
+        -F "image=${DOCKER_REGISTRY}/${GROUP_NAME}/${PROJECT_NAME}:${CI_COMMIT_TAG}" \
+        "${CHOERODON_URL}/devops/ci"
+    # 判断本次上传是否出错
+    if [ $? -ne 0 ]; then
+        echo "upload chart error"
+        exit 1
+    fi
+}
