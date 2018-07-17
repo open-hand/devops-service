7a4d944aa05e190c8e15f698b610d81b7be90f52
devil-scp <changping.shi@hand-china.com>
2018-07-17 16:10:45 +0800

    [ADD] add search by tagname message..

--- src/main/java/io/choerodon/devops/api/controller/v1/DevopsGitController.java
+++ src/main/java/io/choerodon/devops/api/controller/v1/DevopsGitController.java
@@ -93,23 +93,24 @@ public class DevopsGitController {
      *
      * @param projectId     项目ID
      * @param applicationId 应用ID
+     * @param params  查询参数
      * @return null
      */
     @Permission(level = ResourceLevel.PROJECT,
             roles = {InitRoleCode.PROJECT_OWNER, InitRoleCode.PROJECT_MEMBER})
     @ApiOperation(value = "获取标签分页列表")
-    @GetMapping("/tags")
+    @PostMapping("/tags_list_options")
     public ResponseEntity<Page<TagDTO>> getTagByName(
             @ApiParam(value = "项目id", required = true)
             @PathVariable(value = "project_id") Long projectId,
             @ApiParam(value = "应用id", required = true)
             @PathVariable(value = "application_id") Long applicationId,
-            @ApiParam(value = "标记名称", required = false)
-            @RequestParam(value = "tag_name")String tagName,
+            @ApiParam(value = "查询参数")
+            @RequestBody(required = false)String params,
             @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
             @RequestParam(value = "size", required = false, defaultValue = "10") Integer size) {
 
-        return Optional.ofNullable(devopsGitService.getTags(projectId, applicationId,tagName,
+        return Optional.ofNullable(devopsGitService.getTags(projectId, applicationId,params,
                  page, size))
                 .map(result -> new ResponseEntity<>(result, HttpStatus.OK))
                 .orElseThrow(() -> new CommonException("error.tags.get"));
@@ -210,6 +211,7 @@ public class DevopsGitController {
      *
      * @param projectId     项目 ID
      * @param applicationId 应用ID
+     * @param branchName    分支名
      * @return List
      */
     @Permission(level = ResourceLevel.PROJECT,
@@ -220,8 +222,10 @@ public class DevopsGitController {
             @ApiParam(value = "项目id", required = true)
             @PathVariable(value = "project_id") Long projectId,
             @ApiParam(value = "应用id", required = true)
-            @PathVariable(value = "application_id") Long applicationId) {
-        return Optional.ofNullable(devopsGitService.listBranches(projectId, applicationId))
+            @PathVariable(value = "application_id") Long applicationId,
+            @ApiParam(value = "查询参数")
+            @RequestBody(required = false)String params) {
+        return Optional.ofNullable(devopsGitService.listBranches(projectId, applicationId,params))
                 .map(result -> new ResponseEntity<>(result, HttpStatus.OK))
                 .orElseThrow(() -> new CommonException("error.branch.get"));
     }
--- src/main/java/io/choerodon/devops/app/service/impl/DevopsGitServiceImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/DevopsGitServiceImpl.java
@@ -112,7 +112,7 @@ public class DevopsGitServiceImpl implements DevopsGitService {
     }
 
     @Override
-    public List<BranchDTO> listBranches(Long projectId, Long applicationId) {
+    public List<BranchDTO> listBranches(Long projectId, Long applicationId,String params) {
         ProjectE projectE = iamRepository.queryIamProject(projectId);
         ApplicationE applicationE = applicationRepository.query(applicationId);
         Organization organization = iamRepository.queryOrganizationById(projectE.getOrganization().getId());
@@ -198,14 +198,14 @@ public class DevopsGitServiceImpl implements DevopsGitService {
     }
 
     @Override
-    public Page<TagDTO> getTags(Long projectId, Long applicationId, String tagName, Integer page, Integer size) {
+    public Page<TagDTO> getTags(Long projectId, Long applicationId, String params, Integer page, Integer size) {
         ProjectE projectE = iamRepository.queryIamProject(projectId);
         ApplicationE applicationE = applicationRepository.query(applicationId);
         Organization organization = iamRepository.queryOrganizationById(projectE.getOrganization().getId());
         String urlSlash = gitlabUrl.endsWith("/") ? "" : "/";
         String path = String.format("%s%s%s-%s/%s",
                 gitlabUrl, urlSlash, organization.getCode(), projectE.getCode(), applicationE.getCode());
-        return devopsGitRepository.getTags(applicationId, path, page,tagName, size, getGitlabUserId());
+        return devopsGitRepository.getTags(applicationId, path, page,params, size, getGitlabUserId());
     }
 
     @Override
