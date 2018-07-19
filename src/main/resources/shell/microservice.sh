a2e889ec4921484abddece5e4882a680f89ec16f
10980 <zhiyang.zhu@hand-china.com>
2018-07-19 14:20:17 +0800

    [FIX] fix the shell of ci

--- src/main/resources/shell/microservice.sh
+++ src/main/resources/shell/microservice.sh
@@ -2,36 +2,34 @@ export GROUP_NAME={{ GROUP_NAME }}
 export PROJECT_NAME={{ PROJECT_NAME }}
 
 
-CI_COMMIT_YEAR=$(git log -1 --pretty=format:"%cd" --date=format:"%Y")
-CI_COMMIT_MONTH=$(git log -1 --pretty=format:"%cd" --date=format:"%m" | sed s'/^0//')
-CI_COMMIT_DAY=$(git log -1 --pretty=format:"%cd" --date=format:"%d" | sed s'/^0//')
-CI_COMMIT_HOURS=$(git log -1 --pretty=format:"%cd" --date=format:"%H")
-CI_COMMIT_MINUTES=$(git log -1 --pretty=format:"%cd" --date=format:"%M")
-CI_COMMIT_SECONDS=$(git log -1 --pretty=format:"%cd" --date=format:"%S")
-export C7N_GIT_COMMIT_TIME=$CI_COMMIT_YEAR.$CI_COMMIT_MONTH.$CI_COMMIT_DAY-$CI_COMMIT_HOURS$CI_COMMIT_MINUTES$CI_COMMIT_SECONDS
+C7N_COMMIT_YEAR=$(git log -1 --pretty=format:"%cd" --date=format:"%Y")
+C7N_COMMIT_MONTH=$(git log -1 --pretty=format:"%cd" --date=format:"%m" | sed s'/^0//')
+C7N_COMMIT_DAY=$(git log -1 --pretty=format:"%cd" --date=format:"%d" | sed s'/^0//')
+C7N_COMMIT_HOURS=$(git log -1 --pretty=format:"%cd" --date=format:"%H")
+C7N_COMMIT_MINUTES=$(git log -1 --pretty=format:"%cd" --date=format:"%M")
+C7N_COMMIT_SECONDS=$(git log -1 --pretty=format:"%cd" --date=format:"%S")
+export C7N_COMMIT_TIME=$C7N_COMMIT_YEAR.$C7N_COMMIT_MONTH.$C7N_COMMIT_DAY-$C7N_COMMIT_HOURS$C7N_COMMIT_MINUTES$C7N_COMMIT_SECONDS
 
 # 8位sha值
-export C7N_GIT_COMMIT_SHA=$(git log -1 --pretty=format:"%H" | awk '{print substr($1,1,8)}')
+export C7N_COMMIT_SHA=$(git log -1 --pretty=format:"%H" | awk '{print substr($1,1,8)}')
 
 # 分支名
 if [ $CIRCLECI ]; then
-  export C7N_GIT_BRANCH=$CIRCLE_BRANCH
-fi
-
-if [ $GITLAB_CI ]; then
-  export C7N_GIT_BRANCH=$CI_COMMIT_REF_NAME
+  export C7N_BRANCH=$CIRCLE_BRANCH
+elif [ $GITLAB_CI ]; then
+  export C7N_BRANCH=$CI_COMMIT_REF_NAME
 fi
 
 # 默认Version
 if [ $CI_COMMIT_TAG ]; then
-    export C7N_GITVERSION=$CI_COMMIT_TAG
+    export C7N_VERSION=$CI_COMMIT_TAG
 elif [ $CIRCLE_TAG ]; then
-    export C7N_GITVERSION=$CIRCLE_TAG
+    export C7N_VERSION=$CIRCLE_TAG
 else
-    export C7N_GITVERSION=$C7N_GIT_COMMIT_TIME-$C7N_GIT_BRANCH
+    export C7N_VERSION=$C7N_COMMIT_TIME-$C7N_BRANCH
 fi
 
-export CI_COMMIT_TAG=$C7N_GITVERSION
+export CI_COMMIT_TAG=C7N_VERSION
 
 function database_test(){
     while ! mysqlcheck --host=127.0.0.1 --user=root --password=${MYSQL_ROOT_PASSWORD} mysql; do sleep 1; done
