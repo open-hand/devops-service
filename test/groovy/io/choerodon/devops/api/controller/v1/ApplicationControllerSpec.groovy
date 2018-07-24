8871ea377548493a851f916815eb58e087c32ab7
10980 <zhiyang.zhu@hand-china.com>
2018-07-24 13:37:34 +0800

    [ADD] init unit test

---
+++ test/groovy/io/choerodon/devops/api/controller/v1/ApplicationControllerSpec.groovy
@@ -0,0 +1,47 @@
+package io.choerodon.devops.api.controller.v1
+
+import spock.lang.Specification
+
+class ApplicationControllerSpec extends Specification {
+    def "Create"() {
+    }
+
+    def "QueryByAppId"() {
+    }
+
+    def "Update"() {
+    }
+
+    def "QueryByAppIdAndActive"() {
+    }
+
+    def "PageByOptions"() {
+    }
+
+    def "PageByEnvIdAndStatus"() {
+    }
+
+    def "ListByEnvIdAndStatus"() {
+    }
+
+    def "ListByActive"() {
+    }
+
+    def "ListAll"() {
+    }
+
+    def "CheckName"() {
+    }
+
+    def "CheckCode"() {
+    }
+
+    def "ListTemplate"() {
+    }
+
+    def "ListByActiveAndPubAndVersion"() {
+    }
+
+    def "ListCodeRepository"() {
+    }
+}
---
+++ test/groovy/io/choerodon/devops/api/controller/v1/ApplicationTemplateControllerSpec.groovy
@@ -0,0 +1,29 @@
+package io.choerodon.devops.api.controller.v1
+
+import spock.lang.Specification
+
+class ApplicationTemplateControllerSpec extends Specification {
+    def "Create"() {
+    }
+
+    def "Update"() {
+    }
+
+    def "Delete"() {
+    }
+
+    def "QueryByAppTemplateId"() {
+    }
+
+    def "ListByOptions"() {
+    }
+
+    def "ListByOrgId"() {
+    }
+
+    def "CheckName"() {
+    }
+
+    def "CheckCode"() {
+    }
+}
