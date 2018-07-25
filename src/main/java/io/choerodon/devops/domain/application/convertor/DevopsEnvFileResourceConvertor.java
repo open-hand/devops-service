d5c8dfece7b77afb4591b8ea274ec7864fd3b8c0
Runge <fengrun.huang@hand-china.com>
2018-07-25 17:23:39 +0800

    [ADD] add repository

---
+++ src/main/java/io/choerodon/devops/domain/application/convertor/DevopsEnvFileResourceConvertor.java
@@ -0,0 +1,37 @@
+package io.choerodon.devops.domain.application.convertor;
+
+import org.springframework.beans.BeanUtils;
+
+import io.choerodon.core.convertor.ConvertorI;
+import io.choerodon.devops.domain.application.entity.DevopsEnvFileResourceE;
+import io.choerodon.devops.domain.application.entity.DevopsEnvironmentE;
+import io.choerodon.devops.infra.dataobject.DevopsEnvFileResourceDO;
+
+/**
+ * Creator: Runge
+ * Date: 2018/7/25
+ * Time: 17:09
+ * Description:
+ */
+public class DevopsEnvFileResourceConvertor implements ConvertorI<DevopsEnvFileResourceE, DevopsEnvFileResourceDO, Object> {
+
+    @Override
+    public DevopsEnvFileResourceE doToEntity(DevopsEnvFileResourceDO devopsEnvFileResourceDO) {
+        DevopsEnvFileResourceE devopsEnvFileResourceE = new DevopsEnvFileResourceE();
+        BeanUtils.copyProperties(devopsEnvFileResourceDO, devopsEnvFileResourceE);
+        DevopsEnvironmentE environmentE = new DevopsEnvironmentE();
+        environmentE.setId(devopsEnvFileResourceDO.getId());
+        devopsEnvFileResourceE.setEnvironment(environmentE);
+        return devopsEnvFileResourceE;
+    }
+
+    @Override
+    public DevopsEnvFileResourceDO entityToDo(DevopsEnvFileResourceE devopsEnvFileResourceE) {
+        DevopsEnvFileResourceDO devopsEnvFileResourceDO = new DevopsEnvFileResourceDO();
+        BeanUtils.copyProperties(devopsEnvFileResourceE, devopsEnvFileResourceDO);
+        if (devopsEnvFileResourceE.getEnvironment() != null) {
+            devopsEnvFileResourceDO.setEnvId(devopsEnvFileResourceE.getEnvironment().getId());
+        }
+        return devopsEnvFileResourceDO;
+    }
+}
