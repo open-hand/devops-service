411150bcee9e4a5d8de8bdc7e8c51a4edc09cbbb
Runge <fengrun.huang@hand-china.com>
2018-07-27 10:21:36 +0800

    [ADD] add component & imp msg

--- src/main/java/io/choerodon/devops/api/eventhandler/DevopsSagaHandler.java
+++ src/main/java/io/choerodon/devops/api/eventhandler/DevopsSagaHandler.java
@@ -1,10 +1,13 @@
 package io.choerodon.devops.api.eventhandler;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Creator: Runge
  * Date: 2018/7/27
  * Time: 10:06
- * Description: Saga msg by devops self
+ * Description: Saga msg by DevOps self
  */
+@Component
 public class DevopsSagaHandler {
 }
--- src/main/java/io/choerodon/devops/api/eventhandler/SagaHandler.java
+++ src/main/java/io/choerodon/devops/api/eventhandler/SagaHandler.java
@@ -1,10 +1,13 @@
 package io.choerodon.devops.api.eventhandler;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Creator: Runge
  * Date: 2018/7/27
  * Time: 10:06
- * Description:
+ * Description: External saga msg
  */
+@Component
 public class SagaHandler {
 }
