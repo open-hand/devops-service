9f8c5ac7089a898ce73e122848bed2a23d769e6d
Runge <fengrun.huang@hand-china.com>
2018-07-27 15:32:39 +0800

    [FIX] fix db

--- src/main/resources/script/db/devops_env.groovy
+++ src/main/resources/script/db/devops_env.groovy
@@ -50,12 +50,12 @@ databaseChangeLog(logicalFilePath: 'dba/devops_env.groovy') {
     }
 
     changeSet(author: 'younger', id: '2018-07-25-add-column')
-            {
-                addColumn(tableName: 'devops_env') {
-                    column(name: 'gitlab_env_project_id', type: 'BIGINT UNSIGNED', remarks: 'gitlab env project id', afterColumn: 'project_id')
-                    column(name: 'env_id_rsa', type: 'varchar(5000)', remarks: 'ssh id rsa ', afterColumn: 'token')
-                    column(name: 'env_id_rsa_pub', type: 'varchar(5000)', remarks: 'ssh id rsa pub', afterColumn: 'env_id_rsa')
-                }
-                column(name: 'hook_id', type: 'BIGINT UNSIGNED', remarks: 'gitlab webhook', afterColumn: 'gitlab_env_project_id')
-            }
+    {
+        addColumn(tableName: 'devops_env') {
+            column(name: 'gitlab_env_project_id', type: 'BIGINT UNSIGNED', remarks: 'gitlab env project id', afterColumn: 'project_id')
+            column(name: 'env_id_rsa', type: 'varchar(5000)', remarks: 'ssh id rsa ', afterColumn: 'token')
+            column(name: 'env_id_rsa_pub', type: 'varchar(5000)', remarks: 'ssh id rsa pub', afterColumn: 'env_id_rsa')
+            column(name: 'hook_id', type: 'BIGINT UNSIGNED', remarks: 'gitlab webhook', afterColumn: 'gitlab_env_project_id')
+        }
+    }
 }
