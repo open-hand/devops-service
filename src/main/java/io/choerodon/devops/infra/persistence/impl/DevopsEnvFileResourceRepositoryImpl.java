da2f0f6a773da681d66d70ead9daa821d5adf6b7
Runge <fengrun.huang@hand-china.com>
2018-07-31 16:06:04 +0800

    [ADD] add CRUD

--- src/main/java/io/choerodon/devops/infra/persistence/impl/DevopsEnvFileResourceRepositoryImpl.java
+++ src/main/java/io/choerodon/devops/infra/persistence/impl/DevopsEnvFileResourceRepositoryImpl.java
@@ -1,7 +1,12 @@
 package io.choerodon.devops.infra.persistence.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import io.choerodon.core.convertor.ConvertHelper;
 import io.choerodon.devops.domain.application.entity.DevopsEnvFileResourceE;
 import io.choerodon.devops.domain.application.repository.DevopsEnvFileResourceRepository;
+import io.choerodon.devops.infra.dataobject.DevopsEnvFileResourceDO;
+import io.choerodon.devops.infra.mapper.DevopsEnvFileResourceMapper;
 
 /**
  * Creator: Runge
@@ -10,23 +15,34 @@ import io.choerodon.devops.domain.application.repository.DevopsEnvFileResourceRe
  * Description:
  */
 public class DevopsEnvFileResourceRepositoryImpl implements DevopsEnvFileResourceRepository {
+
+    @Autowired
+    private DevopsEnvFileResourceMapper devopsEnvFileResourceMapper;
+
     @Override
     public DevopsEnvFileResourceE createFileResource(DevopsEnvFileResourceE devopsEnvFileResourceE) {
-        return null;
+        DevopsEnvFileResourceDO devopsEnvFileResourceDO =
+                ConvertHelper.convert(devopsEnvFileResourceE, DevopsEnvFileResourceDO.class);
+        return ConvertHelper.convert(devopsEnvFileResourceDO, DevopsEnvFileResourceE.class);
     }
 
     @Override
     public DevopsEnvFileResourceE getFileResource(Long fileResourceId) {
-        return null;
+        return ConvertHelper.convert(
+                devopsEnvFileResourceMapper.selectByPrimaryKey(fileResourceId),
+                DevopsEnvFileResourceE.class);
     }
 
     @Override
     public DevopsEnvFileResourceE updateFileResource(DevopsEnvFileResourceE devopsEnvFileResourceE) {
-        return null;
+        DevopsEnvFileResourceDO devopsEnvFileResourceDO =
+                ConvertHelper.convert(devopsEnvFileResourceE, DevopsEnvFileResourceDO.class);
+        devopsEnvFileResourceMapper.updateByPrimaryKeySelective(devopsEnvFileResourceDO);
+        return devopsEnvFileResourceE;
     }
 
     @Override
     public void deleteFileResource(Long fileResourceId) {
-
+        devopsEnvFileResourceMapper.deleteByPrimaryKey(fileResourceId);
     }
 }
