16614159b068d0f0b49395ff8aa425affad25dd1
Runge <fengrun.huang@hand-china.com>
2018-08-01 16:54:27 +0800

    [IMP] combine create project

--- src/main/java/io/choerodon/devops/api/eventhandler/SagaHandler.java
+++ src/main/java/io/choerodon/devops/api/eventhandler/SagaHandler.java
@@ -5,11 +5,16 @@ import java.io.IOException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import io.choerodon.core.saga.SagaTask;
+import io.choerodon.devops.app.service.GitlabGroupService;
+import io.choerodon.devops.app.service.HarborService;
 import io.choerodon.devops.app.service.ProjectService;
+import io.choerodon.devops.domain.application.event.GitlabGroupPayload;
+import io.choerodon.devops.domain.application.event.HarborPayload;
 import io.choerodon.devops.domain.application.event.ProjectEvent;
 
 /**
@@ -26,6 +31,10 @@ public class SagaHandler {
 
     @Autowired
     private ProjectService projectService;
+    @Autowired
+    private GitlabGroupService gitlabGroupService;
+    @Autowired
+    private HarborService harborService;
 
     private void loggerInfo(Object o) {
         LOGGER.info("data: {}", o);
@@ -37,10 +46,62 @@ public class SagaHandler {
     @SagaTask(code = "devopsCreateProject",
             description = "devops创建项目",
             sagaCode = "iam-create-project",
-            seq = 2)
-    public void handleProjectCreateEvent(String msg) throws IOException {
+            seq = 1)
+    public String handleProjectCreateEvent(String msg) throws IOException {
         ProjectEvent projectEvent = objectMapper.readValue(msg, ProjectEvent.class);
         loggerInfo(projectEvent);
         projectService.createProject(projectEvent);
+        return msg;
     }
+
+    /**
+     * 创建组事件
+     */
+    @SagaTask(code = "devopsCreateGitLabGroup",
+            description = "devops 创建 GitLab Group",
+            sagaCode = "iam-create-project",
+            seq = 2)
+    public String handleGitlabGroupEvent(String msg) throws IOException {
+        ProjectEvent projectEvent = objectMapper.readValue(msg, ProjectEvent.class);
+        GitlabGroupPayload gitlabGroupPayload = new GitlabGroupPayload();
+        BeanUtils.copyProperties(projectEvent, gitlabGroupPayload);
+        loggerInfo(gitlabGroupPayload);
+        gitlabGroupService.createGroup(gitlabGroupPayload, "");
+        return msg;
+    }
+
+    /**
+     * 创建组事件
+     */
+    @SagaTask(code = "devopsCreateGitOpsGroup",
+            description = "devops 创建 GitOps Group",
+            sagaCode = "iam-create-project",
+            seq = 2)
+    public String handleGitOpsGroupEvent(String msg) throws IOException {
+        ProjectEvent projectEvent = objectMapper.readValue(msg, ProjectEvent.class);
+        GitlabGroupPayload gitlabGroupPayload = new GitlabGroupPayload();
+        BeanUtils.copyProperties(projectEvent, gitlabGroupPayload);
+        loggerInfo(gitlabGroupPayload);
+        gitlabGroupService.createGroup(gitlabGroupPayload, "-gitops");
+        return msg;
+    }
+
+    /**
+     * 创建harbor项目事件
+     */
+    @SagaTask(code = "devopsCreateHarbor",
+            description = "devops 创建 Harbor",
+            sagaCode = "iam-create-project",
+            seq = 2)
+    public String handleHarborEvent(String msg) throws IOException {
+        ProjectEvent projectEvent = objectMapper.readValue(msg, ProjectEvent.class);
+        HarborPayload harborPayload = new HarborPayload(
+                projectEvent.getProjectId(),
+                projectEvent.getOrganizationCode() + "-" + projectEvent.getProjectCode()
+        );
+        loggerInfo(harborPayload);
+        harborService.createHarbor(harborPayload);
+        return msg;
+    }
+
 }
--- src/main/java/io/choerodon/devops/app/service/impl/ProjectServiceImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/ProjectServiceImpl.java
@@ -1,19 +1,10 @@
 package io.choerodon.devops.app.service.impl;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import io.choerodon.asgard.saga.SagaClient;
-import io.choerodon.asgard.saga.dto.StartInstanceDTO;
-import io.choerodon.core.exception.CommonException;
-import io.choerodon.core.saga.Saga;
 import io.choerodon.devops.app.service.ProjectService;
-import io.choerodon.devops.domain.application.event.GitlabGroupPayload;
-import io.choerodon.devops.domain.application.event.HarborPayload;
 import io.choerodon.devops.domain.application.event.ProjectEvent;
 import io.choerodon.devops.domain.application.repository.DevopsProjectRepository;
 import io.choerodon.devops.infra.dataobject.DevopsProjectDO;
@@ -28,18 +19,14 @@ import io.choerodon.devops.infra.dataobject.DevopsProjectDO;
 @Component
 public class ProjectServiceImpl implements ProjectService {
 
-    private ObjectMapper objectMapper = new ObjectMapper();
-
     @Value("${spring.application.name}")
     private String applicationName;
 
     private DevopsProjectRepository devopsProjectRepository;
-    private final SagaClient sagaClient;
 
     @Autowired
-    public ProjectServiceImpl(DevopsProjectRepository devopsProjectRepository, SagaClient sagaClient) {
+    public ProjectServiceImpl(DevopsProjectRepository devopsProjectRepository) {
         this.devopsProjectRepository = devopsProjectRepository;
-        this.sagaClient = sagaClient;
     }
 
     @Override
@@ -47,40 +34,6 @@ public class ProjectServiceImpl implements ProjectService {
         // create project in db
         DevopsProjectDO devopsProject = new DevopsProjectDO(projectEvent.getProjectId());
         devopsProjectRepository.createProject(devopsProject);
-
-        createGitlabGroupEvent(projectEvent);
-        createHarborEvent(projectEvent);
-    }
-
-    @Saga(code = "devops-create-gitlab-group", description = "创建gitlab group", inputSchema = "{}")
-    private void createGitlabGroupEvent(ProjectEvent projectEvent) {
-        GitlabGroupPayload gitlabGroupPayload = new GitlabGroupPayload();
-        BeanUtils.copyProperties(projectEvent, gitlabGroupPayload);
-
-        try {
-            String input = objectMapper.writeValueAsString(gitlabGroupPayload);
-            sagaClient.startSaga(
-                    "devops-create-gitlab-group",
-                    new StartInstanceDTO(input, projectEvent.getUserId(), "", ""));
-        } catch (JsonProcessingException e) {
-            throw new CommonException("error.SagaProducer.devops.createGitLabGroup");
-        }
-    }
-
-    @Saga(code = "devops-create-harbor-project", description = "创建 harbor project", inputSchema = "{}")
-    private void createHarborEvent(ProjectEvent projectEvent) {
-        HarborPayload harborPayload = new HarborPayload(
-                projectEvent.getProjectId(),
-                projectEvent.getOrganizationCode() + "-" + projectEvent.getProjectCode()
-        );
-        try {
-            String input = objectMapper.writeValueAsString(harborPayload);
-            sagaClient.startSaga(
-                    "devops-create-harbor-project",
-                    new StartInstanceDTO(input, projectEvent.getUserId(), "", ""));
-        } catch (JsonProcessingException e) {
-            throw new CommonException("error.SagaProducer.devops.createHarbor");
-        }
     }
 
     @Override
