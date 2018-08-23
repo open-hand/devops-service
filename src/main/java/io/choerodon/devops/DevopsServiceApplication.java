0e6112bd2d5a6e41c0fd45ff88cc942fcd45a33a
crockitwood <crockitwood@gmail.com>
2018-08-23 20:07:53 +0800

    [FIX] add enable async
    Signed-off-by: crockitwood <crockitwood@gmail.com>

--- src/main/java/io/choerodon/devops/DevopsServiceApplication.java
+++ src/main/java/io/choerodon/devops/DevopsServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import io.choerodon.resource.annoation.EnableChoerodonResourceServer;
@@ -13,6 +14,7 @@ import io.choerodon.resource.annoation.EnableChoerodonResourceServer;
 @EnableScheduling
 @SpringBootApplication
 @EnableChoerodonResourceServer
+@EnableAsync
 public class DevopsServiceApplication {
 
     public static void main(String[] args) {
