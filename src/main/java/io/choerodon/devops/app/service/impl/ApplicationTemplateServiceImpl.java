c92137caf2832a286062b48c522d95238fa59f07
Runge <fengrun.huang@hand-china.com>
2018-09-05 16:43:51 +0800

    [FIX] template permission error

--- src/main/java/io/choerodon/devops/app/service/impl/ApplicationTemplateServiceImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/ApplicationTemplateServiceImpl.java
@@ -187,7 +187,7 @@ public class ApplicationTemplateServiceImpl implements ApplicationTemplateServic
     public void operationApplicationTemplate(GitlabProjectPayload gitlabProjectPayload) {
         GitlabProjectDO gitlabProjectDO = gitlabRepository.createProject(gitlabProjectPayload.getGroupId(),
                 gitlabProjectPayload.getPath(),
-                gitlabProjectPayload.getUserId(), false);
+                gitlabProjectPayload.getUserId(), true);
         gitlabProjectPayload.setGitlabProjectId(gitlabProjectDO.getId());
 
 
