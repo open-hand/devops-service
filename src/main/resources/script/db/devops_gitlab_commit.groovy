cf113279aadcf76350a2e03f5e54ceaafee55ae6
n1ck <lei.zhang14@hand-china.com>
2018-09-25 21:06:37 +0800

    [IMP] modify groovy

--- src/main/resources/script/db/devops_gitlab_commit.groovy
+++ src/main/resources/script/db/devops_gitlab_commit.groovy
@@ -27,4 +27,9 @@ databaseChangeLog(logicalFilePath: 'db/devops_gitlab_commit.groovy') {
             column(name: 'app_name', type: 'VARCHAR(128)', remarks: '应用名称', afterColumn: 'commit_date')
         }
     }
+    changeSet(author: 'n1ck', id: '2018-09-25-add-column') {
+        addColumn(tableName: 'devops_gitlab_commit') {
+            column(name: 'url', type: 'VARCHAR(512)', remarks: 'commit url', afterColumn: 'app_name')
+        }
+    }
 }
