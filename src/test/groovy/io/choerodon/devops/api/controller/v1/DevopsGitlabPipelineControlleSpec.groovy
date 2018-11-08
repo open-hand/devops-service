41ea01c96f4a7bb1a286011d921813845f8e21e7
n1ck <lei.zhang14@hand-china.com>
2018-11-08 20:46:58 +0800

    [IMP] gitlab pipeline unit test

--- src/test/groovy/io/choerodon/devops/api/controller/v1/DevopsGitlabPipelineControlleSpec.groovy
+++ src/test/groovy/io/choerodon/devops/api/controller/v1/DevopsGitlabPipelineControlleSpec.groovy
@@ -5,32 +5,37 @@ import io.choerodon.devops.IntegrationTestConfiguration
 import io.choerodon.devops.api.dto.PipelineFrequencyDTO
 import io.choerodon.devops.api.dto.PipelineTimeDTO
 import io.choerodon.devops.domain.application.entity.ProjectE
-import io.choerodon.devops.domain.application.entity.iam.UserE
 import io.choerodon.devops.domain.application.repository.IamRepository
 import io.choerodon.devops.domain.application.valueobject.Organization
 import io.choerodon.devops.infra.dataobject.ApplicationDO
 import io.choerodon.devops.infra.dataobject.DevopsGitlabPipelineDO
+import io.choerodon.devops.infra.dataobject.iam.OrganizationDO
+import io.choerodon.devops.infra.dataobject.iam.ProjectDO
+import io.choerodon.devops.infra.dataobject.iam.UserDO
+import io.choerodon.devops.infra.feign.IamServiceClient
 import io.choerodon.devops.infra.mapper.ApplicationMapper
 import io.choerodon.devops.infra.mapper.DevopsGitlabCommitMapper
 import io.choerodon.devops.infra.mapper.DevopsGitlabPipelineMapper
+import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.context.annotation.Import
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Stepwise
+import spock.lang.Subject
 
+import static org.mockito.Matchers.any
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Import(IntegrationTestConfiguration)
+@Subject(DevopsGitlabPipelineController)
 @Stepwise
 class DevopsGitlabPipelineControlleSpec extends Specification {
-
-
-    private static flag = 0
-
 
     @Autowired
     private TestRestTemplate restTemplate
@@ -43,80 +48,85 @@ class DevopsGitlabPipelineControlleSpec extends Specification {
     private ApplicationMapper applicationMapper
 
     @Autowired
-    @Qualifier("mockIamRepository")
     private IamRepository iamRepository
 
-    void setup() {
-        if (flag == 0) {
+    IamServiceClient iamServiceClient = Mockito.mock(IamServiceClient.class)
 
-            ApplicationDO applicationDO = new ApplicationDO()
-            applicationDO.setId(1L)
-            applicationDO.setProjectId(1L)
-            applicationDO.setActive(true)
-            applicationDO.setCode("test")
-            applicationDO.setName("test")
-            applicationDO.setGitlabProjectId(1)
-            applicationMapper.insert(applicationDO)
+    @Shared
+    ApplicationDO applicationDO = new ApplicationDO()
+    @Shared
+    DevopsGitlabPipelineDO devopsGitlabPipelineDO = new DevopsGitlabPipelineDO()
 
+    def setupSpec() {
+        applicationDO.setId(1L)
+        applicationDO.setProjectId(1L)
+        applicationDO.setActive(true)
+        applicationDO.setCode("test")
+        applicationDO.setName("test")
+        applicationDO.setGitlabProjectId(1)
 
-            DevopsGitlabPipelineDO devopsGitlabPipelineDO = new DevopsGitlabPipelineDO()
-            devopsGitlabPipelineDO.setAppId(1L)
-            devopsGitlabPipelineDO.setPipelineId(1L)
-            devopsGitlabPipelineDO.setPipelineCreateUserId(1L)
-            devopsGitlabPipelineDO.setCommitId(1L)
-            devopsGitlabPipelineDO.setPipelineCreationDate(new Date())
-            devopsGitlabPipelineDO.setStatus("passed")
-            devopsGitlabPipelineMapper.insert(devopsGitlabPipelineDO);
+        devopsGitlabPipelineDO.setAppId(1L)
+        devopsGitlabPipelineDO.setPipelineId(1L)
+        devopsGitlabPipelineDO.setPipelineCreateUserId(1L)
+        devopsGitlabPipelineDO.setCommitId(1L)
+        devopsGitlabPipelineDO.setPipelineCreationDate(new Date())
+        devopsGitlabPipelineDO.setStatus("passed")
+    }
 
-            flag = 1
-        }
+    def setup() {
+        iamRepository.initMockIamService(iamServiceClient)
 
+        ProjectDO projectDO = new ProjectDO()
+        projectDO.setId(1L)
+        projectDO.setCode("pro")
+        projectDO.setOrganizationId(1L)
+        ResponseEntity<ProjectDO> responseEntity = new ResponseEntity<>(projectDO, HttpStatus.OK)
+        Mockito.doReturn(responseEntity).when(iamServiceClient).queryIamProject(1L)
+
+        OrganizationDO organizationDO = new OrganizationDO()
+        organizationDO.setId(1L)
+        organizationDO.setCode("org")
+        ResponseEntity<OrganizationDO> responseEntity1 = new ResponseEntity<>(organizationDO, HttpStatus.OK)
+        Mockito.doReturn(responseEntity1).when(iamServiceClient).queryOrganizationById(1L)
+
+        UserDO userDO = new UserDO()
+        userDO.setLoginName("test")
+        userDO.setId(1L)
+        List<UserDO> userDOList = new ArrayList<>()
+        userDOList.add(userDO)
+        ResponseEntity<List<UserDO>> responseEntity3 = new ResponseEntity<>(userDOList, HttpStatus.OK)
+        Mockito.when(iamServiceClient.listUsersByIds(any(Long[].class))).thenReturn(responseEntity3)
     }
 
     def "ListPipelineTime"() {
-        given:
+        given: '初始化数据'
+        applicationMapper.insert(applicationDO)
+        devopsGitlabPipelineMapper.insert(devopsGitlabPipelineDO);
 
-        when:
+        when: '获取pipeline时长报表'
         def pipelineTimeDTO = restTemplate.getForObject("/v1/projects/1/pipeline/time?appId=1&startTime=2015/10/12&endTime=3018/10/18", PipelineTimeDTO.class)
 
-        then:
+        then: '校验返回值'
         pipelineTimeDTO.getRefs().size() != 0
     }
 
     def "ListPipelineFrequency"() {
-        given:
-
-        when:
+        when: '获取pipeline次数报表'
         def pipelineFrequencyDTO = restTemplate.getForObject("/v1/projects/1/pipeline/frequency?appId=1&startTime=2015/10/12&endTime=3018/10/18", PipelineFrequencyDTO.class)
 
-        then:
+        then: '校验返回值'
         pipelineFrequencyDTO.getPipelineFrequencys().size() != 0
     }
 
     def "PagePipeline"() {
-        given:
-        Organization organization = initOrg(1L, "testOrganization")
-        ProjectE projectE = initProj(1L, "testProject", organization)
-        UserE userE = new UserE()
-        userE.setLoginName("test")
-        userE.setId(1L)
-        userE.setRealName("test")
-        userE.setImageUrl("test")
-        iamRepository.queryIamProject(_ as Long) >> projectE
-        iamRepository.queryOrganizationById(_ as Long) >> organization
-        2 * iamRepository.queryUserByUserId(_) >> userE
-
-
-        when:
+        when: '分页获取pipeline'
         def pages = restTemplate.getForObject("/v1/projects/384/pipeline/page?appId=1&startTime=2015/10/13&endTime=3018/10/19&page=0&size=10", Page.class)
 
-        then:
+        then: '校验返回值'
         pages.size() == 1
         applicationMapper.deleteByPrimaryKey(1L)
         devopsGitlabPipelineMapper.deleteByPrimaryKey(1L)
-
     }
-
 
     private static Organization initOrg(Long id, String code) {
         Organization organization = new Organization()
