7e7dea8db5f5070946720281a215a945d628f3bc
zmf <1984654893@qq.com>
2018-12-19 17:10:23 +0800

    [ADD] 环境实例获取相关资源只获取chart包定义的资源

--- src/main/java/io/choerodon/devops/app/service/DevopsEnvResourceService.java
+++ src/main/java/io/choerodon/devops/app/service/DevopsEnvResourceService.java
@@ -11,5 +11,14 @@ import io.choerodon.devops.api.dto.InstanceEventDTO;
 public interface DevopsEnvResourceService {
     DevopsEnvResourceDTO listResources(Long instanceId);
 
+    /**
+     * 相较于{@link DevopsEnvResourceService#listResources(Long)} 方法只展示该应用的chart包中定义的资源，
+     * 不包含前端页面之后创建的资源
+     *
+     * @param instanceId 实例id
+     * @return 返回实例的资源
+     */
+    DevopsEnvResourceDTO listResourcesInHelmRelease(Long instanceId);
+
     List<InstanceEventDTO> listInstancePodEvent(Long instanceId);
 }
