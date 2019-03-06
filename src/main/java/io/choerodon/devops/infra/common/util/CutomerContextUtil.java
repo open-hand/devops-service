40cda6297947d070f258481c8bfcfd4791eabea3
devil-scp <changpingshi0213@gmail.com>
2019-03-06 11:31:58 +0800

    [IMP] 修复设置上下文用户

--- src/main/java/io/choerodon/devops/infra/common/util/CutomerContextUtil.java
+++ src/main/java/io/choerodon/devops/infra/common/util/CutomerContextUtil.java
@@ -1,10 +1,18 @@
 package io.choerodon.devops.infra.common.util;
 
+import io.choerodon.asgard.saga.consumer.MockHttpServletRequest;
 import io.choerodon.core.exception.CommonException;
 import io.choerodon.core.oauth.CustomUserDetails;
 import io.choerodon.core.oauth.DetailsHelper;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+
+import java.util.Collections;
+import java.util.HashMap;
 
 /**
  * Creator: ChangpingShi0213@gmail.com
@@ -19,9 +27,15 @@ public class CutomerContextUtil {
             CustomUserDetails customUserDetails = DetailsHelper.getUserDetails();
             customUserDetails.setUserId(uesrId);
 
-            OAuth2Authentication user = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
-            user.setDetails(customUserDetails);
-            SecurityContextHolder.getContext().setAuthentication(user);
+            Authentication user = new UsernamePasswordAuthenticationToken("default", "N/A", Collections.emptyList());
+            OAuth2Request request = new OAuth2Request(new HashMap<>(0), "", Collections.emptyList(), true,
+                    Collections.emptySet(), Collections.emptySet(), null, null, null);
+            OAuth2Authentication authentication = new OAuth2Authentication(request, user);
+            OAuth2AuthenticationDetails oAuth2AuthenticationDetails = new OAuth2AuthenticationDetails(new MockHttpServletRequest());
+            oAuth2AuthenticationDetails.setDecodedDetails(customUserDetails);
+            authentication.setDetails(oAuth2AuthenticationDetails);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
         } catch (Exception e) {
             throw new CommonException("change user expection", e);
         }
