7a0ab546f4bb2adbca6098d25280993089bc7b4e
devil-scp <changpingshi0213@gmail.com>
2019-04-02 20:42:57 +0800

    [IMP] 升级spring boot修改测试

--- src/main/java/io/choerodon/devops/DevopsServiceApplication.java
+++ src/main/java/io/choerodon/devops/DevopsServiceApplication.java
@@ -2,7 +2,6 @@ package io.choerodon.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
