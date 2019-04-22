d0ddb15a74259d8f8d46f0eec5b9d494dd7cd40d
devil-scp <changpingshi0213@gmail.com>
2019-04-22 16:49:50 +0800

    [IMP] 修复循环依赖问题

--- src/main/java/io/choerodon/devops/infra/common/util/EnvUtil.java
+++ src/main/java/io/choerodon/devops/infra/common/util/EnvUtil.java
@@ -19,6 +19,7 @@ import io.choerodon.websocket.helper.EnvListener;
 import io.choerodon.websocket.helper.EnvSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 /**
@@ -42,6 +43,7 @@ public class EnvUtil {
     @Autowired
     private GitUtil gitUtil;
     @Autowired(required = false)
+    @Lazy
     private EnvListener envListener;
 
 
