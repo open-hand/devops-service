24421836239f12ebd41bece86d90419988547edd
10980 <zhiyang.zhu@hand-china.com>
2019-04-24 10:58:23 +0800

    “【[FIX] fix create secret

--- src/main/java/io/choerodon/devops/domain/service/impl/ConvertC7nSecretServiceImpl.java
+++ src/main/java/io/choerodon/devops/domain/service/impl/ConvertC7nSecretServiceImpl.java
@@ -9,10 +9,10 @@ import io.choerodon.devops.domain.application.entity.DevopsSecretE;
 import io.choerodon.devops.domain.application.handler.GitOpsExplainException;
 import io.choerodon.devops.domain.application.repository.DevopsEnvFileResourceRepository;
 import io.choerodon.devops.domain.application.repository.DevopsSecretRepository;
-import io.choerodon.devops.domain.application.valueobject.C7nSecret;
 import io.choerodon.devops.domain.service.ConvertK8sObjectService;
 import io.choerodon.devops.infra.common.util.TypeUtil;
 import io.choerodon.devops.infra.common.util.enums.GitOpsObjectError;
+import io.kubernetes.client.models.V1Secret;
 
 /**
  * Created by n!Ck
@@ -20,7 +20,7 @@ import io.choerodon.devops.infra.common.util.enums.GitOpsObjectError;
  * Time: 下午7:31
  * Description:
  */
-public class ConvertC7nSecretServiceImpl extends ConvertK8sObjectService<C7nSecret> {
+public class ConvertC7nSecretServiceImpl extends ConvertK8sObjectService<V1Secret> {
 
     private DevopsSecretRepository devopsSecretRepository;
     private DevopsEnvFileResourceRepository devopsEnvFileResourceRepository;
@@ -32,36 +32,36 @@ public class ConvertC7nSecretServiceImpl extends ConvertK8sObjectService<C7nSecr
     }
 
     @Override
-    public void checkParameters(C7nSecret c7nSecret, Map<String, String> objectPath) {
-        String filePath = objectPath.get(TypeUtil.objToString(c7nSecret.hashCode()));
-        if (c7nSecret.getApiVersion() == null) {
+    public void checkParameters(V1Secret v1Secret, Map<String, String> objectPath) {
+        String filePath = objectPath.get(TypeUtil.objToString(v1Secret.hashCode()));
+        if (v1Secret.getApiVersion() == null) {
             throw new GitOpsExplainException(GitOpsObjectError.SECRET_API_VERSION_NOT_FOUND.getError(), filePath);
         } else {
-            if (c7nSecret.getMetadata().getName() == null) {
+            if (v1Secret.getMetadata().getName() == null) {
                 throw new GitOpsExplainException(GitOpsObjectError.SECRET_NAME_NOT_FOUND.getError(), filePath);
             }
         }
-        if (c7nSecret.getStringData() == null) {
+        if (v1Secret.getStringData() == null) {
             throw new GitOpsExplainException(GitOpsObjectError.SECRET_DATA_NOT_FOUND.getError(), filePath);
         }
     }
 
     @Override
-    public void checkIfExist(List<C7nSecret> c7nSecrets, Long envId, List<DevopsEnvFileResourceE> beforeSyncDelete,
-                             Map<String, String> objectPath, C7nSecret c7nSecret) {
-        String filePath = objectPath.get(TypeUtil.objToString(c7nSecret.hashCode()));
-        String secretName = c7nSecret.getMetadata().getName();
+    public void checkIfExist(List<V1Secret> c7nSecrets, Long envId, List<DevopsEnvFileResourceE> beforeSyncDelete,
+                             Map<String, String> objectPath, V1Secret v1Secret) {
+        String filePath = objectPath.get(TypeUtil.objToString(v1Secret.hashCode()));
+        String secretName = v1Secret.getMetadata().getName();
         DevopsSecretE devopsSecretE = devopsSecretRepository.selectByEnvIdAndName(envId, secretName);
         if (devopsSecretE != null) {
             Long secretId = devopsSecretE.getId();
             if (beforeSyncDelete.stream()
                     .filter(devopsEnvFileResourceE -> devopsEnvFileResourceE.getResourceType()
-                            .equals(c7nSecret.getKind()))
+                            .equals(v1Secret.getKind()))
                     .noneMatch(devopsEnvFileResourceE -> devopsEnvFileResourceE.getResourceId().equals(secretId))) {
                 DevopsEnvFileResourceE devopsEnvFileResourceE = devopsEnvFileResourceRepository
-                        .queryByEnvIdAndResource(envId, secretId, c7nSecret.getKind());
+                        .queryByEnvIdAndResource(envId, secretId, v1Secret.getKind());
                 if (devopsEnvFileResourceE != null && !devopsEnvFileResourceE.getFilePath()
-                        .equals(objectPath.get(TypeUtil.objToString(c7nSecret.hashCode())))) {
+                        .equals(objectPath.get(TypeUtil.objToString(v1Secret.hashCode())))) {
                     throw new GitOpsExplainException(GitOpsObjectError.OBJECT_EXIST.getError(), filePath, secretName);
                 }
             }
@@ -69,7 +69,7 @@ public class ConvertC7nSecretServiceImpl extends ConvertK8sObjectService<C7nSecr
         if (c7nSecrets.stream().anyMatch(e -> e.getMetadata().getName().equals(secretName))) {
             throw new GitOpsExplainException(GitOpsObjectError.OBJECT_EXIST.getError(), filePath, secretName);
         } else {
-            c7nSecrets.add(c7nSecret);
+            c7nSecrets.add(v1Secret);
         }
     }
 }
