7e317d79874eb84ee8507af00e4aaae76f628b97
ale <ale0720@163.com>
2019-05-14 10:27:49 +0800

    [FIX]修复菜单层级问题

--- react/config.js
+++ react/config.js
@@ -3,8 +3,8 @@ const config = {
   clientid: 'localhost',
   port: 9090,
   output: './dist',
-  // server: 'http://api.staging.saas.hand-china.com',
-  server: 'http://api.c7nf.choerodon.staging.saas.hand-china.com',
+  server: 'http://api.staging.saas.hand-china.com',
+  // server: 'http://api.c7nf.choerodon.staging.saas.hand-china.com',
   fileServer: 'http://minio.staging.saas.hand-china.com',
   titlename: 'Choerodon | 企业数字化服务平台',
   projectType: 'choerodon',
@@ -21,6 +21,8 @@ const config = {
       locale: './react/src/app/devops/locale/dashboard/*',
     },
   },
+  //对应 主菜单 | 组织层菜单 | 项目层菜单 | 用户菜单
+  resourcesLevel: ['site', 'organization', 'project', 'user'],
 };
 
 module.exports = config;
