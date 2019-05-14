da05b6e9e9c75e86ddc31329a82d5414f1b6276b
devil-scp <changpingshi0213@gmail.com>
2019-05-14 11:54:30 +0800

    [IMP] 修改通知查询

--- src/main/java/io/choerodon/devops/infra/persistence/impl/DevopsNotificationRepositoryImpl.java
+++ src/main/java/io/choerodon/devops/infra/persistence/impl/DevopsNotificationRepositoryImpl.java
@@ -1,23 +1,23 @@
 package io.choerodon.devops.infra.persistence.impl;
 
-import java.util.List;
-import java.util.Map;
+        import java.util.List;
+        import java.util.Map;
 
-import com.google.gson.Gson;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+        import com.google.gson.Gson;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Component;
 
-import io.choerodon.core.convertor.ConvertHelper;
-import io.choerodon.core.convertor.ConvertPageHelper;
-import io.choerodon.core.domain.Page;
-import io.choerodon.core.exception.CommonException;
-import io.choerodon.devops.domain.application.entity.DevopsNotificationE;
-import io.choerodon.devops.domain.application.repository.DevopsNotificationRepository;
-import io.choerodon.devops.infra.common.util.TypeUtil;
-import io.choerodon.devops.infra.dataobject.DevopsNotificationDO;
-import io.choerodon.devops.infra.mapper.DevopsNotificationMapper;
-import io.choerodon.mybatis.pagehelper.PageHelper;
-import io.choerodon.mybatis.pagehelper.domain.PageRequest;
+        import io.choerodon.core.convertor.ConvertHelper;
+        import io.choerodon.core.convertor.ConvertPageHelper;
+        import io.choerodon.core.domain.Page;
+        import io.choerodon.core.exception.CommonException;
+        import io.choerodon.devops.domain.application.entity.DevopsNotificationE;
+        import io.choerodon.devops.domain.application.repository.DevopsNotificationRepository;
+        import io.choerodon.devops.infra.common.util.TypeUtil;
+        import io.choerodon.devops.infra.dataobject.DevopsNotificationDO;
+        import io.choerodon.devops.infra.mapper.DevopsNotificationMapper;
+        import io.choerodon.mybatis.pagehelper.PageHelper;
+        import io.choerodon.mybatis.pagehelper.domain.PageRequest;
 
 /**
  * Creator: ChangpingShi0213@gmail.com
@@ -66,7 +66,7 @@ public class DevopsNotificationRepositoryImpl implements DevopsNotificationRepos
         Map<String, Object> map = gson.fromJson(params, Map.class);
         Map<String, Object> searchParamMap = TypeUtil.cast(map.get(TypeUtil.SEARCH_PARAM));
         String paramMap = TypeUtil.cast(map.get(TypeUtil.PARAM));
-        Page<DevopsNotificationDO> notificationDOPage = PageHelper.doPageAndSort(pageRequest, () -> notificationMapper.listByOptions());
+        Page<DevopsNotificationDO> notificationDOPage = PageHelper.doPageAndSort(pageRequest, () -> notificationMapper.listByOptions(projectId, envId, searchParamMap, paramMap));
         return ConvertPageHelper.convertPage(notificationDOPage, DevopsNotificationE.class);
     }
 }
