02007200883aec266fc7b6bce0172848b8ccce33
ale <ale0720@163.com>
2019-05-15 19:08:00 +0800

    [IMP]更新package.json路由入口

--- react/config.js
+++ react/config.js
@@ -1,16 +1,10 @@
 const config = {
-  local: true,
-  clientid: 'localhost',
-  port: 9090,
-  output: './dist',
-  server: 'http://api.staging.saas.hand-china.com',
-  // server: 'http://api.c7nf.choerodon.staging.saas.hand-china.com',
+  // server: 'http://api.staging.saas.hand-china.com',
+  server: 'http://api.c7nf.choerodon.staging.saas.hand-china.com',
   fileServer: 'http://minio.staging.saas.hand-china.com',
-  titlename: 'Choerodon | 企业数字化服务平台',
   projectType: 'choerodon',
   buildType: 'single',
   master: '@choerodon/master',
-  favicon: 'favicon.ico',
   theme: {
     'primary-color': '#3F51B5',
     'icon-font-size-base': '16px',
