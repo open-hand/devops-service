52ed8955dfd79a5f76f6e78e1ccf8428179d78dc
10980 <zhiyang.zhu@hand-china.com>
2019-05-31 14:26:02 +0800

    [ADD] add index

--- init-database.sh
+++ init-database.sh
@@ -2,11 +2,11 @@
 mkdir -p choerodon_temp
 if [ ! -f choerodon_temp/choerodon-tool-liquibase.jar ]
 then
-    curl http://nexus.choerodon.com.cn/repository/choerodon-release/io/choerodon/choerodon-tool-liquibase/0.11.0.RELEASE/choerodon-tool-liquibase-0.7.0.RELEASE.jar -L  -o choerodon_temp/choerodon-tool-liquibase.jar
+    curl http://nexus.choerodon.com.cn/repository/choerodon-release/io/choerodon/choerodon-tool-liquibase/0.11.0.RELEASE/choerodon-tool-liquibase-0.11.0.RELEASE.jar -L  -o choerodon_temp/choerodon-tool-liquibase.jar
 fi
-java -Dspring.datasource.url="jdbc:mysql://localhost/devops_service?useUnicode=true&characterEncoding=utf-8&useSSL=false" \
- -Dspring.datasource.username=choerodon \
- -Dspring.datasource.password=choerodon \
+java -Dspring.datasource.url="jdbc:mysql://192.168.16.190/devops_service?useUnicode=true&characterEncoding=utf-8&useSSL=false" \
+ -Dspring.datasource.username=root \
+ -Dspring.datasource.password=password \
  -Ddata.drop=false -Ddata.init=true \
  -Ddata.dir=src/main/resources \
  -jar choerodon_temp/choerodon-tool-liquibase.jar
