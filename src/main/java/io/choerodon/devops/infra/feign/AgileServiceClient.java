340e2e3ae2c75adbcbec08e7ae8113bebb7c3bf8
10980 <zhiyang.zhu@hand-china.com>
2019-07-03 20:14:06 +0800

    [FIX]修复feign服务降级

--- src/main/java/io/choerodon/devops/infra/feign/AgileServiceClient.java
+++ src/main/java/io/choerodon/devops/infra/feign/AgileServiceClient.java
@@ -2,13 +2,14 @@ package io.choerodon.devops.infra.feign;
 
 import io.choerodon.devops.domain.application.valueobject.Issue;
 import io.choerodon.devops.domain.application.valueobject.ProjectInfo;
+import io.choerodon.devops.infra.feign.fallback.AgileServiceClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "agile-service")
+@FeignClient(value = "agile-service", fallback = AgileServiceClientFallback.class)
 public interface AgileServiceClient {
 
     @GetMapping(value = "/v1/projects/{project_id}/issues/{issueId}")
