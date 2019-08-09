9fe0112448dfe336a1ac99b5c5d9023024ac74c8
zhaotianxin <ztxemail@163.com>
2019-08-09 16:17:18 +0800

    [UPDATE]AppService

--- src/main/java/io/choerodon/devops/api/controller/v1/DevopsOrganizationConfigController.java
+++ src/main/java/io/choerodon/devops/api/controller/v1/DevopsOrganizationConfigController.java
@@ -1,13 +1,16 @@
 package io.choerodon.devops.api.controller.v1;
 
+import java.util.List;
 import java.util.Optional;
 
 import io.choerodon.base.annotation.Permission;
 import io.choerodon.base.enums.ResourceType;
 import io.choerodon.core.exception.CommonException;
 import io.choerodon.core.iam.InitRoleCode;
+import io.choerodon.devops.api.vo.DefaultConfigVO;
 import io.choerodon.devops.api.vo.DevopsConfigVO;
 import io.choerodon.devops.app.service.DevopsConfigService;
+
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,8 +34,8 @@ public class DevopsOrganizationConfigController {
     /**
      * 组织下创建配置
      * @param organizationId 组织Id
-     * @param devopsConfigVO 配置信息
-     * @return ResponseEntity<DevopsConfigVO>
+     * @param devopsConfigVOS 配置信息
+     * @return void
      */
     @Permission(type = ResourceType.ORGANIZATION, roles = {InitRoleCode.ORGANIZATION_ADMINISTRATOR})
     @ApiOperation(value = "组织下创建配置")
@@ -41,30 +44,42 @@ public class DevopsOrganizationConfigController {
             @ApiParam(value = "组织ID",required = true)
             @PathVariable("organization_id") Long organizationId,
             @ApiParam(value = "配置信息", required = true)
-            @RequestBody DevopsConfigVO devopsConfigVO) {
-
-        return Optional.ofNullable(devopsConfigService.create(organizationId,devopsConfigVO))
-                 .map(target -> new ResponseEntity<>(target,HttpStatus.OK))
-                 .orElseThrow(() -> new CommonException("error.devops.organization.config.create"));
+            @RequestBody List<DevopsConfigVO> devopsConfigVOS) {
+        devopsConfigService.operate(organizationId,ResourceType.ORGANIZATION.value(),devopsConfigVOS);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     /**
-     * 组织下更新配置信息
+     * 组织下查询配置详情
      * @param organizationId 组织Id
-     * @param devopsConfigVO 配置信息
-     * @return ResponseEntity<DevopsConfigVO>
+     * @param type 配置类型
+     * @return
      */
-    @Permission(type = ResourceType.ORGANIZATION, roles = {InitRoleCode.ORGANIZATION_ADMINISTRATOR})
-    @ApiOperation(value = "组织下更新配置信息")
-    @PutMapping
-    public ResponseEntity<DevopsConfigVO> update(
-            @ApiParam(value = "组织ID",required = true)
-            @PathVariable("organization_id") Long organizationId,
-            @ApiParam(value = "配置信息", required = true)
-            @RequestBody DevopsConfigVO devopsConfigVO) {
-
-        return Optional.ofNullable(devopsConfigService.update(organizationId, devopsConfigVO))
-                .map(target -> new ResponseEntity<>(target, HttpStatus.OK))
-                .orElseThrow(() -> new CommonException("error.devops.organization.config.update"));
+    @Permission(type = ResourceType.ORGANIZATION,roles = {InitRoleCode.ORGANIZATION_ADMINISTRATOR})
+    @ApiOperation(value = "组织下查询配置详情")
+    @GetMapping
+    public ResponseEntity<List<DevopsConfigVO>> query(
+            @ApiParam(value = "组织Id",required = true)
+            @PathVariable(value = "organization_id") Long organizationId,
+            @ApiParam(value = "配置类型",required = true)
+            @RequestParam(value = "type") String type) {
+        return Optional.ofNullable(
+              devopsConfigService.queryByResourceId(organizationId, type))
+              .map(target -> new ResponseEntity<>(target, HttpStatus.OK))
+              .orElseThrow(() -> new CommonException("error.devops.organization.config.get.type"));
     }
+
+    @Permission(type = ResourceType.ORGANIZATION,roles = {InitRoleCode.ORGANIZATION_ADMINISTRATOR})
+    @ApiOperation(value = "组织下查询默认配置")
+    @GetMapping
+    public ResponseEntity<DefaultConfigVO> queryOrganizationDefaultConfig(
+            @ApiParam(value = "组织Id")
+            @PathVariable(value = "organization_id") Long organizationId) {
+        return Optional.ofNullable(
+                devopsConfigService.queryDefaultConfig(organizationId,ResourceType.ORGANIZATION.value()))
+                .map(target -> new ResponseEntity<>(target, HttpStatus.OK))
+                .orElseThrow(() -> new CommonException("error.devops.organization.config.get"));
+    }
+
+
 }
--- src/main/java/io/choerodon/devops/api/vo/AppServiceRepVO.java
+++ src/main/java/io/choerodon/devops/api/vo/AppServiceRepVO.java
@@ -1,6 +1,8 @@
 package io.choerodon.devops.api.vo;
 
+
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author younger
@@ -24,10 +26,17 @@ public class AppServiceRepVO {
     private String type;
     private Boolean permission;
     private Long objectVersionNumber;
-    private Long harborConfigId;
-    private Long chartConfigId;
+    private List<DevopsConfigVO> devopsConfigVOS;
     private String imgUrl;
     private Date creationDate;
+
+    public List<DevopsConfigVO> getDevopsConfigVOS() {
+        return devopsConfigVOS;
+    }
+
+    public void setDevopsConfigVOS(List<DevopsConfigVO> devopsConfigVOS) {
+        this.devopsConfigVOS = devopsConfigVOS;
+    }
 
     public Date getCreationDate() {
         return creationDate;
@@ -163,22 +172,6 @@ public class AppServiceRepVO {
 
     public void setObjectVersionNumber(Long objectVersionNumber) {
         this.objectVersionNumber = objectVersionNumber;
-    }
-
-    public Long getHarborConfigId() {
-        return harborConfigId;
-    }
-
-    public void setHarborConfigId(Long harborConfigId) {
-        this.harborConfigId = harborConfigId;
-    }
-
-    public Long getChartConfigId() {
-        return chartConfigId;
-    }
-
-    public void setChartConfigId(Long chartConfigId) {
-        this.chartConfigId = chartConfigId;
     }
 
     public Long getGitlabProjectId() {
