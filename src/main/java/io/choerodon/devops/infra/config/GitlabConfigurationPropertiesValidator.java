71ee19dca35ccc4835a3958b3ae034f888ad5dee
zmf <1984654893@qq.com>
2019-08-14 14:04:12 +0800

    [ADD] add property validation for gitlab password which will make application unavailable if the password is not in correct case

---
+++ src/main/java/io/choerodon/devops/infra/config/GitlabConfigurationPropertiesValidator.java
@@ -0,0 +1,38 @@
+package io.choerodon.devops.infra.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+/**
+ * @author zmf
+ */
+@Configuration
+public class GitlabConfigurationPropertiesValidator implements Validator {
+    private Logger logger = LoggerFactory.getLogger(GitlabConfigurationPropertiesValidator.class);
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return GitlabConfigurationProperties.class == clazz;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        GitlabConfigurationProperties properties = (GitlabConfigurationProperties) target;
+        if (properties.getPassword() == null || properties.getPassword().length() < 8) {
+            logger.error("===============================================ERROR======================================================");
+            logger.error("Error: The gitlab password in gitlab configuration must not be null and it's length should not less than 8");
+            logger.error("===============================================ERROR======================================================");
+            errors.rejectValue("password", "error.password.length");
+        }
+    }
+
+
+    @Bean
+    public static Validator configurationPropertiesValidator() {
+        return new GitlabConfigurationPropertiesValidator();
+    }
+}
