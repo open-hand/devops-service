e0ca20c6ad74c55d95ec02ad71c1fd251e19ca23
devil-scp <changping.shi@hand-china.com>
2019-09-02 15:39:39 +0800

    [IMP] 修改组织下查询所有项目

--- src/main/java/io/choerodon/devops/infra/dto/iam/ApplicationDTO.java
+++ src/main/java/io/choerodon/devops/infra/dto/iam/ApplicationDTO.java
@@ -1,25 +1,37 @@
 package io.choerodon.devops.infra.dto.iam;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import io.choerodon.mybatis.entity.BaseDTO;
 
 /**
  * @author zongw.lee@gmail.com
- * @date 2019/7/30
+ * @since 2019/7/30
  */
 public class ApplicationDTO extends BaseDTO {
+    @ApiModelProperty(value = "主键ID/非必填")
     private Long id;
 
+    @ApiModelProperty(value = "应用名/必填")
     private String name;
 
+    @ApiModelProperty(value = "应用编码/必填")
     private String code;
 
+    @ApiModelProperty(value = "组织ID/必填")
     private Long organizationId;
 
+    @ApiModelProperty(value = "应用图标url")
     private String imageUrl;
 
+    @ApiModelProperty(value = "应用类型/必填")
     private String type;
 
+    @ApiModelProperty(value = "应用来源Id/非必填")
     private Long sourceId;
+
+    @ApiModelProperty(value = "标识应用的UUID")
+    private String token;
 
     public Long getId() {
         return id;
@@ -75,5 +87,13 @@ public class ApplicationDTO extends BaseDTO {
 
     public void setSourceId(Long sourceId) {
         this.sourceId = sourceId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 }
