19b506d1113aa2d5ec99346c41d08ee60d5e039a
zmf <1984654893@qq.com>
2019-09-04 17:57:26 +0800

    [FIX] fix instance and tree menu items according to permissions the user owns

--- src/main/java/io/choerodon/devops/infra/mapper/DevopsEnvironmentMapper.java
+++ src/main/java/io/choerodon/devops/infra/mapper/DevopsEnvironmentMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import io.choerodon.devops.api.vo.DevopsEnvResourceCountVO;
 import io.choerodon.devops.infra.dto.*;
 import io.choerodon.mybatis.common.Mapper;
+
 import org.apache.ibatis.annotations.Param;
 
 /**
@@ -25,20 +26,38 @@ public interface DevopsEnvironmentMapper extends Mapper<DevopsEnvironmentDTO> {
     void updateOptions(@Param("gitlabEnvProjectId") Long gitlabEnvProjectId, @Param("hookId") Long hookId, @Param("isSynchro") Boolean isSynchro, @Param("envId") Long envId);
 
     /**
-     * 项目下，查询实例视图的环境及其应用及实例作为树形目录
+     * 项目下，查询实例视图的环境及其应用及实例作为树形目录（所有环境，以项目所有者的权限）
      *
      * @param projectId 项目id
      * @return 树形目录
      */
-    List<DevopsEnvironmentViewDTO> listInstanceEnvTree(@Param("projectId") Long projectId);
+    List<DevopsEnvironmentViewDTO> listAllInstanceEnvTree(@Param("projectId") Long projectId);
 
     /**
-     * 项目下，查询资源视图的环境及其下资源作为树形目录
+     * 项目下，查询实例视图的环境及其应用及实例作为树形目录（以项目成员的权限）
+     *
+     * @param projectId 项目id
+     * @param memberId  项目成员id
+     * @return 树形目录
+     */
+    List<DevopsEnvironmentViewDTO> listMemberInstanceEnvTree(@Param("projectId") Long projectId, @Param("memberId") Long memberId);
+
+    /**
+     * 项目下，查询资源视图的环境及其下资源作为树形目录所有环境，以项目所有者的权限）
      *
      * @param projectId 项目id
      * @return 树形目录
      */
-    List<DevopsResourceEnvOverviewDTO> listResourceEnvTree(@Param("projectId") Long projectId);
+    List<DevopsResourceEnvOverviewDTO> listAllResourceEnvTree(@Param("projectId") Long projectId);
+
+    /**
+     * 项目下，查询资源视图的环境及其下资源作为树形目录（以项目成员的权限）
+     *
+     * @param projectId 项目id
+     * @param memberId  项目成员id
+     * @return 树形目录
+     */
+    List<DevopsResourceEnvOverviewDTO> listMemberResourceEnvTree(@Param("projectId") Long projectId, @Param("memberId") Long memberId);
 
     /**
      * 查询单个环境及其集群信息
@@ -61,6 +80,7 @@ public interface DevopsEnvironmentMapper extends Mapper<DevopsEnvironmentDTO> {
 
     /**
      * 查询指定分组的相关环境
+     *
      * @param projectId 项目id
      * @param groupId   分组id
      * @param active    是否可用
