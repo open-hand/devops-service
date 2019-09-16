e3c57c1ae2f4736029941feba1204a1cb0d554b5
zhaotianxin <ztxemail@163.com>
2019-09-16 19:28:20 +0800

    [IMP] 修改查询应用版本接口的返回类型

--- src/main/java/io/choerodon/devops/app/service/AppServiceVersionService.java
+++ src/main/java/io/choerodon/devops/app/service/AppServiceVersionService.java
@@ -182,7 +182,7 @@ public interface AppServiceVersionService {
      * @param share         是否是组织共享
      * @return 应用服务版本
      */
-    List<AppServiceVersionVO> listServiceVersionByAppServiceIds(Set<Long> appServiceIds, String share);
+    List<AppServiceVersionDTO> listServiceVersionByAppServiceIds(Set<Long> appServiceIds, String share);
 
     /**
      * 根据应用服务IDs和共享规则去查询应用服务的版本列表
@@ -192,4 +192,6 @@ public interface AppServiceVersionService {
      * @return
      */
     List<AppServiceVersionVO> queryServiceVersionByAppServiceIdAndShare(Long appServiceId, String share);
+
+    List<AppServiceVersionVO> listServiceVersionVoByIds(Set<Long> ids);
 }
