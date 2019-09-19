e4f19bcec38ace562cc52f99acceceff30ec904d
20399 <yanfang.lin@hand-china.com>
2019-09-19 20:46:50 +0800

    [IMP]修改应用服务特殊字符提示

--- react/locale/zh_CN.js
+++ react/locale/zh_CN.js
@@ -426,6 +426,7 @@ const appService = {
   'c7ncd.appService.stop.tips': '确定要停用该应用服务吗？',
   'c7ncd.appService.project.failed': '由小写字母，数字，\'-\'、\'_\'或\'.\'组成，以字母、数字开始和结束',
   'c7ncd.appService.param': '请输入查询条件',
+  'c7ncd.appService.name.failed': '输入内容不合规则，请重新输入',
 };
 
 const envrionment = {
