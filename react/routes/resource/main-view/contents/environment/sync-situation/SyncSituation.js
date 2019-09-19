b37bf2909e9b076b2317e3260f7c9f5a443e9347
安磊 <alforstudy@163.com>
2019-09-19 11:43:08 +0800

    [FIX]更改环境-Gitops日志-文案

--- react/routes/resource/main-view/contents/environment/sync-situation/SyncSituation.js
+++ react/routes/resource/main-view/contents/environment/sync-situation/SyncSituation.js
@@ -69,7 +69,7 @@ const SyncSituation = observer(() => {
         <div className="log-sync-line">
           <div className="log-sync-line-card">
             <div className="log-sync-line-card-title">
-              <FormattedMessage id={`${intlPrefix}.environment.gitlab`} />
+              <FormattedMessage id={`${intlPrefix}.environment.config-lab`} />
             </div>
             <div className="log-sync-line-card-commit">
               <a
@@ -99,7 +99,7 @@ const SyncSituation = observer(() => {
           </div>
           <div className="log-sync-line-card">
             <div className="log-sync-line-card-title">
-              <FormattedMessage id={`${intlPrefix}.environment.analysis`} />
+              <FormattedMessage id={`${intlPrefix}.environment.resolved`} />
             </div>
             <div className="log-sync-line-card-commit">
               <a
@@ -116,7 +116,7 @@ const SyncSituation = observer(() => {
           </div>
           <div className="log-sync-line-card">
             <div className="log-sync-line-card-title">
-              <FormattedMessage id={`${intlPrefix}.environment.agent`} />
+              <FormattedMessage id={`${intlPrefix}.environment.executed`} />
             </div>
             <div className="log-sync-line-card-commit">
               <a
