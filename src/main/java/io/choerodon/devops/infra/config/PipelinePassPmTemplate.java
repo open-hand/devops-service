9f1e932a4718ea4bef3707c170ded60c8acba686
devil-scp <changping.shi@hand-china.com>
2019-09-24 17:18:48 +0800

    [IMP] 修改流水线通知url异常

--- src/main/java/io/choerodon/devops/infra/config/PipelinePassPmTemplate.java
+++ src/main/java/io/choerodon/devops/infra/config/PipelinePassPmTemplate.java
@@ -39,6 +39,6 @@ public class PipelinePassPmTemplate implements PmTemplate {
     @Override
     public String content() {
         return "<p>流水线“${pipelineName}”在【${stageName}】阶段中的或签任务已被${auditName}:${realName}审核<p>" +
-                "<p><a href=#/devops/pipeline-record/detail/${pipelineId}/${pipelineRecordId}?type=project&id=${projectId}&name=${projectName}&category=undefined&organizationId=${organizationId}>查看详情</a >";
+                "<p><a href=#/devops/deployment-operation?type=project&id=${projectId}&name=${projectName}&category=undefined&organizationId=${organizationId}&orgId=${organizationId}&pipelineRecordId=${pipelineRecordId}>查看详情</a >";
     }
 }
