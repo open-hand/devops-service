5397f0610e14f0b2c89590503f4ea428e99c02c0
devil-scp <changping.shi@hand-china.com>
2019-10-23 20:02:05 +0800

    [IMP] 添加集群资源表

---
+++ src/main/java/io/choerodon/devops/infra/dto/devopsClusterResourceDTO.java
@@ -0,0 +1,72 @@
+package io.choerodon.devops.infra.dto;
+
+import io.choerodon.mybatis.entity.BaseDTO;
+
+import javax.persistence.Table;
+
+@Table(name = "devops_cluster_resource")
+public class devopsClusterResourceDTO extends BaseDTO {
+    private Long id;
+    private String name;
+    private String code;
+    private String type;
+    private Long clusterId;
+    private Long instanceId;
+    private Long configId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getClusterId() {
+        return clusterId;
+    }
+
+    public void setClusterId(Long clusterId) {
+        this.clusterId = clusterId;
+    }
+
+    public Long getInstanceId() {
+        return instanceId;
+    }
+
+    public void setInstanceId(Long instanceId) {
+        this.instanceId = instanceId;
+    }
+
+    public Long getConfigId() {
+        return configId;
+    }
+
+    public void setConfigId(Long configId) {
+        this.configId = configId;
+    }
+}
