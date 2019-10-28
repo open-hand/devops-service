e314d2717b35de0eeaaf3d40a67eea65468a9d9e
lizhaozhong <lzz2234@163.com>
2019-10-28 14:03:41 +0800

    [IMP] 添加prometheus实体类

---
+++ src/main/java/io/choerodon/devops/infra/dto/DevopsPrometheusDTO.java
@@ -0,0 +1,69 @@
+package io.choerodon.devops.infra.dto;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import io.choerodon.mybatis.entity.BaseDTO;
+
+/**
+ * @author: 25499
+ * @date: 2019/10/28 13:56
+ * @description:
+ */
+@Table(name = "devops_prometheus")
+public class DevopsPrometheusDTO extends BaseDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String adminPassword;
+
+    private String grafanaDomain;
+
+    private String pvName;
+
+    private String clusterName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+
+    public String getGrafanaDomain() {
+        return grafanaDomain;
+    }
+
+    public void setGrafanaDomain(String grafanaDomain) {
+        this.grafanaDomain = grafanaDomain;
+    }
+
+    public String getPvName() {
+        return pvName;
+    }
+
+    public void setPvName(String pvName) {
+        this.pvName = pvName;
+    }
+
+    public String getClusterName() {
+        return clusterName;
+    }
+
+    public void setClusterName(String clusterName) {
+        this.clusterName = clusterName;
+    }
+}
