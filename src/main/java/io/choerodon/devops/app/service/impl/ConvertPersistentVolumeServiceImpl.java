6d4efb71b616222a62ca8dc5b49fb2c367099e7b
zmf <1984654893@qq.com>
2019-11-07 18:04:36 +0800

    [ADD] add ConvertPersistentVolumeServiceImpl

--- src/main/java/io/choerodon/devops/app/service/impl/ConvertPersistentVolumeServiceImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/ConvertPersistentVolumeServiceImpl.java
@@ -1,12 +1,20 @@
 package io.choerodon.devops.app.service.impl;
 
+import static io.choerodon.devops.infra.util.GitOpsUtil.*;
+
 import java.util.List;
 import java.util.Map;
 
 import io.kubernetes.client.models.V1PersistentVolume;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import io.choerodon.core.exception.CommonException;
+import io.choerodon.devops.app.service.DevopsEnvFileResourceService;
+import io.choerodon.devops.app.service.DevopsPvServcie;
+import io.choerodon.devops.infra.constant.KubernetesConstants;
 import io.choerodon.devops.infra.dto.DevopsEnvFileResourceDTO;
+import io.choerodon.devops.infra.dto.DevopsPvDTO;
 import io.choerodon.devops.infra.enums.GitOpsObjectError;
 import io.choerodon.devops.infra.enums.ResourceType;
 import io.choerodon.devops.infra.exception.GitOpsExplainException;
@@ -18,6 +26,11 @@ import io.choerodon.devops.infra.util.TypeUtil;
  */
 @Component
 public class ConvertPersistentVolumeServiceImpl extends ConvertK8sObjectService<V1PersistentVolume> {
+    @Autowired
+    private DevopsPvServcie devopsPvServcie;
+    @Autowired
+    private DevopsEnvFileResourceService devopsEnvFileResourceService;
+
     public ConvertPersistentVolumeServiceImpl() {
         super(V1PersistentVolume.class);
     }
@@ -33,12 +46,37 @@ public class ConvertPersistentVolumeServiceImpl extends ConvertK8sObjectService<
             throw new GitOpsExplainException(
                     GitOpsObjectError.PERSISTENT_VOLUME_NAME_NOT_FOUND.getError(), filePath);
         }
-        // TODO by zmf
+        if (pv.getSpec() == null || pv.getSpec().getCapacity() == null) {
+            throw new GitOpsExplainException(
+                    GitOpsObjectError.PERSISTENT_VOLUME_CAPACITY_NOT_FOUND.getError(), filePath);
+        } else if (pv.getSpec().getCapacity().get(KubernetesConstants.STORAGE) == null) {
+            throw new GitOpsExplainException(
+                    GitOpsObjectError.PERSISTENT_VOLUME_STORAGE_NOT_FOUND.getError(), filePath);
+        }
+        if (pv.getSpec().getAccessModes() == null) {
+            throw new CommonException(
+                    GitOpsObjectError.PERSISTENT_VOLUME_ACCESS_MODE_NOT_FOUND.getError(), filePath);
+        }
+        if (pv.getSpec().getAccessModes().size() > 1) {
+            throw new CommonException(
+                    GitOpsObjectError.PERSISTENT_VOLUME_ACCESS_MODE_SIZE_NOT_SUPPORTED.getError(), filePath);
+        }
     }
 
     @Override
-    public void checkIfExist(List<V1PersistentVolume> v1PersistentVolumes, Long envId, List<DevopsEnvFileResourceDTO> beforeSyncDelete, Map<String, String> objectPath, V1PersistentVolume v1PersistentVolume) {
-        // TODO by zmf
+    public void checkIfExist(List<V1PersistentVolume> v1PersistentVolumes, Long envId, List<DevopsEnvFileResourceDTO> beforeSyncDelete, Map<String, String> objectPath, V1PersistentVolume pv) {
+        String filePath = objectPath.get(TypeUtil.objToString(pv.hashCode()));
+        DevopsPvDTO devopsPvDTO = devopsPvServcie.queryByEnvIdAndName(envId, pv.getMetadata().getName());
+        if (devopsPvDTO != null
+                && !isDeletedByCurrentOperation(beforeSyncDelete, devopsPvDTO.getId(), ResourceType.PERSISTENT_VOLUME)) {
+            checkNotExistInDb(devopsEnvFileResourceService.baseQueryByEnvIdAndResourceId(envId, devopsPvDTO.getId(), pv.getKind()), filePath, pv.getMetadata().getName());
+        }
+
+        if (isContainedByList(v1PersistentVolumes, pv, obj -> obj.getMetadata().getName())) {
+            throwExistEx(filePath, pv.getMetadata().getName());
+        }
+
+        v1PersistentVolumes.add(pv);
     }
 
     @Override
--- src/main/java/io/choerodon/devops/infra/util/GitOpsUtil.java
+++ src/main/java/io/choerodon/devops/infra/util/GitOpsUtil.java
@@ -2,11 +2,18 @@ package io.choerodon.devops.infra.util;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Function;
 
+import javax.annotation.Nullable;
+
 import io.choerodon.devops.api.vo.kubernetes.C7nHelmRelease;
+import io.choerodon.devops.infra.dto.DevopsEnvFileResourceDTO;
 import io.choerodon.devops.infra.enums.C7NHelmReleaseMetadataType;
 import io.choerodon.devops.infra.enums.EnvironmentType;
+import io.choerodon.devops.infra.enums.GitOpsObjectError;
+import io.choerodon.devops.infra.enums.ResourceType;
+import io.choerodon.devops.infra.exception.GitOpsExplainException;
 
 /**
  * GitOps解析相关的工具类
@@ -53,5 +60,73 @@ public class GitOpsUtil {
                 add.add(obj);
             }
         }
+    }
+
+    /**
+     * 是否被当前的操作删除了
+     *
+     * @param beforeSyncDelete 当前操作删除的文件及其对应关系
+     * @param resourceId       待判断的资源id
+     * @param resourceType     资源类型
+     * @return true表示被删除了，反之，没有被删除
+     */
+    public static boolean isDeletedByCurrentOperation(List<DevopsEnvFileResourceDTO> beforeSyncDelete,
+                                                      Long resourceId, ResourceType resourceType) {
+        return beforeSyncDelete.stream().anyMatch(
+                envFileResourceDTO -> envFileResourceDTO.getResourceType().equals(resourceType.getType())
+                        && envFileResourceDTO.getResourceId().equals(resourceId));
+    }
+
+    /**
+     * 通过对象的某一个key的比较，来确认list中是否包含某个对象
+     *
+     * @param beforeList        已有的list
+     * @param toBeJudged        待判断的对象
+     * @param propertyExtractor 提取对象的用于比较的数据的提取器
+     * @param <T>               泛型参数
+     * @return true表示包含
+     */
+    public static <T> boolean isContainedByList(
+            List<T> beforeList, T toBeJudged, Function<T, Object> propertyExtractor) {
+        return beforeList.stream().anyMatch(before -> identifyByProperty(before, toBeJudged, propertyExtractor));
+    }
+
+    /**
+     * 通过对象的某一个key比较对象是否相等
+     *
+     * @param one               一个对象
+     * @param another           另一个对象
+     * @param propertyExtractor 提取对象的用于比较的数据的提取器
+     * @param <T>               泛型参数
+     * @return true表示相等
+     */
+    public static <T> boolean identifyByProperty(T one, T another, Function<T, Object> propertyExtractor) {
+        return Objects.equals(propertyExtractor.apply(one), propertyExtractor.apply(another));
+    }
+
+    /**
+     * 校验资源未在数据库中定义，已定义则抛异常
+     *
+     * @param devopsEnvFileResourceDTO 资源所对应的数据纪录
+     * @param filePath                 文件路径
+     * @param resourceName             资源名称
+     */
+    public static void checkNotExistInDb(@Nullable DevopsEnvFileResourceDTO devopsEnvFileResourceDTO,
+                                         String filePath, String resourceName) {
+        if (devopsEnvFileResourceDTO != null &&
+                !devopsEnvFileResourceDTO.getFilePath().equals(filePath)) {
+            throwExistEx(filePath, resourceName);
+        }
+    }
+
+    /**
+     * 抛出资源对象存在的异常
+     *
+     * @param filePath     资源所在文件路径
+     * @param resourceName 资源名称
+     */
+    public static void throwExistEx(String filePath, String resourceName) {
+        throw new GitOpsExplainException(
+                GitOpsObjectError.OBJECT_EXIST.getError(), filePath, resourceName);
     }
 }
