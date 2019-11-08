325776a8e63dba641773f7c61d87313458ed3a0f
25499 <zhaozhong.li@hand-china.com>
2019-11-08 09:39:20 +0800

    [IMP] 添加部署prometheus的枚举类

--- src/main/java/io/choerodon/devops/infra/enums/ClusterResourceStatus.java
+++ src/main/java/io/choerodon/devops/infra/enums/ClusterResourceStatus.java
@@ -10,14 +10,7 @@ public enum ClusterResourceStatus {
     AVAILABLE("available"),
     DISABLED("disabled");
     private String status;
-
-    interface Prometheus{
-        public static  final String CREATE_PVC_SUCCESS="create_pvc_success";
-        public static  final String CREATE_CONFIG_SUCCESS="create_config_success";
-        public static  final String INSTALL_PROMETHEUS_SUCCESS="install_prometheus_success";
-        public static  final String INSTALL_PROMETHEUS_FAIL="install_prometheus_fail";
-    }
-
+    
     ClusterResourceStatus(String status) {
         this.status = status;
     }
