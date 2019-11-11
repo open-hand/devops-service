08f51eb7f1c7ed80307973c83fb2216f5fcd7170
NollieLeo <736653759@qq.com>
2019-11-11 09:55:53 +0800

    [ADD] 新增PVC渲染操作标签和状态标签

--- react/routes/resource/main-view/contents/pvc/PvcContent.js
+++ react/routes/resource/main-view/contents/pvc/PvcContent.js
@@ -1,7 +1,7 @@
-import React, { useMemo, useState, useCallback } from 'react';
+import React, { useMemo, useState, useCallback, Fragment } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Action } from '@choerodon/boot';
-import { Modal, Table } from 'choerodon-ui/pro';
+import { Modal, Table, Tooltip } from 'choerodon-ui/pro';
 import { keys } from 'lodash';
 import MouserOverWrapper from '../../../../../components/MouseOverWrapper/MouserOverWrapper';
 import StatusTags from '../../../../../components/status-tag';
@@ -23,6 +23,7 @@ const modalKey = Modal.key();
 const modalStyle = {
   width: 'calc(100vw - 3.52rem)',
 };
+
 
 const ConfigMap = observer((props) => {
   const {
@@ -52,15 +53,46 @@ const ConfigMap = observer((props) => {
 
   }
 
-  function renderAction() {
+  function handleDelete(record) {
 
   }
-  function renderName() {
 
+  function renderAction({ record }) {
+    const status = record.get('status');
+    if (status && (status === 'bound' || status === 'lost')) {
+      const action = [
+        {
+          service: ['devops-service.devops-pvc.delete'],
+          text: formatMessage({ id: 'delete' }),
+          action: handleDelete.bind(this, record),
+        },
+      ];
+      return (<Action data={action} />);
+    } else {
+      return null;
+    }
   }
+
+  function renderName({ record, value }) {
+    const status = record.get('status');
+    if (status) {
+      return (
+        <Fragment>
+          <StatusTags name={status} colorCode={status} />
+          <Tooltip title={value}>
+            <span>
+              {value}
+            </span>
+          </Tooltip>
+        </Fragment>
+      );
+    }
+  }
+
   function renderValue() {
 
   }
+
   return (
     <div className={`${prefixCls}-keyValue-PVC`}>
       <Modals />
@@ -72,7 +104,7 @@ const ConfigMap = observer((props) => {
       >
         <Column name="name" header={formatMessage({ id: `${intlPrefix}.cipher` })} renderer={renderName} />
         <Column renderer={renderAction} width="0.7rem" />
-        <Column name="status" renderer={renderValue} header={formatMessage({ id: 'status' })} />
+        {/* <Column name="status" renderer={renderValue} header={formatMessage({ id: 'status' })} /> */}
         {/* <Column name="lastUpdateDate" renderer={renderDate} width="1rem" /> */}
       </Table>
     </div>
