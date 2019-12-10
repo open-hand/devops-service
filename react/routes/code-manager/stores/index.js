a1f101b7792c52526a50cf7c30b10c3508da19b8
NollieLeo <736653759@qq.com>
2019-12-10 15:18:24 +0800

    [ADD] 新增应用服务最近访问模块

--- react/routes/code-manager/stores/index.js
+++ react/routes/code-manager/stores/index.js
@@ -21,15 +21,56 @@ export const StoreProvider = injectIntl(inject('AppState')(
         currentMenuType: { id: projectId },
       },
     } = props;
-    const handleDataSetChange = ({ record, value, oldValue }) => {
+
+    const checkHasApp = (value, recentApp) => recentApp.some(e => e.id === value);
+
+    const unshiftPop = (value, recentApp, recentAppList) => { // 有数据的话又再一次访问这个appservice则把他放到数组第一位
+      for (let i = 0; i < recentApp.length; i++) {
+        if (recentApp[i].id === value[0].id) {
+          recentApp.splice(i, 1); // 如果数据组存在该元素，则把该元素删除
+          break;
+        }
+      }
+      recentApp.unshift(value[0]);
+      recentAppList[projectId] = recentApp;
+      localStorage.setItem('recent-app', JSON.stringify(recentAppList));
+    };
+
+    const setLocalStorage = (value) => {
+      const recentAppList = localStorage.getItem('recent-app') && JSON.parse(localStorage.getItem('recent-app'));
+      const temp = appServiceDs.toData().filter(e => e.id === value);
+      const objTemp = {};
+      if (recentAppList !== null && recentAppList[projectId]) {
+        const recentApp = recentAppList[projectId];
+        if (!checkHasApp(value, recentApp)) { // 先校验localstorage里面有没有这个数据
+          recentApp.unshift(temp[0]);
+          if (recentApp.length > 5) {
+            recentApp.splice(-1, 1);
+          }
+          recentAppList[projectId] = recentApp;
+          localStorage.setItem('recent-app', JSON.stringify(recentAppList));
+        } else {
+          unshiftPop(temp, recentApp, recentAppList);
+        }
+      } else if (recentAppList === null) {
+        objTemp[projectId] = [temp[0]];
+        localStorage.setItem('recent-app', JSON.stringify(objTemp));
+      } else {
+        recentAppList[projectId] = [temp[0]];
+        localStorage.setItem('recent-app', JSON.stringify(recentAppList));
+      }
+    };
+
+    const handleDataSetChange = ({ dataSet, record, value, oldValue }) => {
       if (!value) return;
-      
+
       const option = {
         props: {
           children: record.get('name'),
           value: record.get('id'),
         },
       };
+      setLocalStorage(value);
       Object.keys(handleMapStore)
         .forEach((key) => {
           if (key.indexOf('Code') !== -1) {
@@ -66,10 +107,14 @@ export const StoreProvider = injectIntl(inject('AppState')(
         url: `/devops/v1/projects/${projectId}/app_service/list_by_active`,
         method: 'get',
       });
+      const recentApp = localStorage.getItem('recent-app') && JSON.parse(localStorage.getItem('recent-app'));
       appServiceDs.query().then((res) => {
-        if (res && res.length && res.length > 0) {
-          selectAppDs.current.set('appServiceId', res[0].id);
-        }  
+        // if (recentApp) {
+        //   selectAppDs.current.set('appServiceId', recentApp[0].id);
+        // } else if (res && res.length && res.length > 0) {
+
+        // }
+        selectAppDs.current.set('appServiceId', res[0].id);
       });
     }, [projectId]);
     const value = {
@@ -84,6 +129,7 @@ export const StoreProvider = injectIntl(inject('AppState')(
       permissions,
       appServiceDs,
       selectAppDs,
+      projectId,
     };
     return (
       <Store.Provider value={value}>
