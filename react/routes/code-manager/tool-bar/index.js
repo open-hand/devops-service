2ef60199f0d99289a5a6fb4a34fbd5bde064758c
NollieLeo <736653759@qq.com>
2019-12-10 15:48:40 +0800

    [IMP] 优化代码管理复制地址，下拉复制旁边的cusors改成pointer

--- react/routes/code-manager/tool-bar/index.js
+++ react/routes/code-manager/tool-bar/index.js
@@ -89,7 +89,7 @@ export const SelectApp = injectIntl(inject('AppState')(observer((props) => {
               text={(currentApp && currentApp.sshRepositoryUrl) || noRepoUrl}
               onCopy={handleCopy}
             >
-              <Icon type="content_copy" />
+              <Icon type="content_copy" style={{ cursor: 'pointer' }} />
             </CopyToClipboard>
           }
         />
