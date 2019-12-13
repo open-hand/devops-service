8ee36a5179dc2de10a3f5153fe1335fe1d984ade
zmf <1984654893@qq.com>
2019-12-13 10:31:45 +0800

    [FIX] fix pv and pvc update by

--- src/main/java/io/choerodon/devops/infra/util/CustomContextUtil.java
+++ src/main/java/io/choerodon/devops/infra/util/CustomContextUtil.java
@@ -1,18 +1,21 @@
 package io.choerodon.devops.infra.util;
 
-import io.choerodon.asgard.saga.consumer.MockHttpServletRequest;
-import io.choerodon.core.exception.CommonException;
-import io.choerodon.core.oauth.CustomUserDetails;
-import io.choerodon.core.oauth.DetailsHelper;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.concurrent.Callable;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 
-import java.util.Collections;
-import java.util.HashMap;
+import io.choerodon.asgard.saga.consumer.MockHttpServletRequest;
+import io.choerodon.core.exception.CommonException;
+import io.choerodon.core.oauth.CustomUserDetails;
+import io.choerodon.core.oauth.DetailsHelper;
 
 /**
  * Creator: ChangpingShi0213@gmail.com
@@ -52,6 +55,70 @@ public class CustomContextUtil {
 
         } catch (Exception e) {
             throw new CommonException("context.set.error", e);
+        }
+    }
+
+    /**
+     * 设置特定的用户上下文
+     *
+     * @param context 用户上下文
+     */
+    public static void setUserContext(UserDetails context) {
+        try {
+            Authentication user = new UsernamePasswordAuthenticationToken("default", "N/A", Collections.emptyList());
+            OAuth2Request request = new OAuth2Request(new HashMap<>(0), "", Collections.emptyList(), true,
+                    Collections.emptySet(), Collections.emptySet(), null, null, null);
+            OAuth2Authentication authentication = new OAuth2Authentication(request, user);
+            OAuth2AuthenticationDetails oAuth2AuthenticationDetails = new OAuth2AuthenticationDetails(new MockHttpServletRequest());
+            oAuth2AuthenticationDetails.setDecodedDetails(context);
+            authentication.setDetails(oAuth2AuthenticationDetails);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (Exception e) {
+            throw new CommonException("context.set.error", e);
+        }
+    }
+
+    /**
+     * 在特定的上下文运行callable中的代码，运行后的上下文与运行前一致
+     *
+     * @param contextUserId 特定用户上下文
+     * @param callable      可执行的操作
+     * @param <T>           返回结果的类型
+     * @return 返回callable的返回值
+     */
+    public static <T> T executeCallableInCertainContext(Long contextUserId, Callable<T> callable) {
+        UserDetails preContext = null;
+        try {
+            preContext = DetailsHelper.getUserDetails();
+            setUserContext(contextUserId);
+            return callable.call();
+        } catch (CommonException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            throw new CommonException("error.execute.in.certain.context", ex);
+        } finally {
+            setUserContext(preContext);
+        }
+    }
+
+    /**
+     * 在特定的上下文运行runnable中的代码，运行后的上下文与运行前一致
+     *
+     * @param contextUserId 特定用户上下文
+     * @param runnable      可执行的操作
+     */
+    public static void executeRunnableInCertainContext(Long contextUserId, Runnable runnable) {
+        UserDetails preContext = null;
+        try {
+            preContext = DetailsHelper.getUserDetails();
+            setUserContext(contextUserId);
+            runnable.run();
+        } catch (CommonException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            throw new CommonException("error.execute.in.certain.context", ex);
+        } finally {
+            setUserContext(preContext);
         }
     }
 }
