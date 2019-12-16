9db0b8d37a1ab13b6bac23a0c02e0a285e6be609
scp <changping.shi@hand-china.com>
2019-12-16 16:30:53 +0800

    [FIX] 修复应用服务导入添加敏捷模板

--- src/main/java/io/choerodon/devops/infra/enums/AppServiceTemplate.java
+++ src/main/java/io/choerodon/devops/infra/enums/AppServiceTemplate.java
@@ -1,7 +1,7 @@
 package io.choerodon.devops.infra.enums;
 
 
-        import java.util.HashMap;
+import java.util.HashMap;
 
 public enum AppServiceTemplate {
     MICROSERVICE_TEMPLATE("MicroService"),
@@ -9,7 +9,9 @@ public enum AppServiceTemplate {
     JAVALIB_TEMPLATE("JavaLib"),
     SPRINGBOOT_TEMPLATE("SpringBoot"),
     GO_TEMPLATE("goTemplate"),
-    CHOERODON_MOCHA_TEMPLATE("choerodonMochaTemplate");
+    CHOERODON_MOCHA_TEMPLATE("choerodonMochaTemplate"),
+    CHOERODON_TESTNG_TEMPLATE("choerodonTestngTemplate"),
+    CHOERODON_TESTNG_SELENIUM_TEMPLATE("choerodonTestngSeleniumTemplate");
     private static final String TEMPLATE_URL = "https://github.com/choerodon/";
     private static final String FORMAT_MODAL = "%s%s";
     private static final String MICROSERVICE_PATH = "choerodon-microservice-template.git?version=0.13.0";
@@ -18,6 +20,8 @@ public enum AppServiceTemplate {
     private static final String SPRINGBOOT_PATH = "choerodon-springboot-template.git?version=0.19.0";
     private static final String GO_PATH = "choerodon-golang-template.git?version=0.13.0";
     private static final String CHOERODON_MOCHA_PATH = "choerodon-mocha-template.git?version=0.13.0";
+    private static final String CHOERODON_TESTNG_TEMPLATE_PATH = "choerodon-testng-template.git?version=0.19.0";
+    private static final String CHOERODON_TESTNG_SELENIUM_TEMPLATE_PATH = "choerodon-testng-selenium-template?version=0.19.0";
 
     private String templateName;
 
@@ -29,7 +33,7 @@ public enum AppServiceTemplate {
         return templateName;
     }
 
-    public static HashMap<String, String> templatePath = new HashMap<>(6);
+    public static HashMap<String, String> templatePath = new HashMap<>(8);
 
     static {
         templatePath.put(AppServiceTemplate.MICROSERVICE_TEMPLATE.templateName, String.format(FORMAT_MODAL, TEMPLATE_URL, MICROSERVICE_PATH));
@@ -38,5 +42,7 @@ public enum AppServiceTemplate {
         templatePath.put(AppServiceTemplate.SPRINGBOOT_TEMPLATE.templateName, String.format(FORMAT_MODAL, TEMPLATE_URL, SPRINGBOOT_PATH));
         templatePath.put(AppServiceTemplate.GO_TEMPLATE.templateName, String.format(FORMAT_MODAL, TEMPLATE_URL, GO_PATH));
         templatePath.put(AppServiceTemplate.CHOERODON_MOCHA_TEMPLATE.templateName, String.format(FORMAT_MODAL, TEMPLATE_URL, CHOERODON_MOCHA_PATH));
+        templatePath.put(AppServiceTemplate.CHOERODON_TESTNG_TEMPLATE.templateName, String.format(FORMAT_MODAL, TEMPLATE_URL, CHOERODON_TESTNG_TEMPLATE_PATH));
+        templatePath.put(AppServiceTemplate.CHOERODON_TESTNG_SELENIUM_TEMPLATE.templateName, String.format(FORMAT_MODAL, TEMPLATE_URL, CHOERODON_TESTNG_SELENIUM_TEMPLATE_PATH));
     }
 }
