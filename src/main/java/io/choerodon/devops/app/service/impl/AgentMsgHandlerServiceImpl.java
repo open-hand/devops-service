7dd9250e9f31f4cf1ff7ab051b4549c0757c5d07
WangXiang <957038053@qq.com>
2019-12-18 15:33:42 +0800

    [FIX]解决循环依赖的问题

--- src/main/java/io/choerodon/devops/app/service/impl/AgentMsgHandlerServiceImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/AgentMsgHandlerServiceImpl.java
@@ -18,6 +18,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
@@ -151,6 +152,7 @@ public class AgentMsgHandlerServiceImpl implements AgentMsgHandlerService {
     @Autowired
     private DevopsCertManagerMapper devopsCertManagerMapper;
     @Autowired
+    @Lazy
     private SendNotificationService sendNotificationService;
 
     public void handlerUpdatePodMessage(String key, String msg, Long envId) {
