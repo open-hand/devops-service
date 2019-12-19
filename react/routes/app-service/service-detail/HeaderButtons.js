c760c179b77e92c8a699b8b2d8e721c90b16fa87
NollieLeo <736653759@qq.com>
2019-12-19 18:02:08 +0800

    [IMP] 增加应用服务详情停用弹框，修改变革实例select样式

--- react/routes/app-service/service-detail/HeaderButtons.js
+++ react/routes/app-service/service-detail/HeaderButtons.js
@@ -1,18 +1,19 @@
-import React, { memo, useMemo } from 'react';
+import React, { memo, useMemo, Fragment } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Header, Permission, Choerodon } from '@choerodon/boot';
 import { Button, Tooltip } from 'choerodon-ui';
-import { Modal } from 'choerodon-ui/pro';
+import { Modal, Spin } from 'choerodon-ui/pro';
 import { observer } from 'mobx-react-lite';
 import { useAppTopStore } from '../stores';
 import { useServiceDetailStore } from './stores';
 import Detail from './modals/detail';
 import CreateForm from '../modals/creat-form';
 import EditForm from '../modals/edit-form';
-
+import { handlePromptError } from '../../../utils';
 
 const modalKey1 = Modal.key();
 const modalKey2 = Modal.key();
+const modalKey3 = Modal.key();
 const editModalKey = Modal.key();
 const modalStyle = {
   width: 380,
@@ -27,7 +28,7 @@ const HeaderButtons = observer(({ children }) => {
   } = useAppTopStore();
   const {
     intl: { formatMessage },
-    AppState: { currentMenuType: { id } },
+    AppState: { currentMenuType: { id: projectId } },
     detailDs,
   } = useServiceDetailStore();
 
@@ -75,12 +76,62 @@ const HeaderButtons = observer(({ children }) => {
     return <FormattedMessage id={`${intlPrefix}.${active}`} />;
   }
 
+  function openStop(record) {
+    const id = record.get('id');
+
+    const stopModal = Modal.open({
+      key: modalKey3,
+      title: formatMessage({ id: `${intlPrefix}.check` }),
+      children: <Spin />,
+      footer: null,
+    });
+
+    appServiceStore.checkAppService(projectId, id).then((res) => {
+      if (handlePromptError(res)) {
+        const { checkResources, checkRule } = res;
+        const status = checkResources || checkRule;
+        let childrenContent;
+
+        if (!status) {
+          childrenContent = <FormattedMessage id={`${intlPrefix}.stop.tips`} />;
+        } else if (checkResources && !checkRule) {
+          childrenContent = formatMessage({ id: `${intlPrefix}.has.resource` });
+        } else if (!checkResources && checkRule) {
+          childrenContent = formatMessage({ id: `${intlPrefix}.has.rules` });
+        } else {
+          childrenContent = formatMessage({ isd: `${intlPrefix}.has.both` });
+        }
+
+        const statusObj = {
+          title: status ? formatMessage({ id: `${intlPrefix}.cannot.stop` }) : formatMessage({ id: `${intlPrefix}.stop` }, { name: detailDs.current.get('name') }),
+          // eslint-disable-next-line no-nested-ternary
+          children: childrenContent,
+          okCancel: !status,
+          onOk: () => (status ? stopModal.close() : handleChangeActive(false)),
+          okText: status ? formatMessage({ id: 'iknow' }) : formatMessage({ id: 'stop' }),
+          footer: ((okBtn, cancelBtn) => (
+            <Fragment>
+              {okBtn}
+              {!status && cancelBtn}
+            </Fragment>
+          )),
+        };
+        stopModal.update(statusObj);
+      } else {
+        stopModal.close();
+      }
+    }).catch((err) => {
+      stopModal.close();
+      Choerodon.handleResponseError(err);
+    });
+  }
+
   async function changeActive() {
     const { current } = detailDs;
     if (current.get('active')) {
       Modal.open({
         key: modalKey2,
-        title: formatMessage({ id: `${intlPrefix}.stop` }, { name: current.get('name') }),
+        title: formatMessage({ id: `${intlPrefix}.stop` }, { name: detailDs.current.get('name') }),
         children: <FormattedMessage id={`${intlPrefix}.stop.tips`} />,
         onOk: () => handleChangeActive(false),
         okText: formatMessage({ id: 'stop' }),
@@ -93,7 +144,7 @@ const HeaderButtons = observer(({ children }) => {
   async function handleChangeActive(active) {
     const { current } = detailDs;
     try {
-      if (await appServiceStore.changeActive(id, current.get('id'), active) !== false) {
+      if (await appServiceStore.changeActive(projectId, current.get('id'), active) !== false) {
         detailDs.query();
       } else {
         return false;
@@ -128,7 +179,7 @@ const HeaderButtons = observer(({ children }) => {
       >
         <Button
           icon={serviceActive ? 'remove_circle_outline' : 'finished'}
-          onClick={changeActive}
+          onClick={serviceActive ? openStop.bind(this, detailDs.current) : handleChangeActive.bind(this, true)}
         >
           {getActiveText()}
         </Button>
--- react/routes/resource/main-view/contents/instance/modals/upgrade/index.js
+++ react/routes/resource/main-view/contents/instance/modals/upgrade/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { StoreProvider } from './stores';
 import Content from './Content';
 
+import './index.less';
+
 export default (props) => (
   <StoreProvider {...props}>
     <Content />
