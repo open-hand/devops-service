07c195421f989299859a67b1402d246afcc20c87
scp <changping.shi@hand-china.com>
2019-12-24 10:32:43 +0800

    [IMP] 优化流水线编辑

--- src/main/java/io/choerodon/devops/infra/util/TypeUtil.java
+++ src/main/java/io/choerodon/devops/infra/util/TypeUtil.java
@@ -1,5 +1,6 @@
 package io.choerodon.devops.infra.util;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -112,6 +113,32 @@ public class TypeUtil {
         }
 
         return mapParams;
+    }
+
+    /**
+     * 判断对象中属性值是否全为空
+     *
+     * @param object
+     * @return
+     */
+    public static boolean checkObjAllFieldsIsNull(Object object) {
+        if (null == object) {
+            return true;
+        }
+
+        try {
+            for (Field f : object.getClass().getDeclaredFields()) {
+                f.setAccessible(true);
+                if (f.get(object) != null && StringUtils.isNotBlank(f.get(object).toString())) {
+                    return false;
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return true;
     }
 
     public static <T> List<T> getListWithType(Map<Class, List> map, Class<T> key) {
