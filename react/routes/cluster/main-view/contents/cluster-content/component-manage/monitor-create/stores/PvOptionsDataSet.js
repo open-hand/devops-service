8ff0e0e667c7b5bff7b624074a977a52db89ace9
20399 <yanfang.lin@hand-china.com>
2020-01-06 11:16:50 +0800

    [IMP]安装prometheus筛选pv条件修改，添加域名校验

--- react/routes/cluster/main-view/contents/cluster-content/component-manage/monitor-create/stores/PvOptionsDataSet.js
+++ react/routes/cluster/main-view/contents/cluster-content/component-manage/monitor-create/stores/PvOptionsDataSet.js
@@ -9,6 +9,7 @@ export default (projectId, clusterId) => ({
         params: [],
         searchParam: {
           status: 'Available',
+          accessModes: 'ReadWriteOnly',
         },
       },
     },
