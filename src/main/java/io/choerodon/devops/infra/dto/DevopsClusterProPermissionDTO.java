bf353013c708f1d6826b3e3ddcb46d78c2fa3ce0
zmf <1984654893@qq.com>
2020-02-13 20:16:31 +0800

    [FIX] fix devops_cluster_pro_rel primary key

--- src/main/java/io/choerodon/devops/infra/dto/DevopsClusterProPermissionDTO.java
+++ src/main/java/io/choerodon/devops/infra/dto/DevopsClusterProPermissionDTO.java
@@ -1,5 +1,7 @@
 package io.choerodon.devops.infra.dto;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -8,11 +10,21 @@ import io.choerodon.mybatis.entity.BaseDTO;
 
 @Table(name = "devops_cluster_pro_rel")
 public class DevopsClusterProPermissionDTO extends BaseDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Id
     private Long clusterId;
-    @Id
+
     private Long projectId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public Long getClusterId() {
         return clusterId;
