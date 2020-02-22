4fbb4f8b24d903cad4e723546302283fa7f4f4c2
20399 <yanfang.lin@hand-china.com>
2020-02-22 13:05:15 +0800

    [IMP] 环境健康检查空界面判断改成全部实例

--- react/routes/resource/main-view/contents/environment/stores/useStore.js
+++ react/routes/resource/main-view/contents/environment/stores/useStore.js
@@ -67,7 +67,7 @@ export default function useStore({ defaultTab }) {
 
     async checkHasInstance(projectId, envId) {
       try {
-        const res = await axios.get(`devops/v1/projects/${projectId}/app_service_instances/count_by_options?env_id=${envId}&status=running&app_service_id=`);
+        const res = await axios.get(`devops/v1/projects/${projectId}/app_service_instances/count_by_options?env_id=${envId}&status=&app_service_id=`);
         const result = handlePromptError(res);
         this.setHasInstance(result);
         return result;
