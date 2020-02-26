f4cb346a37dee659216922e164a8d8a8dc9e6e6b
scp <changping.shi@hand-china.com>
2020-02-26 16:20:31 +0800

    [ADD] 添加webhook测试接口

--- src/main/java/io/choerodon/devops/api/controller/v1/DevopsCheckController.java
+++ src/main/java/io/choerodon/devops/api/controller/v1/DevopsCheckController.java
@@ -1,21 +1,25 @@
 package io.choerodon.devops.api.controller.v1;
 
+import com.google.gson.Gson;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import io.choerodon.core.annotation.Permission;
+import io.choerodon.core.notify.NoticeSendDTO;
 import io.choerodon.devops.app.service.DevopsCheckLogService;
 
 @RestController
 @RequestMapping(value = "/v1/upgrade")
 public class  DevopsCheckController {
+
+    private final Logger logger = LoggerFactory.getLogger(DevopsCheckController.class);
+    private Gson gson = new Gson();
 
     @Autowired
     private DevopsCheckLogService devopsCheckLogService;
@@ -35,4 +39,19 @@ public class  DevopsCheckController {
         return new ResponseEntity<>(System.currentTimeMillis() + "", HttpStatus.OK);
     }
 
+    /**
+     * 给po的webhook json格式测试接口
+     * 发版本之前删除
+     * 各位大佬看见请提醒删除
+     *
+     * @return
+     */
+    @Permission(permissionPublic = true)
+    @ApiOperation(value = "webhook测试接口")
+    @PostMapping(value = "/test")
+    public ResponseEntity<String> testJson(
+            @RequestBody NoticeSendDTO dto) {
+        logger.info("测试webhook内容：{}", gson.toJson(dto));
+        return new ResponseEntity<>(System.currentTimeMillis() + "", HttpStatus.OK);
+    }
 }
