32d6391cb7a0058ace903e4a4657148527f4b998
zmf <1984654893@qq.com>
2020-03-27 15:47:59 +0800

    [ADD] add log for pod log

--- src/main/java/io/choerodon/devops/api/ws/log/devops/DevopsLogSocketHandlerRegistration.java
+++ src/main/java/io/choerodon/devops/api/ws/log/devops/DevopsLogSocketHandlerRegistration.java
@@ -1,13 +1,14 @@
 package io.choerodon.devops.api.ws.log.devops;
 
-import io.choerodon.devops.api.ws.DevopsExecAndLogSocketHandler;
-import io.choerodon.websocket.connect.SocketHandlerRegistration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketSession;
+
+import io.choerodon.devops.api.ws.DevopsExecAndLogSocketHandler;
+import io.choerodon.websocket.connect.SocketHandlerRegistration;
 
 /**
  * Created by Sheep on 2019/8/7.
