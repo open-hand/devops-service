341928a130a637de65ce5b2f4717194660dc601f
王灏 <2507172753@qq.com>
2020-03-27 17:51:51 +0800

    [del]删除多余代码

--- src/main/java/io/choerodon/devops/infra/feign/operator/BaseServiceClientOperator.java
+++ src/main/java/io/choerodon/devops/infra/feign/operator/BaseServiceClientOperator.java
@@ -56,9 +56,6 @@ public class BaseServiceClientOperator {
         if (projectDTO == null || projectDTO.getId() == null) {
             throw new CommonException("error.project.query.by.id", projectId);
         }
-        if (Objects.isNull(projectDTOResponseEntity.getBody().getId())) {
-            throw new CommonException("not.project.id", projectId);
-        }
         return projectDTOResponseEntity.getBody();
     }
 
