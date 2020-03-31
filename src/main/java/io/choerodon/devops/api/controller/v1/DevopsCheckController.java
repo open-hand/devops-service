9696464ce10db87ee775addfacb8a3aeb0590aa6
zmf <1984654893@qq.com>
2020-03-31 10:13:56 +0800

    [DEL] delete unnecessary dependency for DevopsCheckController

--- src/main/java/io/choerodon/devops/api/controller/v1/DevopsCheckController.java
+++ src/main/java/io/choerodon/devops/api/controller/v1/DevopsCheckController.java
@@ -1,25 +1,21 @@
 package io.choerodon.devops.api.controller.v1;
 
-import com.google.gson.Gson;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.choerodon.core.annotation.Permission;
-import io.choerodon.core.notify.NoticeSendDTO;
 import io.choerodon.devops.app.service.DevopsCheckLogService;
 
 @RestController
 @RequestMapping(value = "/v1/upgrade")
 public class  DevopsCheckController {
-
-    private final Logger logger = LoggerFactory.getLogger(DevopsCheckController.class);
-    private Gson gson = new Gson();
 
     @Autowired
     private DevopsCheckLogService devopsCheckLogService;
