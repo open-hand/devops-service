a3c0a91ee2a8c44124a7573531a7bedf7ac7400f
20399 <yanfang.lin@hand-china.com>
2020-04-09 14:47:33 +0800

    [IMP] 流水线全新执行分支下拉框优化

--- react/routes/pipeline-manage/components/PipelineTree/execute-content/stores/index.js
+++ react/routes/pipeline-manage/components/PipelineTree/execute-content/stores/index.js
@@ -3,6 +3,7 @@ import { inject } from 'mobx-react';
 import { injectIntl } from 'react-intl';
 import { DataSet } from 'choerodon-ui/pro';
 import SelectDataSet from './SelectDataSet';
+import useStore from './useStore';
 
 const Store = createContext();
 
@@ -15,13 +16,21 @@ export const StoreProvider = injectIntl(inject('AppState')((props) => {
     AppState: { currentMenuType: { projectId } },
     intl: { formatMessage },
     children,
+    appServiceId,
   } = props;
 
+  const store = useStore();
   const selectDs = useMemo(() => new DataSet(SelectDataSet({ projectId, formatMessage })), [projectId]);
+
+  useEffect(() => {
+    store.loadBranchData({ projectId, appServiceId, page: 1 });
+    store.loadTagData({ projectId, appServiceId, page: 1 });
+  }, []);
 
   const value = {
     ...props,
     selectDs,
+    store,
   };
 
   return (
