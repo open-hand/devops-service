f428eb01d7183dc7ed36292dcfc0559bd5335da9
zmf <1984654893@qq.com>
2020-05-11 16:55:40 +0800

    [REM] remove unnecesary import

--- src/main/java/io/choerodon/devops/DevopsServiceApplication.java
+++ src/main/java/io/choerodon/devops/DevopsServiceApplication.java
@@ -1,6 +1,5 @@
 package io.choerodon.devops;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
