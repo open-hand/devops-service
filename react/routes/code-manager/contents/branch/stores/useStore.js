627353111100029dcc8a864e099caed29550cecf
20399 <yanfang.lin@hand-china.com>
2020-06-18 10:50:52 +0800

    [IMP] 创建分支、标记添加校验

--- react/routes/code-manager/contents/branch/stores/useStore.js
+++ react/routes/code-manager/contents/branch/stores/useStore.js
@@ -11,5 +11,9 @@ export default function useStore() {
       return this.isEmpty;
     },
 
+    checkCreate(projectId, appServiceId) {
+      return axios.get(`/devops/v1/projects/${projectId}/member-check/${appServiceId}?type=BRANCH_CREATE`);
+    },
+
   }));
 }
