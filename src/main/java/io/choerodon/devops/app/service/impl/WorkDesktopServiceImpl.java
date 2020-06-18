afcd617f17bfbffc61222c84980dc4b4c550a258
lihao <310103270@qq.com>
2020-06-18 11:52:10 +0800

    [IMP] 优化待审核接口和最近使用应用服务接口查询逻辑

--- src/main/java/io/choerodon/devops/app/service/impl/WorkDesktopServiceImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/WorkDesktopServiceImpl.java
@@ -17,6 +17,7 @@ import io.choerodon.devops.infra.enums.ApprovalTypeEnum;
 import io.choerodon.devops.infra.feign.operator.BaseServiceClientOperator;
 import io.choerodon.devops.infra.mapper.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -29,31 +30,30 @@ import java.util.stream.Collectors;
 public class WorkDesktopServiceImpl implements WorkDesktopService {
     private static final String MERGE_REQUEST_CONTENT_FORMAT = "%s (%s)在应用服务“%s”中提交了合并请求";
     private static final String PIPELINE_CONTENT_FORMAT = "流水线 “%s” 目前暂停于【%s】阶段，需要您进行审核";
-    private static final String ORGANIZATION_NAME_AND_PROJECT_NAME = "%s-%s";
-
+    private static final String MERGE_REQUEST_URL = "%s/%s-%s/%s/merge_requests/%d";
+    @Autowired
+    DevopsGitService devopsGitService;
+    @Value("${services.gitlab.url}")
+    private String gitlabUrl;
     @Autowired
     private DevopsMergeRequestMapper devopsMergeRequestMapper;
-
     @Autowired
     private AppServiceMapper appServiceMapper;
-
     @Autowired
     private DevopsBranchMapper devopsBranchMapper;
-
     @Autowired
     private DevopsGitlabCommitMapper devopsGitlabCommitMapper;
-
     @Autowired
     private PipelineStageRecordMapper pipelineStageRecordMapper;
-
     @Autowired
     private UserAttrService userAttrService;
-
     @Autowired
     private BaseServiceClientOperator baseServiceClientOperator;
 
-    @Autowired
-    DevopsGitService devopsGitService;
+    private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        Set<Object> seen = ConcurrentHashMap.newKeySet();
+        return t -> seen.add(keyExtractor.apply(t));
+    }
 
     @Override
     public List<LatestAppServiceVO> listLatestAppService(Long organizationId, Long projectId) {
@@ -76,26 +76,26 @@ public class WorkDesktopServiceImpl implements WorkDesktopService {
         } else {
             projectDTOList = Collections.singletonList(baseServiceClientOperator.queryIamProjectById(projectId));
         }
-        return listApprovalVOByProject(projectDTOList);
+        return listApprovalVOByProject(tenant, projectDTOList);
     }
 
-    private List<ApprovalVO> listApprovalVOByProject(List<ProjectDTO> projectDTOList) {
+    private List<ApprovalVO> listApprovalVOByProject(Tenant tenant, List<ProjectDTO> projectDTOList) {
         List<ApprovalVO> approvalVOList = new ArrayList<>();
         List<Long> projectIds = projectDTOList.stream().map(ProjectDTO::getId).collect(Collectors.toList());
-        Map<Long, String> projectNameMap = projectDTOList.stream().collect(Collectors.toMap(ProjectDTO::getId, ProjectDTO::getName));
+        Map<Long, ProjectDTO> projectMap = projectDTOList.stream().collect(Collectors.toMap(ProjectDTO::getId, v -> v));
         // 1.查询合并请求
         List<AppServiceDTO> appServiceDTOList = appServiceMapper.listByActiveAndProjects(projectIds);
-        approvalVOList.addAll(listMergeRequestApproval(projectNameMap, appServiceDTOList));
+        approvalVOList.addAll(listMergeRequestApproval(tenant, projectMap, appServiceDTOList));
 
         // 2.查出流水线请求
-        approvalVOList.addAll(listPipelineApproval(projectNameMap, projectIds));
+        approvalVOList.addAll(listPipelineApproval(projectMap, projectIds));
         return approvalVOList;
     }
 
-    private List<ApprovalVO> listMergeRequestApproval(Map<Long, String> projectNameMap, List<AppServiceDTO> appServiceDTOList) {
+    private List<ApprovalVO> listMergeRequestApproval(Tenant tenant, Map<Long, ProjectDTO> projectMap, List<AppServiceDTO> appServiceDTOList) {
         List<ApprovalVO> approvalVOList = new ArrayList<>();
         List<Integer> gitlabProjectIds = appServiceDTOList.stream().map(AppServiceDTO::getGitlabProjectId).collect(Collectors.toList());
-        Map<Integer, String> gitlabProjectAndAppNameMap = appServiceDTOList.stream().collect(Collectors.toMap(AppServiceDTO::getGitlabProjectId, AppServiceDTO::getName));
+        Map<Integer, AppServiceDTO> gitlabProjectAndAppMap = appServiceDTOList.stream().collect(Collectors.toMap(AppServiceDTO::getGitlabProjectId, v -> v));
         // 查出该用户待审批的合并请求
         List<DevopsMergeRequestDTO> mergeRequestDTOList = new ArrayList<>();
         if (gitlabProjectIds.size() != 0) {
@@ -105,24 +105,25 @@ public class WorkDesktopServiceImpl implements WorkDesktopService {
         Set<Long> authorIds = mergeRequestDTOList.stream().map(DevopsMergeRequestDTO::getAuthorId).collect(Collectors.toSet());
         List<UserAttrVO> userAttrDTOList = userAttrService.listUsersByGitlabUserIds(authorIds);
         List<Long> iamUserIds = userAttrDTOList.stream().map(UserAttrVO::getIamUserId).collect(Collectors.toList());
-        Map<Long, List<IamUserDTO>> iamUserDTOMap = baseServiceClientOperator.queryUsersByUserIds(iamUserIds).stream().collect(Collectors.groupingBy(IamUserDTO::getId));
-        Map<Long, List<UserAttrVO>> userAttrVO = userAttrDTOList.stream().collect(Collectors.groupingBy(UserAttrVO::getGitlabUserId));
+        Map<Long, IamUserDTO> iamUserDTOMap = baseServiceClientOperator.queryUsersByUserIds(iamUserIds).stream().collect(Collectors.toMap(IamUserDTO::getId, v -> v));
+        Map<Long, UserAttrVO> userAttrVO = userAttrDTOList.stream().collect(Collectors.toMap(UserAttrVO::getGitlabUserId, v -> v));
         mergeRequestDTOList.forEach(devopsMergeRequestDTO -> {
-            IamUserDTO iamUserDTO = iamUserDTOMap.get(userAttrVO.get(devopsMergeRequestDTO.getAuthorId()).get(0).getIamUserId()).get(0);
+            IamUserDTO iamUserDTO = iamUserDTOMap.get(userAttrVO.get(devopsMergeRequestDTO.getAuthorId()).getIamUserId());
             ApprovalVO approvalVO = new ApprovalVO()
                     .setImageUrl(iamUserDTO.getImageUrl())
                     .setType(ApprovalTypeEnum.MERGE_REQUEST.getType())
+                    .setMergeRequestUrl(String.format(MERGE_REQUEST_URL, gitlabUrl, tenant.getTenantNum(), projectMap.get(devopsMergeRequestDTO.getProjectId()).getCode(), gitlabProjectAndAppMap.get(devopsMergeRequestDTO.getGitlabProjectId().intValue()).getCode(), devopsMergeRequestDTO.getGitlabMergeRequestId()))
                     .setProjectId(devopsMergeRequestDTO.getProjectId())
-                    .setProjectName(projectNameMap.get(devopsMergeRequestDTO.getProjectId()))
+                    .setProjectName(projectMap.get(devopsMergeRequestDTO.getProjectId()).getName())
                     .setGitlabProjectId(devopsMergeRequestDTO.getGitlabProjectId().intValue())
-                    .setContent(String.format(MERGE_REQUEST_CONTENT_FORMAT, iamUserDTO.getRealName(), iamUserDTO.getId(), gitlabProjectAndAppNameMap.get(devopsMergeRequestDTO.getGitlabProjectId().intValue())));
+                    .setContent(String.format(MERGE_REQUEST_CONTENT_FORMAT, iamUserDTO.getRealName(), iamUserDTO.getLoginName(), gitlabProjectAndAppMap.get(devopsMergeRequestDTO.getGitlabProjectId().intValue()).getName()));
             approvalVOList.add(approvalVO);
         });
 
         return approvalVOList;
     }
 
-    private List<ApprovalVO> listPipelineApproval(Map<Long, String> projectNameMap, List<Long> projectIds) {
+    private List<ApprovalVO> listPipelineApproval(Map<Long, ProjectDTO> projectNameMap, List<Long> projectIds) {
         List<ApprovalVO> approvalVOList = new ArrayList<>();
 
         Long userId = DetailsHelper.getUserDetails().getUserId() == null ? 0 : DetailsHelper.getUserDetails().getUserId();
@@ -135,7 +136,7 @@ public class WorkDesktopServiceImpl implements WorkDesktopService {
             ApprovalVO approvalVO = new ApprovalVO()
                     .setType(ApprovalTypeEnum.PIPE_LINE.getType())
                     .setProjectId(pipelineRecordDTO.getProjectId())
-                    .setProjectName(projectNameMap.get(pipelineRecordDTO.getProjectId()))
+                    .setProjectName(projectNameMap.get(pipelineRecordDTO.getProjectId()).getName())
                     .setContent(String.format(PIPELINE_CONTENT_FORMAT, pipelineRecordDTO.getPipelineName(), pipelineRecordDTO.getStageName()))
                     .setPipelineId(pipelineRecordDTO.getPipelineId())
                     .setPipelineRecordId(pipelineRecordDTO.getId())
@@ -148,7 +149,7 @@ public class WorkDesktopServiceImpl implements WorkDesktopService {
 
     private List<LatestAppServiceVO> listLatestUserAppServiceDTO(List<ProjectDTO> projectDTOList) {
         List<Long> projectIds = projectDTOList.stream().map(ProjectDTO::getId).collect(Collectors.toList());
-        Map<Long, List<ProjectDTO>> projectDTOMap = projectDTOList.stream().collect(Collectors.groupingBy(ProjectDTO::getId));
+        Map<Long, ProjectDTO> projectDTOMap = projectDTOList.stream().collect(Collectors.toMap(ProjectDTO::getId, v -> v));
         Long userId = DetailsHelper.getUserDetails().getUserId() == null ? 0 : DetailsHelper.getUserDetails().getUserId();
         List<LatestAppServiceVO> latestAppServiceVOList = new ArrayList<>();
         latestAppServiceVOList.addAll(appServiceMapper.listLatestUseAppServiceIdAndDate(projectIds, userId));
@@ -166,22 +167,17 @@ public class WorkDesktopServiceImpl implements WorkDesktopService {
         List<LatestAppServiceVO> latestTenAppServiceList = latestAppServiceVOListWithoutRepeatService.subList(0, end);
 
         Set<Long> appServiceIds = latestTenAppServiceList.stream().map(LatestAppServiceVO::getId).collect(Collectors.toSet());
-        Map<Long, List<AppServiceDTO>> appServiceDTOMap = appServiceMapper.listAppServiceByIds(appServiceIds, null, null).stream().collect(Collectors.groupingBy(AppServiceDTO::getId));
+        Map<Long, AppServiceDTO> appServiceDTOMap = appServiceMapper.listAppServiceByIds(appServiceIds, null, null).stream().collect(Collectors.toMap(AppServiceDTO::getId, v -> v));
 
         latestTenAppServiceList.forEach(latestAppServiceVO -> {
-            AppServiceDTO appServiceDTO = appServiceDTOMap.get(latestAppServiceVO.getId()).get(0);
-            ProjectDTO projectDTO = projectDTOMap.get(appServiceDTO.getProjectId()).get(0);
+            AppServiceDTO appServiceDTO = appServiceDTOMap.get(latestAppServiceVO.getId());
+            ProjectDTO projectDTO = projectDTOMap.get(appServiceDTO.getProjectId());
             latestAppServiceVO.setProjectName(projectDTO.getName())
                     .setProjectId(appServiceDTO.getProjectId())
                     .setCode(appServiceDTO.getCode())
                     .setName(appServiceDTO.getName());
         });
         return latestTenAppServiceList;
-    }
-
-    private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
-        Set<Object> seen = ConcurrentHashMap.newKeySet();
-        return t -> seen.add(keyExtractor.apply(t));
     }
 
 }
