5b601599032725a5e3241b04bcaa7a08486ff00f
zmf <1984654893@qq.com>
2020-06-18 15:45:16 +0800

    [FIX] fix groovy

--- src/main/resources/script/db/groovy/devops_service/devops_ci_job.groovy
+++ src/main/resources/script/db/groovy/devops_service/devops_ci_job.groovy
@@ -36,9 +36,10 @@ databaseChangeLog(logicalFilePath: 'dba/devops_ci_job.groovy') {
 
     changeSet(author: 'zmf', id: '2020-06-18-job-add-trigger') {
         addColumn(tableName: 'devops_ci_job') {
-            column(name: 'trigger_type', type: 'VARCHAR(255)', remarks: '触发方式', afterColumn: 'trigger_refs')
+            column(name: 'trigger_type', type: 'VARCHAR(255)', remarks: '触发方式', afterColumn: 'trigger_refs', defaultValue: 'refs')
         }
         renameColumn(columnDataType: 'VARCHAR(255)', newColumnName: 'trigger_value', oldColumnName: 'trigger_refs', remarks: '触发方式对应的值', tableName: 'devops_ci_job')
         sql("UPDATE devops_ci_job dcj SET dcj.trigger_type = 'refs' WHERE dcj.trigger_value IS NOT NULL")
+        sql("UPDATE devops_ci_job dcj SET dcj.trigger_type = 'refs' WHERE dcj.trigger_type IS NULL")
     }
 }
