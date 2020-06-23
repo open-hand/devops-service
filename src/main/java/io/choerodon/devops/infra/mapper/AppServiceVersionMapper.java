0d2118f99916cf6cdabf82a81571a40511fc9ab8
zmf <1984654893@qq.com>
2020-06-24 01:49:31 +0800

    [REM] remove unused methods in app-service version mapper

--- src/main/java/io/choerodon/devops/infra/mapper/AppServiceVersionMapper.java
+++ src/main/java/io/choerodon/devops/infra/mapper/AppServiceVersionMapper.java
@@ -120,17 +120,6 @@ public interface AppServiceVersionMapper extends BaseMapper<AppServiceVersionDTO
 
     List<Long> selectAllAppServiceIdWithNullHarborConfig();
 
-    void updateVersionDefaultBatch(@Param("appServiceVersionDefault") List<AppServiceVersionDTO> appServiceVersionDefault);
-
-    void updateVersionCustomBatch(@Param("appServiceVersionDCustom")List<AppServiceVersionDTO> appServiceVersionDCustom);
-
-    void updateVersionAppServiceId(@Param("appServiceId") Long appServiceId,
-                                   @Param("configId") Long configId,
-                                   @Param("customRepo") String customRepo);
-
-    void updateVersionOrgId(@Param("appServiceId") Long appServiceId,
-                            @Param("customRepo") String defaultRepo);
-
     void updateNullHarborVersionToDefaultType(@Param("appServiceId") Long appServiceId);
 
     void updateNullHarborVersionToCustomType(@Param("appServiceId") Long appServiceId, @Param("configId") Long harborConfigId);
