a99b93bc8c87e40b41c627c9aecb3f4913430ada
isaac <273070111@qq.com>
2020-07-01 17:49:58 +0800

    [ADD]初始化cd流水线

---
+++ react/routes/pipeline-manage/components/PipelineCreate/components/AddCDTask/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+
+export default observer(() => (
+  <div>
+    123
+  </div>
+));
--- react/routes/pipeline-manage/components/PipelineFlow/components/stageEditBlock/stores/AddStepDataset.js
+++ react/routes/pipeline-manage/components/PipelineFlow/components/stageEditBlock/stores/AddStepDataset.js
@@ -1,10 +1,23 @@
 export default () => ({
   autoCreate: true,
   fields: [{
+    name: 'jdsx',
+    type: 'string',
+    label: '阶段属性',
+  }, {
     name: 'step',
     type: 'string',
-    label: '名称',
+    label: '阶段名称',
     required: true,
     maxLength: 15,
+  }, {
+    name: 'lzzcjd',
+    type: 'string',
+    label: '流转至此阶段',
+  }, {
+    name: 'shry',
+    type: 'string',
+    label: '审核人员',
+    multiple: true,
   }],
 });
