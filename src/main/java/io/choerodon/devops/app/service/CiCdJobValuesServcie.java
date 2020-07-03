35a29b3145fa6ff4e721f018b3c493bf9165522a
王灏 <2507172753@qq.com>
2020-07-03 11:44:36 +0800

    [imp]CD流水线表结构修改

--- src/main/java/io/choerodon/devops/app/service/CiCdJobValuesServcie.java
+++
@@ -1,9 +0,0 @@
-package io.choerodon.devops.app.service;
-
-import io.choerodon.devops.infra.dto.DevopsCdJobValuesDTO;
-
-public interface CiCdJobValuesServcie {
-    void create(DevopsCdJobValuesDTO devopsCdJobValuesDTO);
-
-    void deleteByPipelineId(Long ciCdPipelineId);
-}
