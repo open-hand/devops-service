2dc2fd0d2bfbe45acad0d77cf198bafa4935db7b
scp <changping.shi@hand-china.com>
2020-07-08 17:23:43 +0800

    [ADD] 添加cicd全新执行接口

--- src/main/java/io/choerodon/devops/app/service/impl/CiCdPipelineRecordServiceImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/CiCdPipelineRecordServiceImpl.java
@@ -1,29 +1,34 @@
 package io.choerodon.devops.app.service.impl;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
+import io.choerodon.core.exception.CommonException;
 import io.choerodon.core.oauth.CustomUserDetails;
 import io.choerodon.core.oauth.DetailsHelper;
 import io.choerodon.devops.api.vo.CiCdPipelineRecordVO;
 import io.choerodon.devops.api.vo.DevopsCdPipelineRecordVO;
 import io.choerodon.devops.api.vo.DevopsCiPipelineRecordVO;
+import io.choerodon.devops.api.vo.DevopsCiPipelineVO;
 import io.choerodon.devops.app.service.*;
-import io.choerodon.devops.infra.dto.DevopsCdJobRecordDTO;
-import io.choerodon.devops.infra.dto.DevopsCdPipelineRecordDTO;
-import io.choerodon.devops.infra.dto.DevopsCdStageRecordDTO;
+import io.choerodon.devops.infra.dto.*;
+import io.choerodon.devops.infra.dto.gitlab.CommitDTO;
 import io.choerodon.devops.infra.dto.workflow.DevopsPipelineDTO;
 import io.choerodon.devops.infra.enums.PipelineStatus;
 import io.choerodon.devops.infra.enums.WorkFlowStatus;
+import io.choerodon.devops.infra.feign.operator.GitlabServiceClientOperator;
 import io.choerodon.devops.infra.feign.operator.WorkFlowServiceOperator;
-import io.choerodon.devops.infra.mapper.DevopsCdJobRecordMapper;
-import io.choerodon.devops.infra.mapper.DevopsCdStageRecordMapper;
+import io.choerodon.devops.infra.mapper.*;
 import io.choerodon.devops.infra.util.GenerateUUID;
 import io.choerodon.devops.infra.util.GitUserNameUtil;
+import io.choerodon.devops.infra.util.TypeUtil;
 
 @Service
 public class CiCdPipelineRecordServiceImpl implements CiCdPipelineRecordService {
@@ -51,6 +56,18 @@ public class CiCdPipelineRecordServiceImpl implements CiCdPipelineRecordService 
 
     @Autowired
     private WorkFlowServiceOperator workFlowServiceOperator;
+
+    @Autowired
+    private DevopsCiPipelineService devopsCiPipelineService;
+
+    @Autowired
+    private DevopsCiStageMapper devopsCiStageMapper;
+
+    @Autowired
+    private AppServiceMapper appServiceMapper;
+
+    @Autowired
+    private GitlabServiceClientOperator gitlabServiceClientOperator;
 
 
     @Override
@@ -107,6 +124,24 @@ public class CiCdPipelineRecordServiceImpl implements CiCdPipelineRecordService 
             devopsCiPipelineRecordService.cancel(projectId, gitlabPipelineId, gitlabProjectId);
         } else {
             cancelCdPipeline(pipelineRecordId);
+        }
+    }
+
+
+    @Override
+    public void executeNew(Long projectId, Long pipelineId, Long gitlabProjectId, String ref) {
+        DevopsCiStageDTO devopsCdStageDTO = new DevopsCiStageDTO();
+        devopsCdStageDTO.setCiPipelineId(pipelineId);
+        if (devopsCiStageMapper.selectCount(devopsCdStageDTO) == 0) {
+            DevopsCiPipelineVO devopsCiPipelineDTO = devopsCiPipelineService.queryById(pipelineId);
+            AppServiceDTO appServiceDTO = appServiceMapper.selectByPrimaryKey(devopsCiPipelineDTO.getAppServiceId());
+            List<CommitDTO> commitDTOList = gitlabServiceClientOperator.getCommits(TypeUtil.objToInteger(gitlabProjectId), ref, null);
+            if (CollectionUtils.isEmpty(commitDTOList)) {
+                throw new CommonException("error.ref.no.commit");
+            }
+            devopsCdPipelineService.triggerCdPipeline(appServiceDTO.getToken(), commitDTOList.get(0).getId(), ref, null);
+        } else {
+            devopsCiPipelineService.executeNew(projectId, pipelineId, gitlabProjectId, ref);
         }
     }
 
