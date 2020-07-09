de0bfc029e546a1ccd94fd694ccc2aadcc5906db
isaac <273070111@qq.com>
2020-07-09 10:48:23 +0800

    [ADD]增加流水线功能

--- react/routes/pipeline-manage/components/PipelineFlow/components/eidtColumn/AddStage.js
+++ react/routes/pipeline-manage/components/PipelineFlow/components/eidtColumn/AddStage.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Form, Select, SelectBox, TextField } from 'choerodon-ui/pro';
 
 const { Option } = Select;
 
-export default observer(({ addStepDs, curType, optType }) => (
-  <Form className="addStageForm" dataSet={addStepDs}>
-    <Select showHelp="tooltip" name="type" help="123">
-      <Option disabled={curType && (curType === 'CD')} value="CI">CI阶段</Option>
-      <Option value="CD">CD阶段</Option>
-    </Select>
-    <TextField name="step" />
-    {
-      addStepDs?.current?.get('type') === 'CD' ? (
-        <SelectBox name="triggerType">
-          <Option value="auto">自动流转</Option>
-          <Option value="manual">手动流转</Option>
-        </SelectBox>
-      ) : ''
-    }
-    {
-      addStepDs?.current?.get('triggerType') === 'manual' ? (
-        <Select showHelp="tooltip" help="123" name="cdAuditUserIds">
-          <Option value="1">李丹丹(9221)</Option>
-        </Select>
-      ) : ''
-    }
-  </Form>
-));
+export default observer(({ addStepDs, curType, optType }) => {
+  useEffect(() => {
+    const type = addStepDs?.current?.get('type');
+    addStepDs.current.set('parallel', type === 'CI' ? 1 : 0);
+  }, [addStepDs?.current?.get('type')]);
+
+  return (
+    <Form className="addStageForm" dataSet={addStepDs}>
+      <Select showHelp="tooltip" name="type" help="123">
+        <Option disabled={curType && (curType === 'CD')} value="CI">CI阶段</Option>
+        <Option value="CD">CD阶段</Option>
+      </Select>
+      <TextField name="step" />
+      <Select name="parallel">
+        <Option value={0}>任务串行</Option>
+        <Option value={1}>任务并行</Option>
+      </Select>
+      {
+        addStepDs?.current?.get('type') === 'CD' ? (
+          <SelectBox name="triggerType">
+            <Option value="auto">自动流转</Option>
+            <Option value="manual">手动流转</Option>
+          </SelectBox>
+        ) : ''
+      }
+      {
+        addStepDs?.current?.get('triggerType') === 'manual' ? (
+          <Select showHelp="tooltip" help="123" name="cdAuditUserIds" />
+        ) : ''
+      }
+    </Form>
+  );
+});
