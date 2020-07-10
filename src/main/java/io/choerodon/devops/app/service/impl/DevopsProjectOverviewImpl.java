d914721b6ffe54901035ed8b3bd6007dde8bf32e
lihao <310103270@qq.com>
2020-07-10 18:29:30 +0800

    [IMP] 优化提交记录、部署记录查询逻辑

--- src/main/java/io/choerodon/devops/app/service/impl/DevopsProjectOverviewImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/DevopsProjectOverviewImpl.java
@@ -1,9 +1,9 @@
 package io.choerodon.devops.app.service.impl;
 
+import io.choerodon.devops.app.service.AppServiceInstanceService;
 import io.choerodon.devops.app.service.DevopsProjectOverview;
 import io.choerodon.devops.infra.dto.AppServiceDTO;
-import io.choerodon.devops.infra.dto.AppServiceInstanceDTO;
-import io.choerodon.devops.infra.dto.DevopsEnvCommandDTO;
+import io.choerodon.devops.infra.dto.DeployDTO;
 import io.choerodon.devops.infra.dto.DevopsEnvironmentDTO;
 import io.choerodon.devops.infra.dto.agile.SprintDTO;
 import io.choerodon.devops.infra.dto.iam.ProjectDTO;
@@ -15,10 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -37,6 +34,9 @@ public class DevopsProjectOverviewImpl implements DevopsProjectOverview {
 
     @Autowired
     private AgileServiceClientOperator agileServiceClientOperator;
+
+    @Autowired
+    private AppServiceInstanceService appServiceInstanceService;
 
     @Autowired
     private DevopsEnvironmentMapper devopsEnvironmentMapper;
@@ -98,9 +98,10 @@ public class DevopsProjectOverviewImpl implements DevopsProjectOverview {
     public Map<String, Long> getCommitCount(Long projectId) {
         ProjectDTO projectDTO = baseServiceClientOperator.queryIamProjectById(projectId);
         SprintDTO sprintDTO = agileServiceClientOperator.getActiveSprint(projectId, projectDTO.getOrganizationId());
-        List<Date> dateList = devopsGitlabCommitMapper.queryCountByProjectIdAndCreationDate(projectId, sprintDTO.getStartDate());
+        List<Date> dateList = devopsGitlabCommitMapper.queryCountByProjectIdAndDate(projectId, new java.sql.Date(sprintDTO.getStartDate().getTime()), new java.sql.Date(sprintDTO.getEndDate().getTime()));
 
         return dateList.stream().map(simpleDateFormat::format)
+                .sorted(Comparator.naturalOrder())
                 .collect(Collectors.groupingBy(t -> t, Collectors.counting()));
     }
 
@@ -109,35 +110,38 @@ public class DevopsProjectOverviewImpl implements DevopsProjectOverview {
         ProjectDTO projectDTO = baseServiceClientOperator.queryIamProjectById(projectId);
 
         // 该项目下所有启用环境
-        List<Long> updatedClusterList = clusterConnectionHandler.getUpdatedClusterList();
-
         List<DevopsEnvironmentDTO> devopsEnvironmentDTOS = devopsEnvironmentMapper.listByProjectId(projectId);
 
-        List<Long> activeEnvIds = devopsEnvironmentDTOS.stream()
-                .filter(t -> isEnvUp(updatedClusterList, t))
+        List<Long> envIds = devopsEnvironmentDTOS.stream()
                 .map(DevopsEnvironmentDTO::getId)
                 .collect(Collectors.toList());
 
-        // 查出本项目下的所有应用服务
-        Map<String, Object> searchParam = new HashMap<>();
-        searchParam.put("active", 1);
-        List<Long> appServiceIds = appServiceMapper.listByProjectId(projectId, searchParam, null).stream().map(AppServiceDTO::getId).collect(Collectors.toList());
-
-        // 根据启用环境查出所有instance
-
         SprintDTO sprintDTO = agileServiceClientOperator.getActiveSprint(projectId, projectDTO.getOrganizationId());
 
-        List<Long> instanceIds = appServiceInstanceMapper.queryInstanceByAppServiceIds(appServiceIds).stream()
-                .filter(i -> activeEnvIds.contains(i.getEnvId()))
-                .map(AppServiceInstanceDTO::getId)
+        if (envIds.size() == 0) {
+            return new HashMap<>();
+        }
+
+        List<DeployDTO> deployDTOS = appServiceInstanceService.baseListDeployFrequency(projectId, envIds.toArray(new Long[0]), null, sprintDTO.getStartDate(), sprintDTO.getEndDate());
+
+        //以时间维度分组
+        Map<String, List<DeployDTO>> resultMaps = deployDTOS.stream()
+                .collect(Collectors.groupingBy(t -> new java.sql.Date(t.getCreationDate().getTime()).toString()));
+
+        List<String> creationDates = deployDTOS.stream()
+                .map(deployDTO -> new java.sql.Date(deployDTO.getCreationDate().getTime()).toString())
+                .collect(Collectors.toList());
+        creationDates = new ArrayList<>(new HashSet<>(creationDates)).stream().sorted(Comparator.naturalOrder())
                 .collect(Collectors.toList());
 
-        // 查询从自迭代开启到现在,每个实例的部署操作记录
 
-        List<DevopsEnvCommandDTO> instance = devopsEnvCommandMapper.listByInstanceIdsAndStartDate("instance", instanceIds, sprintDTO.getStartDate());
-
-        return instance.stream().map(simpleDateFormat::format)
-                .collect(Collectors.groupingBy(t -> t, Collectors.counting()));
+        Map<String, Long> count = new HashMap<>();
+        creationDates.forEach(date -> {
+            Long[] newDeployFrequency = {0L};
+            resultMaps.get(date).forEach(deployFrequencyDO -> newDeployFrequency[0] = newDeployFrequency[0] + 1L);
+            count.put(date, newDeployFrequency[0]);
+        });
+        return count;
     }
 
     private boolean isEnvUp(List<Long> updatedClusterList, DevopsEnvironmentDTO t) {
