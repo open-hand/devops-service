853ea8bce446c2fccb72b483f7420af5738d03b9
王灏 <2507172753@qq.com>
2020-07-11 16:47:14 +0800

    [imp]自动部署任务查询信息metadata修改

---
+++ src/main/java/io/choerodon/devops/api/vo/DevopsCdEnvDeployInfoVO.java
@@ -0,0 +1,74 @@
+package io.choerodon.devops.api.vo;
+
+/**
+ * 〈功能简述〉
+ * 〈〉
+ *
+ * @author wanghao
+ * @since 2020/7/11 16:45
+ */
+public class DevopsCdEnvDeployInfoVO {
+    private Long appServiceId;
+    private Long envId;
+    private Long valueId;
+    private Long projectId;
+    private String deployType;  // 部署类型：新建实例 create 替换实例 update
+    private Long instanceId;    // 替换实例时需要
+    private String instanceName;    // 新建实例时需要
+
+    public Long getAppServiceId() {
+        return appServiceId;
+    }
+
+    public void setAppServiceId(Long appServiceId) {
+        this.appServiceId = appServiceId;
+    }
+
+    public Long getEnvId() {
+        return envId;
+    }
+
+    public void setEnvId(Long envId) {
+        this.envId = envId;
+    }
+
+    public Long getValueId() {
+        return valueId;
+    }
+
+    public void setValueId(Long valueId) {
+        this.valueId = valueId;
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Long projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getDeployType() {
+        return deployType;
+    }
+
+    public void setDeployType(String deployType) {
+        this.deployType = deployType;
+    }
+
+    public Long getInstanceId() {
+        return instanceId;
+    }
+
+    public void setInstanceId(Long instanceId) {
+        this.instanceId = instanceId;
+    }
+
+    public String getInstanceName() {
+        return instanceName;
+    }
+
+    public void setInstanceName(String instanceName) {
+        this.instanceName = instanceName;
+    }
+}
