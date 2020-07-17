2b374801651fe919dfb98afaaa2e0c2c38f8d066
scp <changping.shi@hand-china.com>
2020-07-17 10:43:53 +0800

    [IMP] 修改values文件

--- init-database.sh
+++ init-database.sh
@@ -2,7 +2,7 @@
 MAVEN_LOCAL_REPO=$(cd / && mvn help:evaluate -Dexpression=settings.localRepository -q -DforceStdout)
 TOOL_GROUP_ID=io.choerodon
 TOOL_ARTIFACT_ID=choerodon-tool-liquibase
-TOOL_VERSION=0.16.2-SNAPSHOT
+TOOL_VERSION=0.17.1.RELEASE
 TOOL_JAR_PATH=${MAVEN_LOCAL_REPO}/${TOOL_GROUP_ID/\./\/}/${TOOL_ARTIFACT_ID}/${TOOL_VERSION}/${TOOL_ARTIFACT_ID}-${TOOL_VERSION}.jar
 mvn org.apache.maven.plugins:maven-dependency-plugin:get \
  -Dartifact=${TOOL_GROUP_ID}:${TOOL_ARTIFACT_ID}:${TOOL_VERSION} \
@@ -18,6 +18,5 @@ java -Dspring.datasource.url="jdbc:mysql://localhost:3306/?serverTimezone=CTT&us
  -Dinstaller.datasources.platform.driver-class-name=com.mysql.jdbc.Driver \
  -Ddata.init=true \
  -Dlogging.level.root=info \
- -Ddata.version=1.4.0.RELEASE \
  -Dinstaller.jarPath=target/app.jar \
  -jar ${TOOL_JAR_PATH}
