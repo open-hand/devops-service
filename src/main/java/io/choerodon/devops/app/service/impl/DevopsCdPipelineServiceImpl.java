b60c1b606825d4a2def7a0231ebf39ad2f6f9bbe
王灏 <2507172753@qq.com>
2020-08-06 10:47:56 +0800

    [imp]修改部署逻辑

--- src/main/java/io/choerodon/devops/app/service/impl/DevopsCdPipelineServiceImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/DevopsCdPipelineServiceImpl.java
@@ -456,7 +456,7 @@ public class DevopsCdPipelineServiceImpl implements DevopsCdPipelineService {
                     DevopsEnvCommandDTO preCommand = devopsEnvCommandService.baseQuery(preInstance.getCommandId());
                     AppServiceVersionRespVO deploydAppServiceVersion = appServiceVersionService.queryById(preCommand.getObjectVersionId());
                     if (preCommand.getObjectVersionId().equals(appServiceDeployVO.getAppServiceVersionId())) {
-                        appServiceInstanceService.restartInstance(preInstance.getProjectId(), preInstance.getId());
+                        appServiceInstanceService.restartInstance(devopsCdEnvDeployInfoDTO.getProjectId(), preInstance.getId());
                         devopsCdJobRecordService.updateStatusById(jobRecordId, PipelineStatus.SUCCESS.toValue());
                         return;
                     }
