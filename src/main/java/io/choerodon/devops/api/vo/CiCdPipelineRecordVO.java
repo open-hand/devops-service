a6a0fd2a75579d69d01129c7241ca52c846c96fb
wangxiang <xiangwang04@hand-china.com>
2020-08-26 11:50:02 +0800

    [FIX] 修改记录id显示

--- src/main/java/io/choerodon/devops/api/vo/CiCdPipelineRecordVO.java
+++ src/main/java/io/choerodon/devops/api/vo/CiCdPipelineRecordVO.java
@@ -1,7 +1,6 @@
 package io.choerodon.devops.api.vo;
 
 
-import java.util.Date;
 import java.util.List;
 
 import org.hzero.starter.keyencrypt.core.Encrypt;
@@ -17,31 +16,60 @@ public class CiCdPipelineRecordVO extends BaseDomain {
 
     private Long devopsPipelineRecordRelId;
     @Encrypt
-    //ci 执行记录的id
+    /**
+     * ci 执行记录的id
+     */
     private Long ciRecordId;
     @Encrypt
-    //cd 执行记录的id
+    /**
+     * cd 执行记录的id
+     */
     private Long cdRecordId;
-    //gitlab的流水线id
+    /**
+     * gitlab的流水线id
+     */
     private Long gitlabPipelineId;
-    //cd流水线记录的状态
+
+    /**
+     * cd流水线记录的状态
+     */
     private String cdStatus;
-    //ci的流水线记录状态
+    /**
+     * ci的流水线记录状态
+     */
     private String ciStatus;
-    //cicd 流水线的状态
+    /**
+     * cicd 流水线的状态
+     */
     private String status;
     private String gitlabTriggerRef;
     private CustomCommitVO commit;
 
     private CiCdPipelineVO ciCdPipelineVO;
 
-    // ci和cd阶段记录的集合
+    /**
+     * ci和cd阶段记录的集合
+     */
     private List<StageRecordVO> stageRecordVOS;
-    // 待审核状态是需要一些数据
+    /**
+     * 待审核状态是需要一些数据
+     */
     private DevopsCdPipelineDeatilVO devopsCdPipelineDeatilVO;
     private String pipelineName;
     private Long gitlabProjectId;
 
+    /**
+     * 界面显示编号，取雪花id
+     */
+    private String viewId;
+
+    public String getViewId() {
+        return viewId;
+    }
+
+    public void setViewId(String viewId) {
+        this.viewId = viewId;
+    }
 
     public Long getGitlabProjectId() {
         return gitlabProjectId;
