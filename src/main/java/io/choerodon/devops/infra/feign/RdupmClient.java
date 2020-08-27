b3785c889752cfaa020ee2a156989c2495d196b1
王灏 <2507172753@qq.com>
2020-08-27 11:47:50 +0800

    [imp]rdupmclient update

--- src/main/java/io/choerodon/devops/infra/feign/RdupmClient.java
+++ src/main/java/io/choerodon/devops/infra/feign/RdupmClient.java
@@ -176,8 +176,8 @@ public interface RdupmClient {
 
     @ApiOperation(value = "项目层/组织层--镜像TAG列表")
     @GetMapping(value = "/v1/harbor-image-tag/list/{projectId}")
-    ResponseEntity<Page<HarborImageTagVo>> pagingImageTag(@PathVariable(value = "projectId") @ApiParam(value = "猪齿鱼项目ID") Long projectId,
-                                                          @ApiParam(value = "仓库名称") @RequestParam String repoName,
-                                                          @ApiParam(value = "版本号") @RequestParam(required = false) String tagName,
+    ResponseEntity<Page<HarborImageTagVo>> pagingImageTag(@PathVariable(value = "projectId") Long projectId,
+                                                          @RequestParam(value = "repoName") String repoName,
+                                                          @RequestParam(value = "tagName", required = false) String tagName,
                                                           @ApiIgnore PageRequest pageRequest);
 }
