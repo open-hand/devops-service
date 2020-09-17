01baeca738c2ad83a98d30b8e867057353b64ec8
zmf <1984654893@qq.com>
2020-09-17 11:43:21 +0800

    [ADD] add validation about hosts

--- src/main/java/io/choerodon/devops/api/validator/DevopsHostAdditionalCheckValidator.java
+++ src/main/java/io/choerodon/devops/api/validator/DevopsHostAdditionalCheckValidator.java
@@ -1,6 +1,11 @@
 package io.choerodon.devops.api.validator;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+
+import io.choerodon.devops.app.service.DevopsHostService;
+import io.choerodon.devops.infra.util.CommonExAssertUtil;
 
 /**
  * @author zmf
@@ -8,19 +13,23 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class DevopsHostAdditionalCheckValidator {
+    @Lazy
+    @Autowired
+    private DevopsHostService devopsHostService;
+
     public void validNameProjectUnique(Long projectId, String name) {
-        // TODO
+        CommonExAssertUtil.assertTrue(devopsHostService.isNameUnique(projectId, name), "error.host.name.not.unique");
     }
 
     public void validIpAndSshPortProjectUnique(Long projectId, String ip, Integer sshPort) {
-        // TODO
+        CommonExAssertUtil.assertTrue(devopsHostService.isSshIpPortUnique(projectId, ip, sshPort), "error.host.ip.ssh.port.not.unique");
     }
 
     public void validIpAndJmeterPortProjectUnique(Long projectId, String ip, Integer jmeterPort) {
-        // TODO
+        CommonExAssertUtil.assertTrue(devopsHostService.isIpJmeterPortUnique(projectId, ip, jmeterPort), "error.host.ip.jmeter.port.not.unique");
     }
 
     public void validJmeterPort(Integer jmeterPort) {
-        // TODO
+        CommonExAssertUtil.assertTrue(jmeterPort != null && jmeterPort > 0 && jmeterPort <= 65535, "error.jmeter.port.invalid");
     }
 }
