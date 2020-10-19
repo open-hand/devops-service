af9ecf519c9a59e6364d75b6b79c5bfe9eca8ae2
lihao <310103270@qq.com>
2020-10-19 17:56:59 +0800

    [ADD] 添加节点连通测试接口

--- src/main/java/io/choerodon/devops/api/controller/v1/DevopsClusterNodeController.java
+++ src/main/java/io/choerodon/devops/api/controller/v1/DevopsClusterNodeController.java
@@ -1,7 +1,19 @@
 package io.choerodon.devops.api.controller.v1;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.hzero.core.util.Results;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import io.choerodon.core.iam.ResourceLevel;
+import io.choerodon.devops.api.vo.DevopsClusterNodeConnectionTestResultVO;
+import io.choerodon.devops.api.vo.DevopsClusterNodeConnectionTestVO;
+import io.choerodon.devops.app.service.DevopsClusterNodeService;
+import io.choerodon.swagger.annotation.Permission;
 
 /**
  * @author lihao
@@ -10,4 +22,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/v1/projects/{project_id}/nodes")
 public class DevopsClusterNodeController {
 
+    @Autowired
+    private DevopsClusterNodeService devopsClusterNodeService;
+
+    @ApiOperation("测试节点连通性")
+    @Permission(level = ResourceLevel.ORGANIZATION)
+    @PostMapping("/connection_test")
+    public ResponseEntity<DevopsClusterNodeConnectionTestResultVO> testConnection(
+            @ApiParam(value = "项目id", required = true)
+            @PathVariable("project_id") Long projectId,
+            @RequestBody @Valid DevopsClusterNodeConnectionTestVO devopsClusterNodeConnectionTestVO) {
+        return Results.success(devopsClusterNodeService.testConnection(projectId, devopsClusterNodeConnectionTestVO));
+    }
+
+    // TODO wx 批量添加节点
+
+    // TODO wx 删除节点
+
+    // TODO wx 移除节点角色
 }
--- src/main/java/io/choerodon/devops/app/service/DevopsClusterNodeService.java
+++ src/main/java/io/choerodon/devops/app/service/DevopsClusterNodeService.java
@@ -1,4 +1,15 @@
 package io.choerodon.devops.app.service;
 
+import io.choerodon.devops.api.vo.DevopsClusterNodeConnectionTestResultVO;
+import io.choerodon.devops.api.vo.DevopsClusterNodeConnectionTestVO;
+
 public interface DevopsClusterNodeService {
+    /**
+     * 测试当前节点连通性
+     *
+     * @param projectId                         项目id
+     * @param devopsClusterNodeConnectionTestVO 连接信息
+     * @return 测试结果 boolean
+     */
+    DevopsClusterNodeConnectionTestResultVO testConnection(Long projectId, DevopsClusterNodeConnectionTestVO devopsClusterNodeConnectionTestVO);
 }
