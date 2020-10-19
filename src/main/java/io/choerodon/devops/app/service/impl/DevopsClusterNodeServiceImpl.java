006a2cb0209f13532ace9364e0b4f050bcdf9965
王灏 <2507172753@qq.com>
2020-10-19 18:15:24 +0800

    [imp]解决冲突

--- src/main/java/io/choerodon/devops/app/service/impl/DevopsClusterNodeServiceImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/DevopsClusterNodeServiceImpl.java
@@ -1,5 +1,7 @@
 package io.choerodon.devops.app.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import io.choerodon.devops.api.vo.DevopsClusterNodeConnectionTestResultVO;
 import io.choerodon.devops.api.vo.DevopsClusterNodeConnectionTestVO;
 import io.choerodon.devops.app.service.DevopsClusterNodeService;
@@ -8,10 +10,13 @@ import io.choerodon.devops.infra.util.SshUtil;
 
 public class DevopsClusterNodeServiceImpl implements DevopsClusterNodeService {
 
+    @Autowired
+    private SshUtil sshUtil;
+
     @Override
     public DevopsClusterNodeConnectionTestResultVO testConnection(Long projectId, DevopsClusterNodeConnectionTestVO devopsClusterNodeConnectionTestVO) {
         DevopsClusterNodeConnectionTestResultVO result = new DevopsClusterNodeConnectionTestResultVO();
-        boolean sshConnected = SshUtil.sshConnect(devopsClusterNodeConnectionTestVO.getHostIp(), devopsClusterNodeConnectionTestVO.getSshPort(), devopsClusterNodeConnectionTestVO.getAuthType(), devopsClusterNodeConnectionTestVO.getUsername(), devopsClusterNodeConnectionTestVO.getPassword());
+        boolean sshConnected = sshUtil.sshConnect(devopsClusterNodeConnectionTestVO.getHostIp(), devopsClusterNodeConnectionTestVO.getSshPort(), devopsClusterNodeConnectionTestVO.getAuthType(), devopsClusterNodeConnectionTestVO.getUsername(), devopsClusterNodeConnectionTestVO.getPassword());
         result.setHostStatus(sshConnected ? DevopsHostStatus.SUCCESS.getValue() : DevopsHostStatus.FAILED.getValue());
         if (!sshConnected) {
             result.setHostCheckError("failed to check ssh, please ensure network and authentication is valid");
