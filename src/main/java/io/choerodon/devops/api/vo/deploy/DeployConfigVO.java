10cdaf18cbcec26371fea1c4bd3abfdd4a9e8527
王灏 <2507172753@qq.com>
2020-10-25 22:35:28 +0800

    [imp]手动部署接口添加参数校验

--- src/main/java/io/choerodon/devops/api/vo/deploy/DeployConfigVO.java
+++ src/main/java/io/choerodon/devops/api/vo/deploy/DeployConfigVO.java
@@ -1,6 +1,8 @@
 package io.choerodon.devops.api.vo.deploy;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import io.swagger.annotations.ApiModelProperty;
 import org.hzero.starter.keyencrypt.core.Encrypt;
@@ -15,50 +17,63 @@ import io.choerodon.devops.api.vo.HostConnectionVO;
  */
 public class DeployConfigVO {
     @ApiModelProperty("主机详情")
+    @Valid
+    @NotNull(message = "error.hostConnectionVO.is.null")
     private HostConnectionVO hostConnectionVO;
 
     @ApiModelProperty("部署类型 env/host")
+    @NotNull(message = "error.deployType.is.null")
     private String deployType;
 
     @ApiModelProperty("镜像部署详情")
+    @Valid
     private ImageDeploy imageDeploy;
 
     @ApiModelProperty("jar部署详情")
+    @Valid
     private JarDeploy jarDeploy;
 
     @ApiModelProperty("部署对象类型 image/jar")
     private String deployObjectType;
-
+    @ApiModelProperty("环境部署详情")
     @Valid
     private AppServiceDeployVO appServiceDeployVO;
 
     public static class ImageDeploy {
 
         @ApiModelProperty("仓库名")
+        @NotNull(message = "error.repoName.is.null")
         private String repoName;
 
         @ApiModelProperty("仓库类型")
+        @NotNull(message = "error.repoType.is.null")
         private String repoType;
 
         @Encrypt
         @ApiModelProperty("仓库Id")
+        @NotNull(message = "error.repoId.is.null")
         private String repoId;
 
         @ApiModelProperty("镜像名称")
+        @NotNull(message = "error.imageName.is.null")
         private String imageName;
 
         @Encrypt
         @ApiModelProperty("镜像Id")
+        @NotNull(message = "error.imageId.is.null")
         private Long imageId;
 
 
         @ApiModelProperty("镜像版本")
+        @NotNull(message = "error.tag.is.null")
         private String tag;
 
         @ApiModelProperty("部署values")
+        @NotNull(message = "error.value.is.null")
         private String value;
 
         @ApiModelProperty("容器名称")
+        @NotNull(message = "error.containerName.is.null")
         private String containerName;
 
         public String getValue() {
@@ -129,27 +144,34 @@ public class DeployConfigVO {
     public static class JarDeploy {
 
         @ApiModelProperty("服务名")
+        @NotNull(message = "error.serverName.is.null")
         private String serverName;
         @ApiModelProperty("仓库名")
         private String neRepositoryName;
 
         @Encrypt
         @ApiModelProperty("仓库id")
+        @NotNull(message = "error.repositoryId.is.null")
         private Long repositoryId;
 
         @ApiModelProperty("groupId")
+        @NotNull(message = "error.groupId.is.null")
         private String groupId;
 
         @ApiModelProperty("artifactId")
+        @NotNull(message = "error.artifactId.is.null")
         private String artifactId;
 
         @ApiModelProperty("版本")
+        @NotNull(message = "error.version.is.null")
         private String version;
 
         @ApiModelProperty("部署values")
+        @NotNull(message = "error.value.is.null")
         private String value;
 
         @ApiModelProperty("工作目录,默认值/temp")
+        @NotNull(message = "error.workingPath.is.null")
         private String workingPath;
 
         public String getWorkingPath() {
