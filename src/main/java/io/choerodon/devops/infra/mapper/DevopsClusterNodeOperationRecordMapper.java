34ac182261c14c360800f3171906661a9c62d7de
lihao <310103270@qq.com>
2020-10-27 11:34:44 +0800

    [ADD] 初始化集群和节点操作记录表

---
+++ src/main/java/io/choerodon/devops/infra/mapper/DevopsClusterNodeOperationRecordMapper.java
@@ -0,0 +1,7 @@
+package io.choerodon.devops.infra.mapper;
+
+import io.choerodon.devops.infra.dto.DevopsClusterNodeOperationRecordDTO;
+import io.choerodon.mybatis.common.BaseMapper;
+
+public interface DevopsClusterNodeOperationRecordMapper extends BaseMapper<DevopsClusterNodeOperationRecordDTO> {
+}
