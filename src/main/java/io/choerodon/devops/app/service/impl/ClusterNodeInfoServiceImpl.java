bd5653c30d754c98d4046999f35052ea9edf998e
lihao <310103270@qq.com>
2020-11-19 17:26:12 +0800

    [FIX] 去掉无效引用

--- src/main/java/io/choerodon/devops/app/service/impl/ClusterNodeInfoServiceImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/ClusterNodeInfoServiceImpl.java
@@ -8,7 +8,6 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 import com.alibaba.fastjson.JSONObject;
-import com.sun.javafx.binding.StringFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
