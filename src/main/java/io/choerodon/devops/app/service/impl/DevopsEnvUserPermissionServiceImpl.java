600f2bb6ea8a61425a4ad0c36d74643a5bbc49d9
王灏 <2507172753@qq.com>
2020-12-02 11:19:26 +0800

    [IMP] 修复流水线部署任务校验环境权限bug
    (cherry picked from commit 1447c41e22ae2211d920dab19ab2eebcbd7cf29f)

--- src/main/java/io/choerodon/devops/app/service/impl/DevopsEnvUserPermissionServiceImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/DevopsEnvUserPermissionServiceImpl.java
@@ -139,6 +139,10 @@ public class DevopsEnvUserPermissionServiceImpl implements DevopsEnvUserPermissi
     @Override
     public Boolean checkUserEnvPermission(Long envId, Long userId) {
         DevopsEnvironmentDTO devopsEnvironmentDTO = devopsEnvironmentService.baseQueryById(envId);
+        // 判断环境是否校验权限
+        if (Boolean.TRUE.equals(devopsEnvironmentDTO.getSkipCheckPermission())) {
+            return true;
+        }
         // 判断当前用户是否是项目所有者或者root，如果是，直接跳过校验，如果不是，校验环境权限
         if (!permissionHelper.isGitlabProjectOwnerOrGitlabAdmin(devopsEnvironmentDTO.getProjectId(), userId)) {
             DevopsEnvUserPermissionDTO devopsEnvUserPermissionDO = new DevopsEnvUserPermissionDTO(envId, userId);
