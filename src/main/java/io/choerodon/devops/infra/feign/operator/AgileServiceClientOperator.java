f28f434ec757ae2f4d9b00e7dbf5034454c01e36
lihao <310103270@qq.com>
2021-01-07 15:39:40 +0800

    [IMP] 优化feign调用

--- src/main/java/io/choerodon/devops/infra/feign/operator/AgileServiceClientOperator.java
+++ src/main/java/io/choerodon/devops/infra/feign/operator/AgileServiceClientOperator.java
@@ -1,14 +1,16 @@
 package io.choerodon.devops.infra.feign.operator;
 
-import io.choerodon.core.exception.CommonException;
-import io.choerodon.devops.infra.dto.agile.IssueDTO;
-import io.choerodon.devops.infra.dto.agile.ProjectInfoDTO;
-import io.choerodon.devops.infra.dto.agile.SprintDTO;
-import io.choerodon.devops.infra.feign.AgileServiceClient;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import io.choerodon.core.exception.ServiceUnavailableException;
+import io.choerodon.core.utils.FeignClientUtils;
+import io.choerodon.devops.infra.dto.agile.IssueDTO;
+import io.choerodon.devops.infra.dto.agile.SprintDTO;
+import io.choerodon.devops.infra.feign.AgileServiceClient;
 
 /**
  * Created by Sheep on 2019/7/11.
@@ -24,29 +26,22 @@ public class AgileServiceClientOperator {
 
     public IssueDTO queryIssue(Long projectId, Long issueId, Long organizationId) {
         try {
-            return agileServiceClient.queryIssue(projectId, issueId, organizationId).getBody();
-        } catch (Exception e) {
-            throw new CommonException("error.issue.get");
+            return FeignClientUtils.doRequest(() -> agileServiceClient.queryIssue(projectId, issueId, organizationId), IssueDTO.class, "error.issue.get");
+        } catch (ServiceUnavailableException e) {
+            return new IssueDTO();
         }
-    }
-
-    public ProjectInfoDTO queryProjectInfo(Long projectId) {
-        return agileServiceClient.queryProjectInfo(projectId).getBody();
     }
 
     public List<IssueDTO> listIssueByIds(Long projectId, List<Long> ids) {
-        try {
-            return agileServiceClient.queryIssues(projectId, ids).getBody();
-        } catch (Exception e) {
-            throw new CommonException("error.issues.list.by.id");
-        }
+        return FeignClientUtils.doRequest(() -> agileServiceClient.queryIssues(projectId, ids), new TypeReference<List<IssueDTO>>() {
+        }, "error.issues.list.by.id");
     }
 
     public SprintDTO getActiveSprint(Long projectId, Long organizationId) {
         try {
-            return agileServiceClient.getActiveSprint(projectId, organizationId).getBody();
-        } catch (Exception e) {
-            throw new CommonException("error.active.sprint.get");
+            return FeignClientUtils.doRequest(() -> agileServiceClient.getActiveSprint(projectId, organizationId), SprintDTO.class, "error.active.sprint.get");
+        } catch (ServiceUnavailableException e) {
+            return new SprintDTO();
         }
     }
 }
--- src/main/java/io/choerodon/devops/infra/feign/operator/TestServiceClientOperator.java
+++ src/main/java/io/choerodon/devops/infra/feign/operator/TestServiceClientOperator.java
@@ -3,10 +3,10 @@ package io.choerodon.devops.infra.feign.operator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
-import io.choerodon.core.exception.CommonException;
+import io.choerodon.core.exception.ServiceUnavailableException;
+import io.choerodon.core.utils.FeignClientUtils;
 import io.choerodon.devops.api.vo.test.ApiTestExecuteVO;
 import io.choerodon.devops.api.vo.test.ApiTestTaskRecordVO;
 import io.choerodon.devops.infra.dto.test.ApiTestTaskRecordDTO;
@@ -37,12 +37,7 @@ public class TestServiceClientOperator {
     public ApiTestTaskRecordDTO executeTask(Long projectId, Long taskId, Long createdBy) {
         ApiTestExecuteVO apiTestExecuteVO = new ApiTestExecuteVO();
         apiTestExecuteVO.setTaskId(taskId);
-        ResponseEntity<ApiTestTaskRecordDTO> entity = testServiceClient.executeTask(projectId, apiTestExecuteVO, createdBy);
-
-        if (entity != null && !entity.getStatusCode().is2xxSuccessful()) {
-            throw new CommonException("error.execute.api.test.task");
-        }
-        return entity == null ? null : entity.getBody();
+        return FeignClientUtils.doRequest(() -> testServiceClient.executeTask(projectId, apiTestExecuteVO, createdBy), ApiTestTaskRecordDTO.class, "error.execute.api.test.task");
     }
 
     /**
@@ -53,26 +48,16 @@ public class TestServiceClientOperator {
      * @return
      */
     public ApiTestTaskRecordVO queryById(Long projectId, Long taskRecordId) {
-
-        ResponseEntity<ApiTestTaskRecordVO> entity = testServiceClient.queryById(projectId, taskRecordId);
-
-        if (entity != null && !entity.getStatusCode().is2xxSuccessful()) {
-            throw new CommonException("error.query.api.test.task.record");
-        }
-        return entity == null ? null : entity.getBody();
+        return FeignClientUtils.doRequest(() -> testServiceClient.queryById(projectId, taskRecordId), ApiTestTaskRecordVO.class, "error.query.api.test.task.record");
     }
 
     public boolean testJmeterConnection(String hostIp, Integer jmeterPort) {
         try {
-            ResponseEntity<Boolean> result = testServiceClient.testConnection(hostIp, jmeterPort);
-            if (result.getStatusCode().is2xxSuccessful()) {
-                return Boolean.TRUE.equals(result.getBody());
-            }
-        } catch (Exception ex) {
+            return FeignClientUtils.doRequest(() -> testServiceClient.testConnection(hostIp, jmeterPort), Boolean.class);
+        } catch (ServiceUnavailableException ex) {
             LOGGER.debug("TestServiceClientOperator: Failed to test jmeter connection for host {} and port {}", hostIp, jmeterPort);
             LOGGER.debug("The ex is", ex);
             return false;
         }
-        return false;
     }
 }
