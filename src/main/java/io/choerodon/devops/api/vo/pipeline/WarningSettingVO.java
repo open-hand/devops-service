13ecca7430526ba29bc7f1488fe956b3f85657e0
王灏 <2507172753@qq.com>
2021-01-12 09:36:57 +0800

    [IMP] 初始化告警设置消息模板

---
+++ src/main/java/io/choerodon/devops/api/vo/pipeline/WarningSettingVO.java
@@ -0,0 +1,58 @@
+package io.choerodon.devops.api.vo.pipeline;
+
+import java.util.Set;
+
+/**
+ * 〈功能简述〉
+ * 〈API测试任务， 告警设置VO〉
+ *
+ * @author wanghao
+ * @since 2021/1/11 16:11
+ */
+public class WarningSettingVO {
+    private Boolean enableWarningSetting;
+    private Double PerformThreshold;
+    private Set<Long> notifyUserIds;
+    private Boolean sendEmail;
+    private Boolean sendSiteMessage;
+
+    public Boolean getEnableWarningSetting() {
+        return enableWarningSetting;
+    }
+
+    public void setEnableWarningSetting(Boolean enableWarningSetting) {
+        this.enableWarningSetting = enableWarningSetting;
+    }
+
+    public Double getPerformThreshold() {
+        return PerformThreshold;
+    }
+
+    public void setPerformThreshold(Double performThreshold) {
+        PerformThreshold = performThreshold;
+    }
+
+    public Set<Long> getNotifyUserIds() {
+        return notifyUserIds;
+    }
+
+    public void setNotifyUserIds(Set<Long> notifyUserIds) {
+        this.notifyUserIds = notifyUserIds;
+    }
+
+    public Boolean getSendEmail() {
+        return sendEmail;
+    }
+
+    public void setSendEmail(Boolean sendEmail) {
+        this.sendEmail = sendEmail;
+    }
+
+    public Boolean getSendSiteMessage() {
+        return sendSiteMessage;
+    }
+
+    public void setSendSiteMessage(Boolean sendSiteMessage) {
+        this.sendSiteMessage = sendSiteMessage;
+    }
+}
