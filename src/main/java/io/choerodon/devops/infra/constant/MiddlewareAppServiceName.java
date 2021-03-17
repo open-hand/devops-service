d5c34d2592391984fa42756bdc78a027019dca3d
lihao <310103270@qq.com>
2021-03-17 17:50:51 +0800

    [IMP] 优化中间件名称设置逻辑

---
+++ src/main/java/io/choerodon/devops/infra/constant/MiddlewareAppServiceName.java
@@ -0,0 +1,21 @@
+package io.choerodon.devops.infra.constant;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MiddlewareAppServiceName {
+    public static Map<String, String> MIDDLE_APP_SERVICE_NAME_MAP;
+
+    /**
+     * 中间件名称映射
+     * key: marketApplicationName-marketServiceName
+     * value: 展示出来的名称
+     */
+    static{
+        Map<String,String> map=new HashMap<>();
+        map.put("Redis-standalone","Redis单机版");
+        map.put("Redis-sentinel","Redis哨兵版");
+        MIDDLE_APP_SERVICE_NAME_MAP= Collections.unmodifiableMap(map);
+    }
+}
