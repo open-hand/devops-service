01c1a2994b9fd00b7fcde7d6cfe183ceac797bcf
wangxiang <xiangwang04@hand-china.com>
2021-03-25 09:12:06 +0800

    [REF] 增加vo

--- src/main/java/io/choerodon/devops/api/vo/MiddlewareRedisEnvDeployVO.java
+++ src/main/java/io/choerodon/devops/api/vo/MiddlewareRedisEnvDeployVO.java
@@ -1,6 +1,7 @@
 package io.choerodon.devops.api.vo;
 
 import io.swagger.annotations.ApiModelProperty;
+import java.util.Map;
 
 public class MiddlewareRedisEnvDeployVO extends MarketInstanceCreationRequestVO {
     @ApiModelProperty("部署模式")
@@ -8,6 +9,19 @@ public class MiddlewareRedisEnvDeployVO extends MarketInstanceCreationRequestVO 
 
     @ApiModelProperty("中间件版本")
     private String version;
+
+    /**
+     * 部署配置
+     */
+    private Map<Long, RedisConfigVO> longRedisConfigVOMap;
+
+    public Map<Long, RedisConfigVO> getLongRedisConfigVOMap() {
+        return longRedisConfigVOMap;
+    }
+
+    public void setLongRedisConfigVOMap(Map<Long, RedisConfigVO> longRedisConfigVOMap) {
+        this.longRedisConfigVOMap = longRedisConfigVOMap;
+    }
 
     public String getMode() {
         return mode;
