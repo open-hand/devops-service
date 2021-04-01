f304bc25d14203cf025e1b8528566e1757565a4a
lihao <310103270@qq.com>
2021-04-01 16:11:58 +0800

    [FIX] 修复错误的请求方式

--- src/main/java/io/choerodon/devops/infra/feign/AgileServiceClient.java
+++ src/main/java/io/choerodon/devops/infra/feign/AgileServiceClient.java
@@ -5,10 +5,7 @@ import java.util.List;
 import io.swagger.annotations.ApiParam;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import io.choerodon.devops.infra.feign.fallback.AgileServiceClientFallback;
 
@@ -21,7 +18,7 @@ public interface AgileServiceClient {
             @PathVariable("issueId") Long issueId,
             @RequestParam("organizationId") Long organizationId);
 
-    @GetMapping(value = "/v1/projects/{project_id}/issues/query_issue_ids")
+    @PostMapping(value = "/v1/projects/{project_id}/issues/query_issue_ids")
     ResponseEntity<String> queryIssues(@ApiParam(value = "项目id", required = true)
                                                @PathVariable(name = "project_id") Long projectId,
                                                @ApiParam(value = "issue编号", required = true)
