d10800ef3dfcef54df2c72604210d2c1b6706a73
lihao <310103270@qq.com>
2021-06-16 15:28:00 +0800

    [FIX] 修复工作负载更新失败

--- src/main/java/io/choerodon/devops/infra/mapper/DevopsWorkloadResourceContentMapper.java
+++ src/main/java/io/choerodon/devops/infra/mapper/DevopsWorkloadResourceContentMapper.java
@@ -7,4 +7,6 @@ import io.choerodon.mybatis.common.BaseMapper;
 
 public interface DevopsWorkloadResourceContentMapper extends BaseMapper<DevopsWorkloadResourceContentDTO> {
     void deleteByResourceId(@Param("type") String type, @Param("workloadId") Long workloadId);
+
+    Integer updateContentByResourceIdAndResourceKind(@Param("kind") String kind, @Param("workloadId") Long workloadId, @Param("content") String content);
 }
