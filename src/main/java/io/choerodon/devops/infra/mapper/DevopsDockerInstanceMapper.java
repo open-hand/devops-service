668fd04c595aa16bd496102f855eb97fb1284b11
王灏 <2507172753@qq.com>
2021-07-05 21:29:24 +0800

    [imp] 修改主机查询排序

--- src/main/java/io/choerodon/devops/infra/mapper/DevopsDockerInstanceMapper.java
+++ src/main/java/io/choerodon/devops/infra/mapper/DevopsDockerInstanceMapper.java
@@ -3,6 +3,8 @@ package io.choerodon.devops.infra.mapper;
 import io.choerodon.devops.infra.dto.DevopsDockerInstanceDTO;
 import io.choerodon.mybatis.common.BaseMapper;
 
+import java.util.List;
+
 /**
  * 〈功能简述〉
  * 〈〉
@@ -11,4 +13,6 @@ import io.choerodon.mybatis.common.BaseMapper;
  * @Date 2021/6/30 14:12
  */
 public interface DevopsDockerInstanceMapper extends BaseMapper<DevopsDockerInstanceDTO> {
+
+    List<DevopsDockerInstanceDTO> listByHostId(Long hostId);
 }
