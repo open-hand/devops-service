6ccf674ed0066ce7c70985a45358e21deb65197c
王灏 <2507172753@qq.com>
2021-07-14 10:21:26 +0800

    [imp] 主机应用实例关系表

---
+++ src/main/resources/script/db/groovy/devops_service/devops_host_app_instance_rel.groovy
@@ -0,0 +1,34 @@
+package script.db.groovy.devops_service
+
+databaseChangeLog(logicalFilePath: 'dba/devops_host_app_instance_rel.groovy') {
+    changeSet(author: 'wanghao', id: '2021-07-14-create-devops_host_app_instance_rel') {
+        createTable(tableName: "devops_host_app_instance_rel", remarks: '主机应用实例关系表') {
+            column(name: 'id', type: 'BIGINT UNSIGNED', remarks: '主键，ID', autoIncrement: true) {
+                constraints(primaryKey: true)
+            }
+            column(name: 'host_id', type: 'BIGINT UNSIGNED', remarks: '主机id') {
+                constraints(nullable: false)
+            }
+            column(name: 'instance_id', type: 'BIGINT UNSIGNED', remarks: '实例id') {
+                constraints(nullable: false)
+            }
+            column(name: 'app_id', type: 'BIGINT UNSIGNED', remarks: '应用id') {
+                constraints(nullable: false)
+            }
+            column(name: 'app_source', type: 'VARCHAR(32)', remarks: '应用来源') {
+                constraints(nullable: false)
+            }
+
+
+            column(name: "object_version_number", type: "BIGINT UNSIGNED", defaultValue: "1")
+            column(name: "created_by", type: "BIGINT UNSIGNED", defaultValue: "0")
+            column(name: "creation_date", type: "DATETIME", defaultValueComputed: "CURRENT_TIMESTAMP")
+            column(name: "last_updated_by", type: "BIGINT UNSIGNED", defaultValue: "0")
+            column(name: "last_update_date", type: "DATETIME", defaultValueComputed: "CURRENT_TIMESTAMP")
+        }
+
+        addUniqueConstraint(tableName: 'devops_host_app_instance_rel',
+                constraintName: 'uk_app_instance', columnNames: 'instance_id,app_id')
+    }
+
+}
