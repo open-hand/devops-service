e6b232d0ea4c35f5e01a36fbfff3aa51af538c1c
19750 <xingxing.wu@hand-china.com>
2021-07-15 11:31:03 +0800

    feat: 修改卡片资源数据接口层级

--- src/main/java/io/choerodon/devops/api/controller/v1/UserResourceController.java
+++ src/main/java/io/choerodon/devops/api/controller/v1/UserResourceController.java
@@ -1,11 +1,9 @@
 package io.choerodon.devops.api.controller.v1;
 
-import io.choerodon.core.iam.ResourceLevel;
 import io.choerodon.devops.api.vo.ClusterDetailResourceVO;
 import io.choerodon.devops.api.vo.GeneralResourceVO;
 import io.choerodon.devops.api.vo.HostDetailResourceVO;
 import io.choerodon.devops.app.service.UserResourceService;
-import io.choerodon.swagger.annotation.CustomPageRequest;
 import io.choerodon.swagger.annotation.Permission;
 import io.swagger.annotations.ApiOperation;
 import java.util.List;
@@ -14,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,21 +27,21 @@ public class UserResourceController {
 
 
     @ApiOperation("查询上下文用户在指定组织下的资源概览")
-    @Permission(level = ResourceLevel.ORGANIZATION)
+    @Permission(permissionLogin = true)
     @GetMapping("/general")
     public ResponseEntity<GeneralResourceVO> queryGeneral(@PathVariable("organization_id") Long organizationId) {
         return Results.success(userResourceService.queryGeneral(organizationId));
     }
 
     @ApiOperation("查询上下文用户在指定组织下的主机资源详情")
-    @Permission(level = ResourceLevel.ORGANIZATION)
+    @Permission(permissionLogin = true)
     @GetMapping("/host")
     public ResponseEntity<List<HostDetailResourceVO>> queryHostResource(@PathVariable("organization_id") Long organizationId) {
         return Results.success(userResourceService.queryHostResource(organizationId));
     }
 
     @ApiOperation("查询上下文用户在指定组织下的集群资源详情")
-    @Permission(level = ResourceLevel.ORGANIZATION)
+    @Permission(permissionLogin = true)
     @GetMapping("/cluster")
     public ResponseEntity<List<ClusterDetailResourceVO>> queryClusterResource(@PathVariable("organization_id") Long organizationId) {
         return Results.success(userResourceService.queryClusterResource(organizationId));
