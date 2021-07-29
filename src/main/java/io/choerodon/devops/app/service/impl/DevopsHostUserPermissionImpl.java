30eb5e12a0f0b354636c0967507c2bafb4b69bc8
lihao <310103270@qq.com>
2021-07-29 10:38:53 +0800

    [ADD] 添加主机权限管理基础类

---
+++ src/main/java/io/choerodon/devops/app/service/impl/DevopsHostUserPermissionImpl.java
@@ -0,0 +1,9 @@
+package io.choerodon.devops.app.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import io.choerodon.devops.app.service.DevopsHostUserPermissionService;
+
+@Service
+public class DevopsHostUserPermissionImpl implements DevopsHostUserPermissionService {
+}
