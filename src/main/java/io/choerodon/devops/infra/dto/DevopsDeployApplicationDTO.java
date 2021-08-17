d4e185cf873ddf3831d9a964c2a70d48c909348f
lihao <310103270@qq.com>
2021-08-17 18:17:35 +0800

    [ADD] 添加部署应用表及dto类

---
+++ src/main/java/io/choerodon/devops/infra/dto/DevopsDeployApplicationDTO.java
@@ -0,0 +1,118 @@
+package io.choerodon.devops.infra.dto;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import io.swagger.annotations.ApiModelProperty;
+
+import io.choerodon.mybatis.annotation.ModifyAudit;
+import io.choerodon.mybatis.annotation.VersionAudit;
+import io.choerodon.mybatis.domain.AuditDomain;
+
+/**
+ * devops部署应用表
+ */
+@Table(name = "devops_deploy_application")
+@ModifyAudit
+@VersionAudit
+public class DevopsDeployApplicationDTO extends AuditDomain {
+    @Id
+    private Long id;
+
+    @ApiModelProperty(name = "name")
+    private String name;
+
+    @ApiModelProperty(name = "code")
+    private String code;
+
+    @ApiModelProperty(name = "项目id")
+    private Long projectId;
+
+    @ApiModelProperty(name = "部署对象id")
+    private Long objectId;
+
+    @ApiModelProperty(name = "Id")
+    private Long envId;
+
+    @ApiModelProperty(name = "Id")
+    private Long hostId;
+
+    @ApiModelProperty(name = "操作类型")
+    private String operationType;
+
+    @ApiModelProperty(name = "chart/jar/docker")
+    private String rdupmType;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Long projectId) {
+        this.projectId = projectId;
+    }
+
+    public Long getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(Long objectId) {
+        this.objectId = objectId;
+    }
+
+    public Long getEnvId() {
+        return envId;
+    }
+
+    public void setEnvId(Long envId) {
+        this.envId = envId;
+    }
+
+    public Long getHostId() {
+        return hostId;
+    }
+
+    public void setHostId(Long hostId) {
+        this.hostId = hostId;
+    }
+
+    public String getOperationType() {
+        return operationType;
+    }
+
+    public void setOperationType(String operationType) {
+        this.operationType = operationType;
+    }
+
+    public String getRdupmType() {
+        return rdupmType;
+    }
+
+    public void setRdupmType(String rdupmType) {
+        this.rdupmType = rdupmType;
+    }
+}
---
+++ src/main/java/io/choerodon/devops/infra/mapper/DevopsDeployApplicationMapper.java
@@ -0,0 +1,7 @@
+package io.choerodon.devops.infra.mapper;
+
+import io.choerodon.devops.infra.dto.DevopsDeployApplicationDTO;
+import io.choerodon.mybatis.common.BaseMapper;
+
+public interface DevopsDeployApplicationMapper extends BaseMapper<DevopsDeployApplicationDTO> {
+}
