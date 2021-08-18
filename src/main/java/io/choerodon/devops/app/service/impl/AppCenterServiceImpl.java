61e03b7c0c1c93e4235ead31549061187e93f861
18986 <changping.shi@hand-chian.com>
2021-08-18 15:55:22 +0800

    [ADD] 添加应用中心查询应用详情接口

---
+++ src/main/java/io/choerodon/devops/app/service/impl/AppCenterServiceImpl.java
@@ -0,0 +1,20 @@
+package io.choerodon.devops.app.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import io.choerodon.devops.api.vo.AppCenterDetailVO;
+import io.choerodon.devops.app.service.AppCenterService;
+
+/**
+ * @Author: scp
+ * @Description:
+ * @Date: Created in 2021/8/18
+ * @Modified By:
+ */
+@Service
+public class AppCenterServiceImpl implements AppCenterService {
+    @Override
+    public AppCenterDetailVO appCenterDetail(Long projectId, Long appCenterId) {
+        return null;
+    }
+}
