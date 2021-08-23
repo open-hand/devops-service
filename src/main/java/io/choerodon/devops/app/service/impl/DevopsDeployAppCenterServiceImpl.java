235874596a572b88ee9aecab771c88bab5ff9a51
王灏 <hao.wang08@hand-china.com>
2021-08-23 17:04:07 +0800

    [ADD] 解决循环依赖

--- src/main/java/io/choerodon/devops/app/service/impl/DevopsDeployAppCenterServiceImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/DevopsDeployAppCenterServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
@@ -40,6 +41,7 @@ public class DevopsDeployAppCenterServiceImpl implements DevopsDeployAppCenterSe
     @Autowired
     private DevopsDeployAppCenterEnvMapper appCenterEnvMapper;
     @Autowired
+    @Lazy
     private AppServiceInstanceService instanceService;
     @Autowired
     private AppServiceService appServiceService;
