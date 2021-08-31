2ac5ad28cf35f68fbb3aff21b668e5125df64011
shanyu <yu.shan@hand-china.com>
2021-08-31 17:53:08 +0800

    [FIX] 停止部署后，部署耗时内容为空的问题。

--- src/main/java/io/choerodon/devops/app/service/impl/DevopsHzeroDeployDetailsServiceImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/DevopsHzeroDeployDetailsServiceImpl.java
@@ -64,7 +64,7 @@ public class DevopsHzeroDeployDetailsServiceImpl implements DevopsHzeroDeployDet
     public void updateStatusById(Long id, HzeroDeployDetailsStatusEnum status) {
         DevopsHzeroDeployDetailsDTO devopsHzeroDeployDetailsDTO = devopsHzeroDeployDetailsMapper.selectByPrimaryKey(id);
         devopsHzeroDeployDetailsDTO.setStatus(status.value());
-        if (Objects.equals(status.value(), HzeroDeployDetailsStatusEnum.SUCCESS.value()) || Objects.equals(status.value(), HzeroDeployDetailsStatusEnum.FAILED.value())) {
+        if (Objects.equals(status.value(), HzeroDeployDetailsStatusEnum.SUCCESS.value()) || Objects.equals(status.value(), HzeroDeployDetailsStatusEnum.FAILED.value()) || Objects.equals(status.value(), HzeroDeployDetailsStatusEnum.CANCELED.value())) {
             devopsHzeroDeployDetailsDTO.setEndTime(new Date());
         }
         MapperUtil.resultJudgedUpdateByPrimaryKeySelective(devopsHzeroDeployDetailsMapper, devopsHzeroDeployDetailsDTO, ERROR_UPDATE_DEPLOY_DETAILS_FAILED);
