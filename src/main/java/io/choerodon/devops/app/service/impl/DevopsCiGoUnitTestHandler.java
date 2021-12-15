050a6a38438fbbe09c8671948a9e3a859abb6cb6
王灏 <hao.wang08@hand-china.com>
2021-12-15 10:24:45 +0800

    [IMP] 添加步骤相关处理器

---
+++ src/main/java/io/choerodon/devops/app/service/impl/DevopsCiGoUnitTestHandler.java
@@ -0,0 +1,36 @@
+package io.choerodon.devops.app.service.impl;
+
+import java.util.Set;
+
+import io.choerodon.devops.api.vo.DevopsCiStepVO;
+import io.choerodon.devops.app.service.AbstractDevopsCiStepHandler;
+import io.choerodon.devops.infra.enums.DevopsCiStepTypeEnum;
+
+/**
+ * 〈功能简述〉
+ * 〈〉
+ *
+ * @author wanghao
+ * @since 2021/12/15 10:23
+ */
+public class DevopsCiGoUnitTestHandler extends AbstractDevopsCiStepHandler {
+    @Override
+    public void fillConfigInfo(DevopsCiStepVO devopsCiStepVO) {
+
+    }
+
+    @Override
+    protected void saveConfig(Long stepId, DevopsCiStepVO devopsCiStepVO) {
+
+    }
+
+    @Override
+    protected void batchDeleteConfig(Set<Long> stepIds) {
+
+    }
+
+    @Override
+    public DevopsCiStepTypeEnum getType() {
+        return DevopsCiStepTypeEnum.GO_UNIT_TEST;
+    }
+}
