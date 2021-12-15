b1dad92c4777038d956b90a5df8efb3ebd630fae
王灏 <hao.wang08@hand-china.com>
2021-12-15 14:07:13 +0800

    [IMP] 添加Maven配置表

---
+++ src/main/java/io/choerodon/devops/app/service/CiTemplateMavenBuildService.java
@@ -0,0 +1,12 @@
+package io.choerodon.devops.app.service;
+
+/**
+ * devops_ci_template_maven_build(CiTemplateMavenBuild)应用服务
+ *
+ * @author hao.wang08@hand-china.com
+ * @since 2021-12-15 14:06:07
+ */
+public interface CiTemplateMavenBuildService {
+
+}
+
---
+++ src/main/java/io/choerodon/devops/infra/dto/CiTemplateMavenPublish.java
@@ -0,0 +1,106 @@
+package io.choerodon.devops.infra.dto;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import io.choerodon.mybatis.annotation.ModifyAudit;
+import io.choerodon.mybatis.annotation.VersionAudit;
+import io.choerodon.mybatis.domain.AuditDomain;
+
+/**
+ * devops_ci_template_maven_publish(CiTemplateMavenPublish)实体类
+ *
+ * @author hao.wang08@hand-china.com
+ * @since 2021-12-15 14:06:08
+ */
+
+@ApiModel("devops_ci_template_maven_publish")
+@VersionAudit
+@ModifyAudit
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
+@Table(name = "devops_ci_template_maven_publish")
+public class CiTemplateMavenPublishDTO extends AuditDomain {
+    public static final String FIELD_ID = "id";
+    public static final String FIELD_NEXUS_MAVEN_REPO_ID_STR = "nexusMavenRepoIdStr";
+    public static final String FIELD_REPO_STR = "repoStr";
+    public static final String FIELD_MAVEN_SETTINGS = "mavenSettings";
+    public static final String FIELD_NEXUS_REPO_ID = "nexusRepoId";
+    public static final String FIELD_CI_TEMPLATE_STEP_ID = "ciTemplateStepId";
+    private static final long serialVersionUID = -67796445138548357L;
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ApiModelProperty(value = "项目下已有的maven仓库id列表")
+    private String nexusMavenRepoIdStr;
+
+    @ApiModelProperty(value = "表单填写的Maven的依赖仓库")
+    private String repoStr;
+
+    @ApiModelProperty(value = "直接粘贴的maven的settings内容")
+    private String mavenSettings;
+
+    @ApiModelProperty(value = "nexus的maven仓库在制品库的主键id")
+    private String nexusRepoId;
+
+    @ApiModelProperty(value = "所属步骤Id", required = true)
+    @NotNull
+    private Long ciTemplateStepId;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNexusMavenRepoIdStr() {
+        return nexusMavenRepoIdStr;
+    }
+
+    public void setNexusMavenRepoIdStr(String nexusMavenRepoIdStr) {
+        this.nexusMavenRepoIdStr = nexusMavenRepoIdStr;
+    }
+
+    public String getRepoStr() {
+        return repoStr;
+    }
+
+    public void setRepoStr(String repoStr) {
+        this.repoStr = repoStr;
+    }
+
+    public String getMavenSettings() {
+        return mavenSettings;
+    }
+
+    public void setMavenSettings(String mavenSettings) {
+        this.mavenSettings = mavenSettings;
+    }
+
+    public String getNexusRepoId() {
+        return nexusRepoId;
+    }
+
+    public void setNexusRepoId(String nexusRepoId) {
+        this.nexusRepoId = nexusRepoId;
+    }
+
+    public Long getCiTemplateStepId() {
+        return ciTemplateStepId;
+    }
+
+    public void setCiTemplateStepId(Long ciTemplateStepId) {
+        this.ciTemplateStepId = ciTemplateStepId;
+    }
+
+}
+
