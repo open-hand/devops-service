06c73d52e40e3f89c13f8d0c4b8a689a3e384dc1
wangxiang <xiangwang04@hand-china.com>
2021-12-16 17:01:10 +0800

    [FIX] 调整指令

--- src/main/java/io/choerodon/devops/infra/util/GitlabCiUtil.java
+++ src/main/java/io/choerodon/devops/infra/util/GitlabCiUtil.java
@@ -263,14 +263,12 @@ public class GitlabCiUtil {
             String rawCommand = "ssh -o StrictHostKeyChecking=no root@127.0.0.1 /kaniko/kaniko %s --no-push -c $PWD/%s -f $PWD/%s -d ${DOCKER_REGISTRY}/${GROUP_NAME}/${PROJECT_NAME}:${CI_COMMIT_TAG} --tarPath ${PWD}/${PROJECT_NAME}.tar";
             commands.add(String.format(rawCommand, skipTlsVerify ? "--skip-tls-verify " : "", dockerBuildContextDir, dockerFilePath));
             //kaniko推镜像成功后可以执行trivy  这里是将镜像扫描的结果保存为json文件 以commmit_tag作为文件的名字 这个文件存在于runner的 /builds/orgCode-projectCode/appCode下，runner的pod停掉以后会自动删除
-            if (imageScan) {
-                String resolveCommond = "trivyScanImage %s";
-                commands.add(String.format(resolveCommond, jobVO.getId()));
-            }
+            String resolveCommond = "trivyScanImage %s";
+            commands.add(String.format(resolveCommond, jobVO.getId()));
             String skopeoCommand = "skopeo copy --dest-tls-verify=false --dest-creds=${DOCKER_USERNAME}:${DOCKER_PASSWORD} docker-archive:${PWD}/${PROJECT_NAME}.tar docker://${DOCKER_REGISTRY}/${GROUP_NAME}/${PROJECT_NAME}:${CI_COMMIT_TAG}";
             commands.add(skopeoCommand);
         } else {
-            String rawCommand = "kaniko %s-c $PWD/%s -f $PWD/%s -d ${DOCKER_REGISTRY}/${GROUP_NAME}/${PROJECT_NAME}:${CI_COMMIT_TAG}";
+            String rawCommand = "ssh -o StrictHostKeyChecking=no root@127.0.0.1 /kaniko/kaniko %s-c $PWD/%s -f $PWD/%s -d ${DOCKER_REGISTRY}/${GROUP_NAME}/${PROJECT_NAME}:${CI_COMMIT_TAG}";
             commands.add(String.format(rawCommand, skipTlsVerify ? "--skip-tls-verify " : "", dockerBuildContextDir, dockerFilePath));
         }
         return commands;
