15920d3edcf274c73b94770315df95b9abc48a51
王灏 <hao.wang08@hand-china.com>
2021-12-20 11:32:27 +0800

    [IMP] 流水线测试任务支持测试套件

--- src/main/java/io/choerodon/devops/infra/feign/fallback/TestServiceClientFallback.java
+++ src/main/java/io/choerodon/devops/infra/feign/fallback/TestServiceClientFallback.java
@@ -20,6 +20,11 @@ public class TestServiceClientFallback implements TestServiceClient {
     }
 
     @Override
+    public ResponseEntity<String> executeSuite(Long projectId, Long apiTestSuiteId, Long executorId, String triggerType, Long triggerId) {
+        throw new CommonException("error.failed.to.execute.suite.task");
+    }
+
+    @Override
     public ResponseEntity<String> queryById(Long projectId, Long recordId) {
         throw new CommonException("error.failed.to.query.test.record");
     }
