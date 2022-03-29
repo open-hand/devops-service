beeb75c274042b0df68adb2a1e7edeef4033abc2
王灏 <hao.wang08@abchand-china.com>
2022-03-29 15:59:24 +0800

    [imp] bug fix

--- src/main/java/io/choerodon/devops/app/service/impl/DevopsCdPipelineServiceImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/DevopsCdPipelineServiceImpl.java
@@ -1218,7 +1218,7 @@ public class DevopsCdPipelineServiceImpl implements DevopsCdPipelineService {
             } else if (ApiTestTaskType.SUITE.getValue().equals(devopsCdApiTestInfoDTO.getTaskType())) {
                 taskRecordDTO = testServiceClientoperator
                         .executeSuite(devopsCdJobRecordDTO.getProjectId(),
-                                devopsCdApiTestInfoDTO.getApiTestTaskId(),
+                                devopsCdApiTestInfoDTO.getApiTestSuiteId(),
                                 devopsCdJobRecordDTO.getCreatedBy(),
                                 ApiTestTriggerType.PIPELINE.getValue(),
                                 jobRecordId);
