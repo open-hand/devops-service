68da92c828eba9d18e433d3444477851e000aeb4
王灏 <hao.wang08@abchand-china.com>
2022-05-18 10:53:35 +0800

    [IMP] 注释应用监控代码

--- src/main/java/io/choerodon/devops/app/service/AppExceptionRecordService.java
+++ src/main/java/io/choerodon/devops/app/service/AppExceptionRecordService.java
@@ -1,16 +1,16 @@
-package io.choerodon.devops.app.service;
-
-import io.choerodon.devops.infra.dto.AppServiceInstanceDTO;
-
-/**
- * chart应用异常信息记录表(AppExceptionRecord)应用服务
- *
- * @author hao.wang08@hand-china.com
- * @since 2022-05-10 11:17:14
- */
-public interface AppExceptionRecordService {
-
-    void createOrUpdateExceptionRecord(String resourceType, String resource, AppServiceInstanceDTO appServiceInstanceDTO);
-
-}
-
+//package io.choerodon.devops.app.service;
+//
+//import io.choerodon.devops.infra.dto.AppServiceInstanceDTO;
+//
+///**
+// * chart应用异常信息记录表(AppExceptionRecord)应用服务
+// *
+// * @author hao.wang08@hand-china.com
+// * @since 2022-05-10 11:17:14
+// */
+//public interface AppExceptionRecordService {
+//
+//    void createOrUpdateExceptionRecord(String resourceType, String resource, AppServiceInstanceDTO appServiceInstanceDTO);
+//
+//}
+//
--- src/main/java/io/choerodon/devops/app/service/DevopsDeployAppCenterService.java
+++ src/main/java/io/choerodon/devops/app/service/DevopsDeployAppCenterService.java
@@ -152,19 +152,19 @@ public interface DevopsDeployAppCenterService {
 
     List<DevopsDeployAppCenterVO> listByAppServiceIds(Long envId, Set<Long> appServiceIds);
 
-    /**
-     * 启用chart应用监控
-     *
-     * @param projectId 项目id
-     * @param appId     应用id
-     */
-    void enableMetric(Long projectId, Long appId);
-
-    /**
-     * 关闭chart应用监控
-     *
-     * @param projectId 项目id
-     * @param appId     应用id
-     */
-    void disableMetric(Long projectId, Long appId);
+//    /**
+//     * 启用chart应用监控
+//     *
+//     * @param projectId 项目id
+//     * @param appId     应用id
+//     */
+//    void enableMetric(Long projectId, Long appId);
+//
+//    /**
+//     * 关闭chart应用监控
+//     *
+//     * @param projectId 项目id
+//     * @param appId     应用id
+//     */
+//    void disableMetric(Long projectId, Long appId);
 }
