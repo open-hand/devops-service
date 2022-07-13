31eb503776569476f2a1dfbcfe813e957dd5b888
王灏 <hao.wang@zknow.com>
2022-07-13 17:16:39 +0800

    [IMP] 拆分应用服务版本表

---
+++ src/main/java/io/choerodon/devops/app/service/AppServiceHelmVersionService.java
@@ -0,0 +1,12 @@
+package io.choerodon.devops.app.service;
+
+/**
+ * 应用版本表(AppServiceHelmVersion)应用服务
+ *
+ * @author hao.wang@zknow.com
+ * @since 2022-07-13 16:47:41
+ */
+public interface AppServiceHelmVersionService {
+
+}
+
---
+++ src/main/java/io/choerodon/devops/infra/mapper/AppServiceHelmVersionMapper.java
@@ -0,0 +1,14 @@
+package io.choerodon.devops.infra.mapper;
+
+import io.choerodon.mybatis.common.BaseMapper;
+import io.choerodon.devops.infra.dto.AppServiceHelmVersionDTO;
+
+/**
+ * 应用版本表(AppServiceHelmVersion)应用服务
+ *
+ * @author hao.wang@zknow.com
+ * @since 2022-07-13 16:47:41
+ */
+public interface AppServiceHelmVersionMapper extends BaseMapper<AppServiceHelmVersionDTO> {
+}
+
