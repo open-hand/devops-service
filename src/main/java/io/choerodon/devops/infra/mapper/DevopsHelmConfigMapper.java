9b6223e425b3347cda3eda6bc9a193a72ac3ad10
李浩 <hao.li@zknow.com>
2022-07-14 11:30:15 +0800

    [ADD] 添加helm仓库查询、创建、修改、删除、设置默认接口

--- src/main/java/io/choerodon/devops/infra/mapper/DevopsHelmConfigMapper.java
+++ src/main/java/io/choerodon/devops/infra/mapper/DevopsHelmConfigMapper.java
@@ -1,7 +1,12 @@
 package io.choerodon.devops.infra.mapper;
 
+import org.apache.ibatis.annotations.Param;
+
 import io.choerodon.devops.infra.dto.DevopsHelmConfigDTO;
 import io.choerodon.mybatis.common.BaseMapper;
 
 public interface DevopsHelmConfigMapper extends BaseMapper<DevopsHelmConfigDTO> {
+    void updateAllHelmConfigRepoDefaultToFalse(@Param("projectId") Long projectId);
+
+    void updateHelmConfigRepoDefaultToTrue(@Param("projectId") Long projectId, @Param("helmConfigId") Long helmConfigId);
 }
