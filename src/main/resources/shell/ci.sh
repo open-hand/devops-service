f4b476ae1ce26f69f09c2a4a4d62608a907e4ed4
李浩 <hao.li@zknow.com>
2022-08-15 14:11:57 +0800

    [IMP] 修改chart构建逻辑

--- src/main/resources/shell/ci.sh
+++ src/main/resources/shell/ci.sh
@@ -187,12 +187,7 @@ function chart_build() {
   export C7N_COMMIT_SHA=$(git log -1 --pretty=format:"%H" | awk '{print substr($1,1,8)}')
 
   rewrite_image_info_for_chart
-  #判断chart主目录名是否与应用编码保持一致
-  CHART_DIRECTORY_PATH=$(find . -maxdepth 2 -name ${PROJECT_NAME})
-  if [ ! -n "${CHART_DIRECTORY_PATH}" ]; then
-    echo "The chart's home directory should be consistent with the application code!"
-    exit 1
-  fi
+
   # 查找Chart.yaml文件
   CHART_PATH=$(find . -maxdepth 3 -name Chart.yaml)
   # 重置values.yaml文件中image属性
@@ -204,6 +199,12 @@ function chart_build() {
     export DOCKER_REPOSITORY="${GROUP_NAME}/${PROJECT_NAME}"
     yq e -i '.image.registry=strenv(DOCKER_REGISTRY)' ${CHART_PATH%/*}/values.yaml
     yq e -i '.image.repository=strenv(DOCKER_REPOSITORY)' ${CHART_PATH%/*}/values.yaml
+  fi
+  nameInChart=$(grep -e '^name:' ${CHART_PATH%/*}/Chart.yaml| awk '{print $2}')
+  # 重置Chart.yaml中的name属性，与当前应用的code保持一致
+  if [ $nameInChart != ${PROJECT_NAME} ]; then
+    echo "Rewrite chart name from ${nameInChart} to ${PROJECT_NAME} "
+    sed -i "s,^name:.*$,name: ${PROJECT_NAME},g" ${CHART_PATH%/*}/Chart.yaml
   fi
   # 构建chart包，重写version与app-version为当前版本
   helm package -u ${CHART_PATH%/*} --version ${CI_COMMIT_TAG} --app-version ${CI_COMMIT_TAG}
