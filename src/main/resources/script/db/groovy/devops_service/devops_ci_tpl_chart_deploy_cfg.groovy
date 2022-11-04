f15c49ef73479f033a4d962ba6f42c1433d99deb
王灏 <hao.wang@zknow.com>
2022-11-04 17:22:51 +0800

    [IMP] 修改种子数据

--- src/main/resources/script/db/groovy/devops_service/devops_ci_tpl_chart_deploy_cfg.groovy
+++ src/main/resources/script/db/groovy/devops_service/devops_ci_tpl_chart_deploy_cfg.groovy
@@ -6,24 +6,12 @@ databaseChangeLog(logicalFilePath: 'dba/devops_ci_tpl_chart_deploy_cfg.groovy') 
             column(name: 'id', type: 'BIGINT UNSIGNED', remarks: '主键，ID', autoIncrement: true) {
                 constraints(primaryKey: true)
             }
-            column(name: 'env_id', type: 'BIGINT UNSIGNED', remarks: '环境Id,devops_env.id') {
-                constraints(nullable: false)
-            }
-            column(name: 'value_id', type: 'BIGINT UNSIGNED', remarks: 'valueId,devops_deploy_value.id') {
-                constraints(nullable: false)
-            }
-            column(name: 'deploy_type', type: 'VARCHAR(100)', remarks: '部署类型：create 新建实例， update 替换实例') {
-                constraints(nullable: false)
-            }
-            column(name: "skip_check_permission", type: "TINYINT UNSIGNED", defaultValue: "0", remarks: '是否校验环境权限') {
-                constraints(nullable: false)
-            }
-            column(name: 'app_name', type: 'VARCHAR(64)', remarks: '应用名称,devops_deploy_app_center_env.name') {
-                constraints(nullable: false)
-            }
-            column(name: 'app_code', type: 'VARCHAR(64)', remarks: '应用编码,devops_deploy_app_center_env.code') {
-                constraints(nullable: false)
-            }
+            column(name: 'env_id', type: 'BIGINT UNSIGNED', remarks: '环境Id,devops_env.id')
+            column(name: 'value_id', type: 'BIGINT UNSIGNED', remarks: 'valueId,devops_deploy_value.id')
+            column(name: 'deploy_type', type: 'VARCHAR(100)', remarks: '部署类型：create 新建实例， update 替换实例')
+            column(name: "skip_check_permission", type: "TINYINT UNSIGNED", defaultValue: "0", remarks: '是否校验环境权限')
+            column(name: 'app_name', type: 'VARCHAR(64)', remarks: '应用名称,devops_deploy_app_center_env.name')
+            column(name: 'app_code', type: 'VARCHAR(64)', remarks: '应用编码,devops_deploy_app_center_env.code')
             column(name: "object_version_number", type: "BIGINT UNSIGNED", defaultValue: "1")
             column(name: "created_by", type: "BIGINT UNSIGNED", defaultValue: "0")
             column(name: "creation_date", type: "DATETIME", defaultValueComputed: "CURRENT_TIMESTAMP")
