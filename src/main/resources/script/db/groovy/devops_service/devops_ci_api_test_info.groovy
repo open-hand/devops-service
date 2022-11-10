ff0d52ebd6e9f70d3c964e1ad8958c36dafc492d
王灏 <hao.wang@zknow.com>
2022-11-10 15:26:24 +0800

    [IMP] 修改删除流水线接口

--- src/main/resources/script/db/groovy/devops_service/devops_ci_api_test_info.groovy
+++ src/main/resources/script/db/groovy/devops_service/devops_ci_api_test_info.groovy
@@ -28,7 +28,7 @@ databaseChangeLog(logicalFilePath: 'dba/devops_ci_api_test_info.groovy') {
         }
     }
     changeSet(author: 'wanghao', id: '2022-11-10-add-index') {
-        createIndex(tableName: 'devops_ci_job_record', indexName: 'devops_ci_api_test_info_n1') {
+        createIndex(tableName: 'devops_ci_api_test_info', indexName: 'devops_ci_api_test_info_n1') {
             column(name: 'ci_pipeline_id')
         }
     }
