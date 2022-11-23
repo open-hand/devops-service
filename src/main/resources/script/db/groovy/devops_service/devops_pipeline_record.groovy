d28b010ed3099047899cee34ae1264496b5b354f
王灏 <hao.wang@zknow.com>
2022-11-23 15:40:52 +0800

    [IMP] 添加持续部署相关表

---
+++ src/main/resources/script/db/groovy/devops_service/devops_pipeline_record.groovy
@@ -0,0 +1,27 @@
+package script.db.groovy.devops_service
+
+databaseChangeLog(logicalFilePath: 'dba/devops_pipeline_record.groovy') {
+    changeSet(author: 'wanghao', id: '2022-11-23-create-table') {
+        createTable(tableName: "devops_pipeline_record", remarks: '流水线执行记录') {
+            column(name: 'id', type: 'BIGINT UNSIGNED', remarks: '主键，ID', autoIncrement: true) {
+                constraints(primaryKey: true)
+            }
+            column(name: 'pipeline_id', type: 'BIGINT UNSIGNED', remarks: '所属流水线Id,devops_pipeline.id') {
+                constraints(nullable: false)
+            }
+            column(name: 'status', type: 'VARCHAR(20)', remarks: '状态') {
+                constraints(nullable: false)
+            }
+
+            column(name: "object_version_number", type: "BIGINT UNSIGNED", defaultValue: "1")
+            column(name: "created_by", type: "BIGINT UNSIGNED", defaultValue: "0")
+            column(name: "creation_date", type: "DATETIME", defaultValueComputed: "CURRENT_TIMESTAMP")
+            column(name: "last_updated_by", type: "BIGINT UNSIGNED", defaultValue: "0")
+            column(name: "last_update_date", type: "DATETIME", defaultValueComputed: "CURRENT_TIMESTAMP")
+        }
+        createIndex(tableName: 'devops_pipeline_record', indexName: 'devops_pipeline_record_n1') {
+            column(name: 'pipeline_id')
+        }
+    }
+
+}
