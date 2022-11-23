d8d0c0f2087e502741defcf319e4f87ce519e6bf
王灏 <hao.wang@zknow.com>
2022-11-23 16:06:25 +0800

    [IMP] 添加流水线容器部署参数校验

--- src/main/java/io/choerodon/devops/app/eventhandler/pipeline/job/AbstractAppDeployJobHandlerImpl.java
+++ src/main/java/io/choerodon/devops/app/eventhandler/pipeline/job/AbstractAppDeployJobHandlerImpl.java
@@ -3,19 +3,24 @@ package io.choerodon.devops.app.eventhandler.pipeline.job;
 import java.util.List;
 import java.util.Objects;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.util.CollectionUtils;
 
+import io.choerodon.core.exception.CommonException;
 import io.choerodon.core.oauth.CustomUserDetails;
 import io.choerodon.core.oauth.DetailsHelper;
 import io.choerodon.devops.api.vo.DevopsCiJobVO;
+import io.choerodon.devops.api.vo.pipeline.AppDeployConfigVO;
 import io.choerodon.devops.app.service.DevopsCiPipelineService;
 import io.choerodon.devops.app.service.DevopsEnvironmentService;
+import io.choerodon.devops.infra.constant.ExceptionConstants;
 import io.choerodon.devops.infra.dto.CiCdPipelineDTO;
 import io.choerodon.devops.infra.dto.DevopsEnvUserPermissionDTO;
 import io.choerodon.devops.infra.dto.DevopsEnvironmentDTO;
 import io.choerodon.devops.infra.dto.iam.ProjectDTO;
+import io.choerodon.devops.infra.enums.deploy.DeployTypeEnum;
 import io.choerodon.devops.infra.feign.operator.BaseServiceClientOperator;
 import io.choerodon.devops.infra.mapper.DevopsEnvUserPermissionMapper;
 
@@ -28,6 +33,8 @@ import io.choerodon.devops.infra.mapper.DevopsEnvUserPermissionMapper;
  */
 public abstract class AbstractAppDeployJobHandlerImpl extends AbstractJobHandler {
 
+    private static final String DEVOPS_APP_DEPLOY_CONFIG_EMPTY = "devops.app.deploy.config.empty";
+
     @Autowired
     protected DevopsEnvironmentService devopsEnvironmentService;
     @Autowired
@@ -37,6 +44,32 @@ public abstract class AbstractAppDeployJobHandlerImpl extends AbstractJobHandler
     @Autowired
     @Lazy
     private DevopsCiPipelineService devopsCiPipelineService;
+
+    @Override
+    protected void checkConfigInfo(Long projectId, DevopsCiJobVO devopsCiJobVO) {
+        AppDeployConfigVO appDeployConfigVO = devopsCiJobVO.getCiDeployDeployCfg();
+        if (appDeployConfigVO == null) {
+            throw new CommonException(DEVOPS_APP_DEPLOY_CONFIG_EMPTY, devopsCiJobVO.getName());
+        }
+        if (appDeployConfigVO.getEnvId() == null) {
+            throw new CommonException(DEVOPS_APP_DEPLOY_CONFIG_EMPTY, devopsCiJobVO.getName());
+        }
+        if (StringUtils.isEmpty(appDeployConfigVO.getDeployType())) {
+            throw new CommonException(ExceptionConstants.AppCode.DEVOPS_APP_DEPLOY_TYPE_IS_EMPTY);
+        }
+        if (DeployTypeEnum.CREATE.value().equals(appDeployConfigVO.getDeployType())) {
+            if (StringUtils.isEmpty(appDeployConfigVO.getAppName())) {
+                throw new CommonException(ExceptionConstants.AppCode.DEVOPS_APP_NAME_IS_EMPTY);
+            }
+            if (StringUtils.isEmpty(appDeployConfigVO.getAppCode())) {
+                throw new CommonException(ExceptionConstants.AppCode.DEVOPS_APP_CODE_IS_EMPTY);
+            }
+        } else {
+            if (appDeployConfigVO.getAppId() == null) {
+                throw new CommonException(ExceptionConstants.AppCode.DEVOPS_APP_ID_IS_EMPTY);
+            }
+        }
+    }
 
     @Override
     public void fillJobAdditionalInfo(DevopsCiJobVO devopsCiJobVO) {
