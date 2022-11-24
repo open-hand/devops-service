25aea8981213272abee1f247cc40e9079331c17f
王灏 <hao.wang@zknow.com>
2022-11-24 09:39:43 +0800

    [IMP] 添加持续集成代码

---
+++ src/main/resources/script/db/groovy/devops_service/devops_pipeline.groovy
@@ -0,0 +1,27 @@
+package script.db.groovy.devops_service
+
+databaseChangeLog(logicalFilePath: 'dba/devops_pipeline.groovy') {
+    changeSet(author: 'wanghao', id: '2022-11-23-create-table') {
+        createTable(tableName: "devops_pipeline", remarks: '流水线表') {
+            column(name: 'id', type: 'BIGINT UNSIGNED', remarks: '主键，ID', autoIncrement: true) {
+                constraints(primaryKey: true)
+            }
+            column(name: 'name', type: 'VARCHAR(64)', remarks: '流水线名称') {
+                constraints(nullable: false)
+            }
+            column(name: 'effect_version_id', type: 'BIGINT UNSIGNED', remarks: '当前生效的版本，devops_pipeline_version.id') {
+                constraints(nullable: false)
+            }
+
+            column(name: "object_version_number", type: "BIGINT UNSIGNED", defaultValue: "1")
+            column(name: "created_by", type: "BIGINT UNSIGNED", defaultValue: "0")
+            column(name: "creation_date", type: "DATETIME", defaultValueComputed: "CURRENT_TIMESTAMP")
+            column(name: "last_updated_by", type: "BIGINT UNSIGNED", defaultValue: "0")
+            column(name: "last_update_date", type: "DATETIME", defaultValueComputed: "CURRENT_TIMESTAMP")
+        }
+        createIndex(tableName: 'devops_pipeline', indexName: 'devops_pipeline_n1') {
+            column(name: 'name')
+        }
+    }
+
+}
--- src/main/resources/script/db/groovy/devops_service/devops_pipeline_stage.groovy
+++ src/main/resources/script/db/groovy/devops_service/devops_pipeline_stage.groovy
@@ -1,22 +1,20 @@
 package script.db.groovy.devops_service
 
-databaseChangeLog(logicalFilePath: 'dba/devops_pipeline_record.groovy') {
+databaseChangeLog(logicalFilePath: 'dba/devops_pipeline_stage.groovy') {
     changeSet(author: 'wanghao', id: '2022-11-23-create-table') {
-        createTable(tableName: "devops_pipeline_record", remarks: '流水线执行记录') {
+        createTable(tableName: "devops_pipeline_stage", remarks: '流水线阶段表') {
             column(name: 'id', type: 'BIGINT UNSIGNED', remarks: '主键，ID', autoIncrement: true) {
                 constraints(primaryKey: true)
             }
             column(name: 'pipeline_id', type: 'BIGINT UNSIGNED', remarks: '所属流水线Id,devops_pipeline.id') {
                 constraints(nullable: false)
             }
-            column(name: 'status', type: 'VARCHAR(20)', remarks: '状态') {
+            column(name: 'version_id', type: 'BIGINT UNSIGNED', remarks: '所属版本Id,devops_pipeline_version.id') {
                 constraints(nullable: false)
             }
-            column(name: 'trigger_type', type: 'VARCHAR(20)', remarks: '触发方式') {
+            column(name: 'name', type: 'VARCHAR(64)', remarks: '名称') {
                 constraints(nullable: false)
             }
-            column(name: 'app_service_id', type: 'BIGINT UNSIGNED', remarks: '触发应用服务id,devops_app_service.id')
-            column(name: 'app_service_version_id', type: 'BIGINT UNSIGNED', remarks: '触发应用服务版本id,devops_app_service_version.id')
 
             column(name: "object_version_number", type: "BIGINT UNSIGNED", defaultValue: "1")
             column(name: "created_by", type: "BIGINT UNSIGNED", defaultValue: "0")
@@ -24,9 +22,14 @@ databaseChangeLog(logicalFilePath: 'dba/devops_pipeline_record.groovy') {
             column(name: "last_updated_by", type: "BIGINT UNSIGNED", defaultValue: "0")
             column(name: "last_update_date", type: "DATETIME", defaultValueComputed: "CURRENT_TIMESTAMP")
         }
-        createIndex(tableName: 'devops_pipeline_record', indexName: 'devops_pipeline_record_n1') {
+        createIndex(tableName: 'devops_pipeline_stage', indexName: 'devops_pipeline_stage_n1') {
             column(name: 'pipeline_id')
         }
+        createIndex(tableName: 'devops_pipeline_stage', indexName: 'devops_pipeline_stage_n2') {
+            column(name: 'version_id')
+        }
+        addUniqueConstraint(tableName: 'devops_pipeline_stage',
+                constraintName: 'devops_pipeline_stage_u1', columnNames: 'version_id,name')
     }
 
 }
