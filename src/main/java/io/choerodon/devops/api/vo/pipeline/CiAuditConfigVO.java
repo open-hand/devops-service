42764efed402766a99935d66a22b0f6f59a78d33
汪翔 <xiang.wang@zknow.com>
2022-11-30 14:39:58 +0800

    [FIX] 增加加密注解

--- src/main/java/io/choerodon/devops/api/vo/pipeline/CiAuditConfigVO.java
+++ src/main/java/io/choerodon/devops/api/vo/pipeline/CiAuditConfigVO.java
@@ -14,6 +14,7 @@ import io.choerodon.devops.infra.dto.iam.IamUserDTO;
 public class CiAuditConfigVO {
 
 
+    @Encrypt
     private Long id;
     @ApiModelProperty(value = "是否会签 1是会签,0 是或签", required = true)
     private Boolean countersigned;
