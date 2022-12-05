342d0ff2fc06a4229312bf445db31ec12dbd6fed
王灏 <hao.wang@zknow.com>
2022-12-05 12:35:15 +0800

    [IMP] bug fix

--- src/main/java/io/choerodon/devops/app/service/impl/PipelineServiceImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/PipelineServiceImpl.java
@@ -424,7 +424,7 @@ public class PipelineServiceImpl implements PipelineService {
         pipelineRecordDTO.setStatus(PipelineStatusEnum.CREATED.value());
         pipelineRecordDTO.setPipelineId(id);
         pipelineRecordDTO.setStartedDate(new Date());
-        if (PipelineTriggerTypeEnum.API.equals(triggerType)) {
+        if (PipelineTriggerTypeEnum.APP_VERSION.equals(triggerType)) {
             pipelineRecordDTO.setAppServiceId((Long) params.get(MiscConstants.APP_SERVICE_ID));
             pipelineRecordDTO.setAppServiceVersionId((Long) params.get(MiscConstants.APP_VERSION_ID));
         }
