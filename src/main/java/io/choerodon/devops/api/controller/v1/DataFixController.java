c674e5a7baf833a922286da8f3610d46a148f3de
王灏 <hao.wang@zknow.com>
2022-12-21 16:49:18 +0800

    [IMP] 代码调整

--- src/main/java/io/choerodon/devops/api/controller/v1/DataFixController.java
+++ src/main/java/io/choerodon/devops/api/controller/v1/DataFixController.java
@@ -2,7 +2,6 @@ package io.choerodon.devops.api.controller.v1;
 
 import static io.choerodon.devops.app.service.impl.DevopsCheckLogServiceImpl.MIGRATION_CD_PIPELINE_DATE;
 
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,13 +19,8 @@ public class DataFixController {
     @Autowired
     private DevopsCheckLogService devopsCheckLogService;
 
-    /**
-     * 释放这个微服务实例所持有的一些资源(比如redis的键)
-     * 一般由外部的容器根据生命周期调用
-     */
     @PostMapping("/migration_cdpipeline_data")
     @Permission(level = ResourceLevel.SITE)
-    @ApiOperation(value = "释放这个微服务实例所持有的一些资源(比如redis的键),一般由外部的容器根据生命周期调用")
     public ResponseEntity<Void> migrationCdPipelineDate() {
         devopsCheckLogService.checkLog(MIGRATION_CD_PIPELINE_DATE);
         return ResponseEntity.ok().build();
