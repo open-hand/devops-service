955c2fe6029f642446e5a2c9532ff5c52caa14eb
王灏 <hao.wang@zknow.com>
2023-01-04 16:02:15 +0800

    [IMP] 流水线chart发布支持配置推送的helm仓库

---
+++ src/main/java/io/choerodon/devops/infra/dto/CiChartPublishConfigDTO.java
@@ -0,0 +1,70 @@
+package io.choerodon.devops.infra.dto;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import io.choerodon.mybatis.annotation.ModifyAudit;
+import io.choerodon.mybatis.annotation.VersionAudit;
+import io.choerodon.mybatis.domain.AuditDomain;
+
+/**
+ * 流水线chart发布配置(CiChartPublishConfig)实体类
+ *
+ * @author hao.wang@zknow.com
+ * @since 2023-01-04 15:28:30
+ */
+
+@ApiModel("流水线chart发布配置")
+@VersionAudit
+@ModifyAudit
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
+@Table(name = "devops_ci_chart_publish_config")
+public class CiChartPublishConfigDTO extends AuditDomain {
+    public static final String FIELD_ID = "id";
+    public static final String FIELD_STEP_ID = "stepId";
+    public static final String FIELD_REPO_ID = "repoId";
+    private static final long serialVersionUID = -56756448103662134L;
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ApiModelProperty(value = "所属步骤id", required = true)
+    @NotNull
+    private Long stepId;
+
+    @ApiModelProperty(value = "helm仓库id")
+    private Long repoId;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getStepId() {
+        return stepId;
+    }
+
+    public void setStepId(Long stepId) {
+        this.stepId = stepId;
+    }
+
+    public Long getRepoId() {
+        return repoId;
+    }
+
+    public void setRepoId(Long repoId) {
+        this.repoId = repoId;
+    }
+
+}
+
