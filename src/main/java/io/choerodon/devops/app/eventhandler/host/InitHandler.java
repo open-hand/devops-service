4e899b65a95070ac9fbbea7968281cd22500f82a
李浩 <hao.li@zknow.com>
2023-01-05 10:44:16 +0800

    [IMP] 添加主机网卡信息

---
+++ src/main/java/io/choerodon/devops/app/eventhandler/host/InitHandler.java
@@ -0,0 +1,36 @@
+package io.choerodon.devops.app.eventhandler.host;
+
+import static io.choerodon.devops.infra.enums.host.HostMsgEventEnum.INIT_AGENT;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+
+import io.choerodon.devops.api.vo.host.HostInfoVO;
+import io.choerodon.devops.infra.dto.DevopsHostDTO;
+import io.choerodon.devops.infra.mapper.DevopsHostMapper;
+import io.choerodon.devops.infra.util.JsonHelper;
+
+@Component
+public class InitHandler implements HostMsgHandler {
+    @Autowired
+    private DevopsHostMapper devopsHostMapper;
+
+
+    @Override
+    public void handler(String hostId, Long commandId, String payload) {
+        if (!ObjectUtils.isEmpty(payload)) {
+            HostInfoVO hostInfoVO = JsonHelper.unmarshalByJackson(payload, HostInfoVO.class);
+            if (!ObjectUtils.isEmpty(hostInfoVO.getNetwork())) {
+                DevopsHostDTO devopsHostDTO = devopsHostMapper.selectByPrimaryKey(hostId);
+                devopsHostDTO.setNetwork(hostInfoVO.getNetwork());
+                devopsHostMapper.updateByPrimaryKeySelective(devopsHostDTO);
+            }
+        }
+    }
+
+    @Override
+    public String getType() {
+        return INIT_AGENT.value();
+    }
+}
