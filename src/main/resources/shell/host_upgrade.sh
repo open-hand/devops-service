79d92c2469b123775ecdf56b907e3c05b95bc9dd
李浩 <hao.li@zknow.com>
2023-01-11 10:02:52 +0800

    [IMP] 优化主机agent的安装和升级命令以兼容运行在权限被限制的主机上

--- src/main/resources/shell/host_upgrade.sh
+++ src/main/resources/shell/host_upgrade.sh
@@ -11,22 +11,8 @@ AGENT_NAME=c7n-agent
 AGENT=${WORK_DIR}/${AGENT_NAME}
 TAR_FILE=${WORK_DIR}/c7n-agent.tar.gz
 
-# 1. 校验当前用户有var目录
-if [ -z "${VAR}" ]; then
-  sudo mkdir /var
-fi
-
-# 2. 创建choerodon目录
-if [ ! -d "${WORK_DIR}" ]; then
-  echo "Creating ${WORK_DIR} directory"
-  sudo mkdir $WORK_DIR
-  sudo chmod 0777 $WORK_DIR
-  sudo chown $USER:$USER $WORK_DIR
-  echo "Working directory ${WORK_DIR} created successfully"
-fi
-
 # 3. 保存环境变量
-cat <<EOF | sudo tee ${WORK_DIR}/c7n-agent.env
+cat <<EOF | tee ${WORK_DIR}/c7n-agent.env
 VAR=/var
 WORK_DIR=${VAR}/choerodon
 TOKEN={{ TOKEN }}
@@ -38,9 +24,10 @@ AGENT=${WORK_DIR}/${AGENT_NAME}
 TAR_FILE=${WORK_DIR}/c7n-agent.tar.gz
 
 EOF
-sudo chmod 0777 ${WORK_DIR}/c7n-agent.env
 
-cat <<EOF | sudo tee ${WORK_DIR}/c7n-agent.sh
+chmod 0777 ${WORK_DIR}/c7n-agent.env
+
+cat <<EOF | tee ${WORK_DIR}/c7n-agent.sh
 #!/bin/sh
 operate=\$1
 case \$operate in
@@ -59,7 +46,7 @@ esac
 
 EOF
 
-sudo chmod 0777 ${WORK_DIR}/c7n-agent.sh
+chmod 0777 ${WORK_DIR}/c7n-agent.sh
 
 cd "$WORK_DIR" || exit
 
