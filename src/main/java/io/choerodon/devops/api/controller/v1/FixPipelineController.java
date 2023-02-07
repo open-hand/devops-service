5436f2e5ad9bcf4b8a962f69a5fce8a458f63c6f
李浩 <hao.li@zknow.com>
2023-02-07 17:43:18 +0800

    [IMP] 修改接口访问权限

--- src/main/java/io/choerodon/devops/api/controller/v1/FixPipelineController.java
+++ src/main/java/io/choerodon/devops/api/controller/v1/FixPipelineController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.choerodon.core.iam.ResourceLevel;
 import io.choerodon.devops.app.service.DevopsCheckLogService;
 import io.choerodon.swagger.annotation.Permission;
 
@@ -17,7 +18,7 @@ public class FixPipelineController {
     @Autowired
     private DevopsCheckLogService devopsCheckLogService;
 
-    @Permission(permissionLogin = true)
+    @Permission(level = ResourceLevel.ORGANIZATION)
     @ApiOperation("修复流水线")
     @PostMapping("/{pipeline_id}")
     public void fixPipeline(@PathVariable("pipeline_id") Long pipelineId) {
