30ec0a59f37b193bcf0771cb32560c9f52459279
李浩 <hao.li@zknow.com>
2023-03-07 14:06:06 +0800

    [ADD] 添加流水线触发器模板

---
+++ src/main/java/io/choerodon/devops/app/service/impl/CiTemplatePipelineTriggerServiceImpl.java
@@ -0,0 +1,9 @@
+package io.choerodon.devops.app.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import io.choerodon.devops.app.service.CiTemplatePipelineTriggerService;
+
+@Service
+public class CiTemplatePipelineTriggerServiceImpl implements CiTemplatePipelineTriggerService {
+}
