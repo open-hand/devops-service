ea799c593913f2116a456a92339aff7ee3694ebd
李浩 <hao.li@zknow.com>
2023-03-08 09:37:13 +0800

    [IMP] 修改groovy

--- src/main/resources/script/db/groovy/devops_service/devops_ci_pipeline_trigger_config_variable.groovy
+++ src/main/resources/script/db/groovy/devops_service/devops_ci_pipeline_trigger_config_variable.groovy
@@ -26,7 +26,7 @@ databaseChangeLog(logicalFilePath: 'dba/devops_ci_pipeline_trigger_config_variab
 
         }
         addUniqueConstraint(tableName: 'devops_ci_pipeline_trigger_config_variable',
-                constraintName: 'uk_key_value', columnNames: 'job_id,variable_key')
+                constraintName: 'uk_key_value', columnNames: 'pipeline_trigger_config_id,variable_key')
         createIndex(tableName: 'devops_ci_pipeline_trigger_config_variable', indexName: 'idx_pipeline_id') {
             column(name: 'pipeline_trigger_config_id')
         }
